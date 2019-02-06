cf6c79b75b618753c59a6946eb00e107cc34fef9
Fares Osman <fares.osman@outlook.com>
2019-02-05 21:52:05 -0500

    Modified UI - added grid to board

--- ruby_curriculum/3_memory_puzzle_game/board.rb
+++ ruby_curriculum/3_memory_puzzle_game/board.rb
@@ -4,18 +4,19 @@ class Board
 
   LETTERS = ("A".."Z").to_a.shuffle
 
-  attr_accessor :deck
+  attr_accessor :deck, :letters_grid
 
   def initialize(deck_size = 10)
     @deck = Array.new(deck_size)
+    @letters_grid = Array.new(deck_size)
     populate_deck
   end
 
   def populate_deck 
     @deck.each_index do |idx| 
       @deck[idx] = Card.new(populate_letters[idx])
+      @letters_grid[idx] = idx
     end
-    @deck
   end
 
   def populate_letters
@@ -28,7 +29,10 @@ class Board
 
   def delete_pairs
     @deck.each_index do |card_index| 
-      @deck.delete(@deck[card_index]) if @deck[card_index].face_up
+      if @deck[card_index].face_up 
+         @deck.delete(@deck[card_index]) 
+         @letters_grid = @letters_grid.take(@letters_grid.count - 1)
+      end
     end
   end
 
@@ -39,6 +43,8 @@ class Board
   def clear
     system('clear')
     puts self.render.join(" ")
+    puts self.letters_grid.join(" ")
+    puts "\n"
   end
 
 end
