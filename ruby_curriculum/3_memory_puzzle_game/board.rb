d31e8a8e21b6e774e5a475527aa1676900d4e346
Fares Osman <fares.osman@outlook.com>
2019-02-07 23:15:40 -0500

    Improved game logic - Complete randomization of card deck

--- ruby_curriculum/3_memory_puzzle_game/board.rb
+++ ruby_curriculum/3_memory_puzzle_game/board.rb
@@ -4,47 +4,63 @@ class Board
 
   LETTERS = ("A".."Z").to_a.shuffle
 
-  attr_accessor :deck, :letters_grid
+  attr_accessor :deck
 
-  def initialize(deck_size = 10)
+  def initialize(deck_size=10)
     @deck = Array.new(deck_size)
-    @letters_grid = Array.new(deck_size)
     populate_deck
   end
 
   def populate_deck 
     @deck.each_index do |idx| 
-      @deck[idx] = Card.new(populate_letters[idx])
-      @letters_grid[idx] = idx
+      @deck[idx] = Card.new(populate_letters[idx], idx)
     end
+    self.shuffle_deck
   end
 
   def populate_letters
     LETTERS.take(@deck.count/2) * 2
   end
 
+  def shuffle_deck 
+    @deck.shuffle!
+  end
+
   def render
-   @deck.map { |card| card.value? }
+    @deck.map { |card| card.value? }
   end
 
   def delete_pairs
     @deck.each_index do |card_index| 
       if @deck[card_index].face_up 
          @deck.delete(@deck[card_index]) 
-         @letters_grid = @letters_grid.take(@letters_grid.count - 1)
       end
     end
   end
 
+  def select_card(player_input)
+    @deck.select { |card| card.numerical_value == player_input }
+  end
+
+   def get_card_pos(card)
+     @deck.find_index(self.select_card(card).first)
+   end
+
   def reset
     @deck.each { |card| card.hide }
   end
-
+  
   def clear
     system('clear')
     puts self.render.join(" ")
-    puts self.letters_grid.join(" ")
+    puts self.display_grid.join(" ")
     puts "\n"
   end
 
+  def display_grid 
+    @deck.map { |card| card.numerical_value }
+  end
+
 end
+
+
