c10189c598e768936d9738d8e4a39ec91ee1b6a2
Fares Osman <fares.osman@outlook.com>
2019-02-20 21:18:23 -0500

    Finished exercise

--- alpha_curriculum/[SWE foundations] advanced_methods_exercises/02_adult_in_group.rb
+++ alpha_curriculum/[SWE foundations] advanced_methods_exercises/02_adult_in_group.rb
@@ -3,6 +3,11 @@
 # The method should return false otherwise.
 
 
+def adult_in_group?(people)
+    people.any? { |person| person[:age] >= 18 }
+end
+
+
 people_1 = [
     {name: "Jack", age: 17},
     {name: "Jill", age: 21},
