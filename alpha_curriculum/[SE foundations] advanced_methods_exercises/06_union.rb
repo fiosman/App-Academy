f1eb0af4b7151ef4e82df3250d28c30c521d930c
Fares Osman <fares.osman@outlook.com>
2019-02-20 22:20:39 -0500

    Finished exercise

--- alpha_curriculum/[SE foundations] advanced_methods_exercises/06_union.rb
+++ alpha_curriculum/[SE foundations] advanced_methods_exercises/06_union.rb
@@ -1,6 +1,7 @@
 # Write a method, union, that accepts any number of arrays as arguments.
 # The method should return an array containing all elements of the given arrays.
-
-
+  def union (*arr)
+    arr.inject { |complete_arr, current_arr| complete_arr.concat(current_arr) }
+  end
 p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
 p union(["x", "y"], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
