f439fe98c0b210f36c8694edf5fb14be5ceb81f4
Fares Osman <fares.osman@outlook.com>
2019-02-20 21:41:23 -0500

    Finished exercise

--- alpha_curriculum/[SWE foundations] advanced_methods_exercises/03_only_vowels.rb
+++ alpha_curriculum/[SWE foundations] advanced_methods_exercises/03_only_vowels.rb
@@ -2,6 +2,10 @@
 # The method should return true if the string contains only vowels.
 # The method should return false otherwise.
 
+def only_vowels?(str)
+  vowels = "aeiou"
+  str.split("").all? { |letter| vowels.include?(letter) }
+end
 
 p only_vowels?("aaoeee")  # => true
 p only_vowels?("iou")     # => true
