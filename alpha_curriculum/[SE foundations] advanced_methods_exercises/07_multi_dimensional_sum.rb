1ababb4dab6bfc8272c91755dd2f616ca2f0a0c9
Fares Osman <fares.osman@outlook.com>
2019-02-20 22:29:07 -0500

    Finished exercise

--- alpha_curriculum/[SE foundations] advanced_methods_exercises/07_multi_dimensional_sum.rb
+++ alpha_curriculum/[SE foundations] advanced_methods_exercises/07_multi_dimensional_sum.rb
@@ -1,6 +1,9 @@
 # Write a method, multi_dimensional_sum(array), that accepts a multidimensional array as an arg 
 # and returns the sum of all numbers in the array.
 
+def multi_dimensional_sum(array)
+    array.inject { |one_dim_arr, sub_arr| one_dim_arr + sub_arr }.flatten.reduce(&:+)
+end
 
 arr_1 = [
     [4, 3, 1],
