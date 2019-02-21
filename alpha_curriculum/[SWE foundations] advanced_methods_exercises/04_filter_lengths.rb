9a0a038ea43ba7df19248d86b6eec8dafd94cb11
Fares Osman <fares.osman@outlook.com>
2019-02-20 21:47:44 -0500

    Finished exercise

--- alpha_curriculum/[SWE foundations] advanced_methods_exercises/04_filter_lengths.rb
+++ alpha_curriculum/[SWE foundations] advanced_methods_exercises/04_filter_lengths.rb
@@ -3,6 +3,9 @@
 # that have at least the given length. The length argument should be optional; if no length
 # is passed in, then 5 should be used as the length.
 
+def filter_lengths(strings,length=5)
+  strings.select { |word| word.length >= length }
+end
 
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"], 4)   # => ["pear", "dragonfruit", "clementine"]
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"])      # => ["dragonfruit", "clementine"]
