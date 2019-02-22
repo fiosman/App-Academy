b8fafb0489e2d149c46bcf9eeb488b0413d10789
Fares Osman <fares.osman@outlook.com>
2019-02-21 22:34:17 -0500

    Finished exercise

--- alpha_curriculum/[SE foundations]algorithmic_strategies_exercise/04_peak_finder.rb
+++ alpha_curriculum/[SE foundations]algorithmic_strategies_exercise/04_peak_finder.rb
@@ -3,6 +3,23 @@
 # An element is considered a "peak" if it is greater than both it's left and right neighbor.
 # The first or last element of the array is considered a "peak" if it is greater than it's one neighbor.
 
+def peak_finder(arr)
+  peaks_arr = []
+
+  arr.each_index do |idx| 
+    left = arr[idx - 1]
+    right = arr[idx + 1]
+
+    if idx == 0 && arr[idx] > right
+      peaks_arr << arr[idx] 
+    elsif idx == arr.length - 1 && arr[idx]  > left
+      peaks_arr << arr[idx] 
+    elsif arr[idx]  > left && arr[idx]  > right
+      peaks_arr << arr[idx] 
+    end
+  end
+  peaks_arr
+end
 
 p peak_finder([1, 3, 5, 4])         # => [5]
 p peak_finder([4, 2, 3, 6, 10])     # => [4, 10]
