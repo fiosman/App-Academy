f0c91752f9e1b2b1b93c2723c6e649aa64729d0c
Fares Osman <fares.osman@outlook.com>
2019-02-22 22:05:26 -0500

    Finished exercise

--- alpha_curriculum/[SE foundations]algorithmic_strategies_exercise/03_bubble_sort.rb
+++ alpha_curriculum/[SE foundations]algorithmic_strategies_exercise/03_bubble_sort.rb
@@ -9,7 +9,7 @@ def bubble_sort(arr)
 
   loop do 
      swapped = false
-    (0...arr.length - 1).each do |idx| 
+    (arr.length-1).times do |idx| 
       if arr[idx] > arr[idx + 1]
         arr[idx], arr[idx+1] = arr[idx+1], arr[idx]
         swapped = true
@@ -22,3 +22,11 @@ end
 
 p bubble_sort([2, 8, 5, 2, 6])      # => [2, 2, 5, 6, 8]
 p bubble_sort([10, 8, 7, 1, 2, 3])  # => [1, 2, 3, 7, 8, 10]
+
+
+#Bubble Sort - Pretty simple sorting algorithm. Also INEFFICIENT. Never use it!
+#Its purpose is to arrange an array of numbers in increasing order. 
+#Does this by comparing adjacent pairs, if first number in the pair is larger than the second then swap the values.
+#The key is to make sure the above keeps going until the array is entirely sorted. 
+#This is done pretty easily by assigning a variable that keeps track of the "swapped status" through each pass. 
+#If no swaps are made, swapped is false and the array is sorted. Otherwise, swapped is true -- Program keeps running until swapped is false. 
