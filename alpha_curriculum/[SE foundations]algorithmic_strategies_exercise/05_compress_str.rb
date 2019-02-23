555189ecb10e5c95843e156a3eb063cc42756282
Fares Osman <fares.osman@outlook.com>
2019-02-23 14:26:25 -0500

    Finished exercise

--- alpha_curriculum/[SE foundations]algorithmic_strategies_exercise/05_compress_str.rb
+++ alpha_curriculum/[SE foundations]algorithmic_strategies_exercise/05_compress_str.rb
@@ -1,7 +1,17 @@
 # Write a method, compress_str(str), that accepts a string as an arg.
 # The method should return a new str where streaks of consecutive characters are compressed.
 # For example "aaabbc" is compressed to "3a2bc".
-
+  def compress_str(string)
+    compressed_str = ""
+    string.chars.chunk(&:itself).to_a.each do |ele| 
+      if ele.last.length > 1 
+        compressed_str += ele.last.length.to_s + ele.first
+      else
+        compressed_str += ele.first
+      end
+    end
+    compressed_str
+  end
 
 p compress_str("aaabbc")        # => "3a2bc"
 p compress_str("xxyyyyzz")      # => "2x4y2z"
