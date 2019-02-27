15a24080b3d7ea93112ce5924f647f7556af8ee5
Fares Osman <fares.osman@outlook.com>
2019-02-26 22:43:56 -0500

    Finished add_numbers

--- ruby_curriculum/5_recursion_homework/recursion_homework.rb
+++ ruby_curriculum/5_recursion_homework/recursion_homework.rb
@@ -4,7 +4,8 @@ def sum_to(n)
 end
 
 def add_numbers(nums_array)
-  
+  return nums_array.first if nums_array.length <= 1
+  nums_array.first + add_numbers(nums_array[1..-1])
 end
 
 def gamma_function(n)
@@ -19,4 +20,3 @@ def reverse(string)
 
 end
 
-p sum_to(5)
