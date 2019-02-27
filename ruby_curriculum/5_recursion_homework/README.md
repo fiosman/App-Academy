2786ed9d739ea92435ae9a68f87802a9b9b057a4
Fares Osman <fares.osman@outlook.com>
2019-02-26 22:38:40 -0500

    sum_to finished

---
+++ ruby_curriculum/5_recursion_homework/README.md
@@ -0,0 +1,6 @@
+# Recursion Homework
+
+
+
+
+
---
+++ ruby_curriculum/5_recursion_homework/recursion_homework.rb
@@ -0,0 +1,22 @@
+def sum_to(n)
+  return n if n == 1
+  sum_to(n-1) + n 
+end
+
+def add_numbers(nums_array)
+  
+end
+
+def gamma_function(n)
+
+end
+
+def ice_cream_shop(flavors, favorite)
+
+end
+
+def reverse(string)
+
+end
+
+p sum_to(5)
