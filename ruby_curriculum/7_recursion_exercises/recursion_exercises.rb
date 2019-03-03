abb5186ebdeb7a14a8c465e15096a5380ab2fb77
Fares Osman <fares.osman@outlook.com>
2019-03-03 01:10:56 -0500

    First commit

--- ruby_curriculum/7_recursion_exercises/recursion_exercises.rb
+++ ruby_curriculum/7_recursion_exercises/recursion_exercises.rb
@@ -1,14 +1,22 @@
-#Write a recursive method, range, that takes a start and an end and returns an array of all 
-#numbers in that range, exclusive. For example, range(1, 5) should return [1, 2, 3, 4]. 
-#If end < start, you can return an empty array. Write both a recursive and iterative version of sum of an array.
+
+
+#Write both a recursive and iterative version of sum of an array.
 
 #Iterative approach
-def range(start_range, end_range)
-  (start_range...end_range).inject([]) do |array, ele|
-    return [] if end_range < start_range
-    array << ele 
-  end
+def sum_iterative(array)
+  array.reduce(&:+)
 end
 
 #Recursive approach 
-p range(7,0)
+def sum_recursive(array)
+
+end
+
+#Write a recursive method, range, that takes a start and an end and returns an array of all 
+#numbers in that range, exclusive. For example, range(1, 5) should return [1, 2, 3, 4]. 
+#If end < start, you can return an empty array. 
+
+def range
+
+
+end
