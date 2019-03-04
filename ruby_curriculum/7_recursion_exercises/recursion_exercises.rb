256fe2aa932ba0d3016e6b74114939b75637a90a
Fares Osman <fares.osman@outlook.com>
2019-03-03 19:56:26 -0500

    Finished range

--- ruby_curriculum/7_recursion_exercises/recursion_exercises.rb
+++ ruby_curriculum/7_recursion_exercises/recursion_exercises.rb
@@ -17,7 +17,7 @@ end
 #numbers in that range, exclusive. For example, range(1, 5) should return [1, 2, 3, 4]. 
 #If end < start, you can return an empty array. 
 
-def range
-
-
+def range(start_range, end_range)
+  return [] if end_range <= start_range
+  range(start_range, end_range - 1) << end_range - 1
 end
