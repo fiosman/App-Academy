ee53a9d1f03a80c36a637eaa9159d10016660014
Fares Osman <fares.osman@outlook.com>
2019-03-07 21:29:35 -0500

    deep_dup start

--- ruby_curriculum/7_recursion_exercises/recursion_exercises.rb
+++ ruby_curriculum/7_recursion_exercises/recursion_exercises.rb
@@ -57,3 +57,9 @@ def second_exponent(base, pwr)
   end
 end
 
+#Using recursion and the is_a? method, write an Array#deep_dup method 
+#that will perform a "deep" duplication of the interior arrays.
+def deep_dup 
+
+end
+
