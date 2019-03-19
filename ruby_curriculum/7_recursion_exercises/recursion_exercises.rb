42740e5c7ef07c3493515b8f3f09fc4e81e83ee6
Fares Osman <fares.osman@outlook.com>
2019-03-19 19:01:23 -0400

    permutations init

--- ruby_curriculum/7_recursion_exercises/recursion_exercises.rb
+++ ruby_curriculum/7_recursion_exercises/recursion_exercises.rb
@@ -154,6 +154,12 @@ def subsets(arr)
 end
 
 
+#Write a recursive method permutations(array) that calculates all the permutations of the given array. 
+#For an array of length n there are n! different permutations. 
+#So for an array with three elements we will have 3 * 2 * 1 = 6 different permutations.
+def permutations(array)
+
+end
 
 
 
