8f9822a71b075023e6bf2a93154286bb472f8224
Fares Osman <fares.osman@outlook.com>
2019-03-24 20:45:25 -0400

    v1 - attempt to fetch all words that are different at only 1 position

--- ruby_curriculum/8_word_chains/word_chains.rb
+++ ruby_curriculum/8_word_chains/word_chains.rb
@@ -1,4 +1,5 @@
 require 'set'
+require 'byebug'
 
 class WordChainer
 
@@ -9,11 +10,28 @@ class WordChainer
   end
 
   def adjacent_words(word)
+    #check for length
     same_length_words = self.dictionary.select { |word_d| word_d.length == word.length }
+    #check for words that are different by only one character
+    #keep a counter, it should be only 1, if higher than 1, do not return the word, if 1, return it 
+    #debugger
+    selected_words = []
+    counter = 0 
+    same_length_words.each do |d_word|
+      (0...d_word.length).each do |idx| 
+        if !word.include?(d_word[idx])
+          counter += 1
+        end
+      end
+    selected_words << d_word if counter == 1 
+    counter = 0
+    end
+    selected_words
   end
 
 end
 
-#word_c = WordChainer.new("dictionary.txt")
-#p word_c.dictionary
+
+word_c = WordChainer.new("dictionary.txt")
+p word_c.adjacent_words('yow')
 
