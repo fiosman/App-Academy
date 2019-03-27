f6bcebd4137965525773445bf48fda24fb52e410
Fares Osman <fares.osman@outlook.com>
2019-03-26 22:34:25 -0400

    Finished phase I

--- ruby_curriculum/8_word_chains/word_chains.rb
+++ ruby_curriculum/8_word_chains/word_chains.rb
@@ -12,27 +12,40 @@ class WordChainer
   def adjacent_words(word)
     #check for length
     same_length_words = self.dictionary.select { |word_d| word_d.length == word.length }
-    #check for words that are different by only one character
-    #keep a counter, it should be only 1, if higher than 1, do not return the word, if 1, return it 
+    #check for words that are different by 1 character at only 1 position
+    #strategy - loop through the chars of the word, then return the index of the letter that is different from the dictionary word. Keep track in a counter
     #debugger
     selected_words = []
-    counter = 0 
-    same_length_words.each do |d_word|
+    diff_counter = 0 
+
+    same_length_words.each do |d_word| 
       (0...d_word.length).each do |idx| 
-        if !word.include?(d_word[idx])
-          counter += 1
+        if d_word[idx] != word[idx]
+          diff_counter += 1
         end
       end
-    selected_words << d_word if counter == 1 
-    counter = 0
+      selected_words << d_word if diff_counter == 1
+      diff_counter = 0 
     end
     selected_words
   end
+
+  #   word_chars.each_with_index do |d_word, idx1|
+  #     (0...d_word.length).each do |idx2| 
+  #       if !word_chars.include?(d_word[idx])
+  #         counter += 1
+  #       end
+  #     end
+  #   selected_words << d_word if counter == 1 
+  #   counter = 0
+  #   end
+  #   selected_words
+  # end
 
 end
 
 
  word_c = WordChainer.new("dictionary.txt")
-# p word_c.adjacent_words('yow')
-p word_c.dictionary
+p word_c.adjacent_words('yow')
+#p word_c.dictionary
 
