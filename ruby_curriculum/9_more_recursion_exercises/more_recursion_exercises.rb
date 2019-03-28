37f6f1a0e1bac1e3a693b0b77b0d8c1ac9a03f24
Fares Osman <fares.osman@outlook.com>
2019-03-27 22:25:02 -0400

    finished includes?

--- ruby_curriculum/9_more_recursion_exercises/more_recursion_exercises.rb
+++ ruby_curriculum/9_more_recursion_exercises/more_recursion_exercises.rb
@@ -5,16 +5,22 @@ def sum_recur(array)
   array.first + sum_recur(array[1..-1])
 end
 
-p sum_recur([1,2,3,4])
+p sum_recur([1,2,3,4]) # => 10
 
 #Problem 2: You have array of integers. Write a recursive solution to determine whether or not the array contains a specific value.
 
 def includes?(array, target)
+  return true if array.first == target 
+  return false if array.empty? 
+  includes?(array[1..-1], target)
 end
+
+p includes?([1,2,3], 4) # => false 
 
 # Problem 3: You have an unsorted array of integers. Write a recursive solution to count the number of occurrences of a specific value.
 
 def num_occur(array, target)
+  
 end
 
 # Problem 4: You have array of integers. Write a recursive solution to determine whether or not two adjacent elements of the array add to 12.
