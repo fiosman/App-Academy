62efa024b6848a9ae0a8d6570d646e3e044258a0
Fares Osman <fares.osman@outlook.com>
2019-03-27 20:01:05 -0400

    First commit

---
+++ ruby_curriculum/9_more_recursion_exercises/more_recursion_exercises.rb
@@ -0,0 +1,29 @@
+#Problem 1: You have array of integers. Write a recursive solution to find the sum of the integers.
+
+def sum_recur(array)
+end
+
+#Problem 2: You have array of integers. Write a recursive solution to determine whether or not the array contains a specific value.
+
+def includes?(array, target)
+end
+
+# Problem 3: You have an unsorted array of integers. Write a recursive solution to count the number of occurrences of a specific value.
+
+def num_occur(array, target)
+end
+
+# Problem 4: You have array of integers. Write a recursive solution to determine whether or not two adjacent elements of the array add to 12.
+
+def add_to_twelve?(array)
+end
+
+# Problem 5: You have array of integers. Write a recursive solution to determine if the array is sorted.
+
+def sorted?(array)
+end
+
+# Problem 6: Write a recursive function to reverse a string. Don't use any built-in #reverse methods!
+
+def reverse(string)
+end
