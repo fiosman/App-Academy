ed6fcb127c756943d20e67b7f2b7ed9c09e1b4a4
Fares Osman <fares.osman@outlook.com>
2019-03-28 22:41:58 -0400

    finished sorted?

--- ruby_curriculum/9_more_recursion_exercises/more_recursion_exercises.rb
+++ ruby_curriculum/9_more_recursion_exercises/more_recursion_exercises.rb
@@ -5,7 +5,7 @@ def sum_recur(array)
   array.first + sum_recur(array[1..-1])
 end
 
-p sum_recur([1,2,3,4]) # => 10
+#p sum_recur([1,2,3,4]) # => 10
 
 #Problem 2: You have array of integers. Write a recursive solution to determine whether or not the array contains a specific value.
 
@@ -15,7 +15,7 @@ def includes?(array, target)
   includes?(array[1..-1], target)
 end
 
-p includes?([1,2,3], 4) # => false 
+#p includes?([1,2,3], 4) # => false 
 
 # Problem 3: You have an unsorted array of integers. Write a recursive solution to count the number of occurrences of a specific value.
 
@@ -26,12 +26,22 @@ end
 # Problem 4: You have array of integers. Write a recursive solution to determine whether or not two adjacent elements of the array add to 12.
 
 def add_to_twelve?(array)
+  return true if array[0] + array[1] == 12 
+  add_to_twelve?(array[0...-1])
 end
+
+#p add_to_twelve?([1,2,3,4,5])
 
 # Problem 5: You have array of integers. Write a recursive solution to determine if the array is sorted.
 
 def sorted?(array)
+  return true if array.size == 1 
+  return false if array.first > array.last
+  sorted?(array[1..-1])
 end
+
+#p sorted?(18, 1, 2, 9, 1101)
+#p sorted?(7, 19, 22, 42, 113)
 
 # Problem 6: Write a recursive function to reverse a string. Don't use any built-in #reverse methods!
 
@@ -40,4 +50,4 @@ def reverse(string)
   string[-1] + reverse(string[0...-1])
 end
 
-p reverse('hello')
+#p reverse('hello')
