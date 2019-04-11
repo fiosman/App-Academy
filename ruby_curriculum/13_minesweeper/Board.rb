e47325a64b08d8e0e8bfb9e087d2a8382e9221b0
Fares Osman <fares.osman@outlook.com>
2019-04-10 21:51:27 -0400

    Dynamic grid rendering

--- ruby_curriculum/13_minesweeper/Board.rb
+++ ruby_curriculum/13_minesweeper/Board.rb
@@ -4,15 +4,40 @@ class Board
   
   attr_accessor :grid
 
-  def initialize
-    @grid = Array.new(9) { Array.new(9) { Tile.new }} 
+  def initialize(grid_size)
+    @grid = Array.new(grid_size) { Array.new(grid_size) { Tile.new }} 
   end
 
-  def render 
+  def render(column_size, row_size)
+    print "  "
+    (0...column_size).each do |column| 
+      print "#{column}" + "  "
+    end
     
-
+    puts "\n"
+    
+    (0...row_size).each do |row|
+      puts "#{row}"
+    end
   end
 
 end
 
 #to-do -> Print out actual grid (i.e. 9x9, each square is a tile and is a bomb or not). 
+# | | | | | | | | |
+#------------------ 
+# | | | | | | | | |
+#------------------ 
+# | | | | | | | | |
+#------------------ 
+# | | | | | | | | |
+#------------------ 
+# | | | | | | | | |
+#------------------ 
+# | | | | | | | | |
+#------------------ 
+# | | | | | | | | |
+#------------------ 
+# | | | | | | | | | 
+#------------------
+# | | | | | | | | | 
