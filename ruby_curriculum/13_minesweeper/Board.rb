1dee0af85a7c9a54c0d2b2367bae2c8d90150db3
Fares Osman <fares.osman@outlook.com>
2019-04-10 20:47:56 -0400

    render init

--- ruby_curriculum/13_minesweeper/Board.rb
+++ ruby_curriculum/13_minesweeper/Board.rb
@@ -8,6 +8,11 @@ class Board
     @grid = Array.new(9) { Array.new(9) { Tile.new }} 
   end
 
+  def render 
+    
+
+  end
+
 end
 
 #to-do -> Print out actual grid (i.e. 9x9, each square is a tile and is a bomb or not). 
