7ebab235782a9026858a82933803989a10f7fc5b
Fares Osman <fares.osman@outlook.com>
2019-04-11 21:49:55 -0400

    UI for grid rendering v1

--- ruby_curriculum/13_minesweeper/Board.rb
+++ ruby_curriculum/13_minesweeper/Board.rb
@@ -10,27 +10,27 @@ class Board
 
   def render(column_size, row_size)
     print "   "
-    (0...column_size).each do |column| 
+    (0...column_size).each do  |column| 
       print "#{column}" + "  "
     end
     
     print "\n"
     
     (0...row_size).each do |row|
+      print "\n"
       print "#{row}" 
-
       (0...column_size).each do |column|
         print "  #{column}"
       end
-
       print "\n"
+
     end
   end
 
 end
 
 board_1 = Board.new(8)
-board_1.render(8,8)
+board_1.render(9,9)
 
 #to-do -> Print out actual grid (i.e. 9x9, each square is a tile and is a bomb or not). 
 # | | | | | | | | |
