3c6fcdba1527f30691501cebf067cd5198ad90d5
Fares Osman <fares.osman@outlook.com>
2019-04-12 23:56:57 -0400

    Added logic to randomly assign a tile to bomb/no-bomb

--- ruby_curriculum/13_minesweeper/Tile.rb
+++ ruby_curriculum/13_minesweeper/Tile.rb
@@ -3,6 +3,18 @@ class Tile
   attr_accessor :value
   def initialize(value)
     @value = value
+    @bomb = nil 
+    @revealed = nil
+    @hidden = true 
   end
 
+  def status? 
+     if self.value == 1 
+       @bomb = true 
+       @revealed = false 
+     else 
+      @bomb = false 
+      @revealed = true
+    end
+  end
 end
