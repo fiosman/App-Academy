f2e2e984d54d639d76564305ad441ba765acaa93
Fares Osman <fares.osman@outlook.com>
2019-04-14 15:12:00 -0400

    Added position parsing

--- ruby_curriculum/13_minesweeper/Player.rb
+++ ruby_curriculum/13_minesweeper/Player.rb
@@ -2,4 +2,19 @@ class Player
   def initialize
     
   end
+
+  def prompt_pos 
+    p "Please enter a position (e.g. 3,4)"
+    response = gets.chomp
+  end
+
+  def parse_pos 
+    new_arr = prompt_pos.split(",").map { |pos| Integer(pos) }
+    #prompt_pos.split.length
+    #new_arr 
+  end
 end
+
+player_1 = Player.new
+#p player_1.prompt_pos
+p player_1.parse_pos
