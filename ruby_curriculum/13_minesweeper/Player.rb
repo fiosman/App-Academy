1436f74d24c0d3890491c76d0c48ec33f4d0b97b
Fares Osman <fares.osman@outlook.com>
2019-04-15 22:30:32 -0400

    Clean class

--- ruby_curriculum/13_minesweeper/Player.rb
+++ ruby_curriculum/13_minesweeper/Player.rb
@@ -3,19 +3,11 @@ class Player
     
   end
 
-  # def prompt_pos 
-  #   p "Please enter a position (e.g. 3,4)"
-  # end
-
   def parse_pos 
     p "Please enter a position (e.g. 3,4)"
     response = gets.chomp
 
-    response.split(",").map { |pos| Integer(pos) }
-    #prompt_pos.split.length
-    #new_arr 
+    response.split(",").map { |pos| Integer(pos) } 
   end
 end
 
-player_1 = Player.new
-#p player_1.prompt_pos
