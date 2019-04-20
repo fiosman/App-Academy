c7e74a310a8a8f5f39b32c635684f1adad7c814c
Fares Osman <fares.osman@outlook.com>
2019-04-19 20:38:43 -0400

    Added case sensitivity to prompt_flag method

--- ruby_curriculum/13_minesweeper/Player.rb
+++ ruby_curriculum/13_minesweeper/Player.rb
@@ -1,8 +1,5 @@
 class Player
-  def initialize
-    
-  end
-
+  
   def parse_pos 
     p "Please enter a position (e.g. 3,4)"
     response = gets.chomp
@@ -12,7 +9,7 @@ class Player
 
   def prompt_flag 
     p "Would you like to flag this position as a mine? (Y/N)"
-    response = gets.chomp 
+    response = gets.chomp.downcase 
   end
 
 end
