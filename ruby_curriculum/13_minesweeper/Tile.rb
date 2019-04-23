c8c6906d15454d21acc7ede706e84bb720c9e08d
Fares Osman <fares.osman@outlook.com>
2019-04-22 20:19:55 -0400

    Change UI symbols

--- ruby_curriculum/13_minesweeper/Tile.rb
+++ ruby_curriculum/13_minesweeper/Tile.rb
@@ -4,23 +4,23 @@ class Tile
 
   def initialize
     @bomb_state = nil
-    @revealed = false
+    @revealed = true
     @ui_val = nil
     @flag_state = nil
   end
 
   def ui_status 
     if self.bomb_state == true && self.revealed == true
-      @ui_val = :*
+      @ui_val = :B
     elsif self.bomb_state == true && self.flag_state == true 
-      @ui_val = :&
+      @ui_val = :F
     else
       @ui_val = :" "
     end
   end
 
   def bomb_status 
-    rand(1..2) == 1 ? @bomb_state = true : @bomb_state = false
+    rand(1..5) == 1 ? @bomb_state = true : @bomb_state = false
   end
 
 end
