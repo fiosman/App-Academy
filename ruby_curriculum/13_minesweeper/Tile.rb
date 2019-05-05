8857b7fdce377e3cfaf9c23d2526a9ca86294f9a
Fares Osman <fares.osman@outlook.com>
2019-05-04 21:30:45 -0400

    Assign position instance variable to class

--- ruby_curriculum/13_minesweeper/Tile.rb
+++ ruby_curriculum/13_minesweeper/Tile.rb
@@ -2,13 +2,14 @@ require 'colorize'
 
 class Tile
 
-  attr_accessor :bomb_state, :ui_val, :revealed, :neighbors
+  attr_accessor :bomb_state, :ui_val, :revealed, :neighbors, :position
 
   def initialize
     @bomb_state = nil
     @revealed = false
     @ui_val = "*".colorize(:blue)
     @neighbors = Array.new
+    @position = nil
   end
 
   #Handle UI values for each game condition
@@ -24,7 +25,7 @@ class Tile
 
   #Randomize bomb cells in the grid (25-30%)
   def bomb_status 
-    rand(1..4) == 1 ? @bomb_state = true : @bomb_state = false
+    rand(1..15) == 1 ? @bomb_state = true : @bomb_state = false
   end
 
   #Generate the number of adjacent bombs
@@ -39,9 +40,3 @@ class Tile
   end
 
 end
-
-#per GeeksforGeeks: 
-#If there are no adjacent mines to this cell, then we recursively click/step on all the safe adjacent cells (hence reducing the time of the game-play). 
-#And if there is atleast a single adjacent mine to this cell then that count is displayed on the current cell. 
-#This is given as a hint to the player so that he can avoid stepping/clicking on the cells having mines by logic.
-#Also if you click on a cell having no adjacent mines (in any of the surrounding eight cells) then all the adjacent cells are automatically cleared, thus saving our time.
