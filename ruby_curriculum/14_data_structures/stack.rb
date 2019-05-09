57d9208c6b141b779e3ae9d4384cc2d998b2e985
Fares Osman <fares.osman@outlook.com>
2019-05-08 21:57:52 -0400

    First commit

---
+++ ruby_curriculum/14_data_structures/stack.rb
@@ -0,0 +1,17 @@
+  class Stack
+    def initialize
+      # create ivar to store stack here!
+    end
+
+    def push(el)
+      # adds an element to the stack
+    end
+
+    def pop
+      # removes one element from the stack
+    end
+
+    def peek
+      # returns, but doesn't remove, the top element in the stack
+    end
+  end
