2e0036a46d4a9e7b66ef7d7ecf1794b81ad685b1
Fares Osman <fares.osman@outlook.com>
2019-05-09 22:33:23 -0400

    Finish get and set methods

--- ruby_curriculum/14_data_structures/map.rb
+++ ruby_curriculum/14_data_structures/map.rb
@@ -1,17 +1,28 @@
 class Map
 
   attr_accessor :ivar 
-  
+
   def initialize
-    @ivar = []
+    @ivar = [["Chris", 1]]
   end
 
   def get(key)
-
+    #get the value of a key, iterate through the array and see if key matches, return value
+    @ivar.each do |sub_arr| 
+      if sub_arr.first == key 
+        return sub_arr.last
+      end
+    end
   end
 
   def set(key, value)
-
+    #first search if the key exists, replace value with new value. Otherwise make new key value pair
+    @ivar.each do |sub_arr| 
+      if sub_arr.include?(key)
+        sub_arr.last = value 
+      end
+    end
+    @ivar << [key, value]
   end
 
   def delete(key)
@@ -23,3 +34,10 @@ class Map
   end
 
 end
+
+map_1 = Map.new
+#p map_1.get("Fares")
+p map_1.ivar
+map_1.set('Fares', 1)
+p map_1.ivar
+p map_1.get('Fares')
