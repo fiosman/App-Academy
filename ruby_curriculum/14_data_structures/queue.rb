c9149d2e69bcb673b1885abd9cf1722cea346daf
Fares Osman <fares.osman@outlook.com>
2019-05-09 20:59:20 -0400

    Finished implementing Queue

--- ruby_curriculum/14_data_structures/queue.rb
+++ ruby_curriculum/14_data_structures/queue.rb
@@ -1,18 +1,22 @@
 class Queue
-  def initialize
+  
+  attr_accessor :ivar 
 
+  def initialize
+    @ivar = []
   end
 
   def enqueue(el)
-
+    @ivar.push(el)
   end
 
   def dequeue
-
+    @ivar.shift
   end
 
   def peek
-
+    @ivar.dup
   end
 
 end
+
