5f5548c381053b1bc733b5445f1af795c64931f6
Fares Osman <fares.osman@outlook.com>
2019-05-09 20:34:36 -0400

    Finished implementing Stack

--- ruby_curriculum/14_data_structures/stack.rb
+++ ruby_curriculum/14_data_structures/stack.rb
@@ -1,17 +1,25 @@
   class Stack
+
+    attr_accessor :ivar
+
     def initialize
       # create ivar to store stack here!
+      @ivar = []
     end
 
     def push(el)
       # adds an element to the stack
+      @ivar.push(el)
     end
 
     def pop
       # removes one element from the stack
+      @ivar.pop
     end
 
     def peek
       # returns, but doesn't remove, the top element in the stack
+      @ivar.dup
     end
+    
   end
