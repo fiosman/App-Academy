d84c3ffb2bcb0589de7832be505f5f48009e8937
Fares Osman <fares.osman@outlook.com>
2019-05-11 23:26:58 -0400

    Finished phase 1

--- ruby_curriculum/16_polytreenode/polytree/lib/00_tree_node.rb
+++ ruby_curriculum/16_polytreenode/polytree/lib/00_tree_node.rb
@@ -1,3 +1,26 @@
-class PolyTreeNode
+class PolyTreeNode #this is just one node in a tree
+
+  def initialize(value)
+    @value = value #returns the value stored at that node
+    @parent = nil #returns the node's parent
+    @children = [] #returns the node's children 
+  end
+
+  def parent 
+    @parent 
+  end
+
+  def children 
+    @children
+  end
+
+  def value 
+    @value 
+  end
+
+  def parent=(node)
+    @parent = node
+    node.children << self unless node.nil?
+  end
 
 end
