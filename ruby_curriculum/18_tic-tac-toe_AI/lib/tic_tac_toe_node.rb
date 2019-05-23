3fd7e164eb544b25819a3fb2cbb523ec0b344eb4
Fares Osman <fares.osman@outlook.com>
2019-05-22 21:53:39 -0400

    Finished children method

--- ruby_curriculum/18_tic-tac-toe_AI/lib/tic_tac_toe_node.rb
+++ ruby_curriculum/18_tic-tac-toe_AI/lib/tic_tac_toe_node.rb
@@ -19,7 +19,22 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    nodes = []
 
+    @board.rows.each_with_index do |row, idx1| 
+      row.each_with_index do |block, idx2| 
+        if block.nil? 
+          updated_board = @board.rows.dup
+          updated_board[idx1][idx2] = @next_mover_mark
+          next_mark = @next_mover_mark == :x ? :o : :x
+          new_node = TicTacToeNode.new(updated_board, next_mark, [idx1,idx2]) 
+          nodes << new_node
+        end
+      end
+    end
+    
+    nodes
   end
+
 end
 
