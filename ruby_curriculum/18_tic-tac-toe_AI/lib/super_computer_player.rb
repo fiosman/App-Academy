b2c5f21b8ecfe701d7943297b96aa9cffd1e995a
Fares Osman <fares.osman@outlook.com>
2019-05-23 20:34:49 -0400

    Finished move method

--- ruby_curriculum/18_tic-tac-toe_AI/lib/super_computer_player.rb
+++ ruby_curriculum/18_tic-tac-toe_AI/lib/super_computer_player.rb
@@ -2,6 +2,12 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+
+    node.children.any? { |child| return child.prev_move_pos if child.winning_node?(mark) }
+    node.children.select { |child| return child.prev_move_pos if !child.losing_node?(mark) }
+    
+    raise 'No moves!' 
   end
 end
 
