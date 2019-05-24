8e7d49d6cf39a25ffdef7f5b13330fd807578e5a
Fares Osman <fares.osman@outlook.com>
2019-05-23 20:42:01 -0400

    Finished Phase I TicTacToeNode

--- ruby_curriculum/18_tic-tac-toe_AI/lib/tic_tac_toe_node.rb
+++ ruby_curriculum/18_tic-tac-toe_AI/lib/tic_tac_toe_node.rb
@@ -11,9 +11,31 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    if @board.over? 
+      return false if @board.winner.nil? || @board.winner == evaluator
+      return true
+    end
+
+    if @next_mover_mark == evaluator 
+      self.children.all? { |child| child.losing_node?(evaluator) }
+    else 
+      self.children.any? { |child| child.losing_node?(evaluator) }
+    end
+
   end
 
   def winning_node?(evaluator)
+    if @board.over? 
+      return true if @board.winner == evaluator 
+      return false 
+    end
+
+    if @next_mover_mark == evaluator 
+      self.children.any? { |child| child.winning_node?(evaluator)}
+    else
+      self.children.all? { |child| child.winning_node?(evaluator)}
+    end
+
   end
 
   # This method generates an array of all moves that can be made after
@@ -21,20 +43,30 @@ class TicTacToeNode
   def children
     nodes = []
 
-    @board.rows.each_with_index do |row, idx1| 
-      row.each_with_index do |block, idx2| 
-        if block.nil? 
-          updated_board = @board.rows.dup
-          updated_board[idx1][idx2] = @next_mover_mark
-          next_mark = @next_mover_mark == :x ? :o : :x
-          new_node = TicTacToeNode.new(updated_board, next_mark, [idx1,idx2]) 
-          nodes << new_node
-        end
-      end
+    self.get_empty_posns.each do |posn| 
+      updated_board = @board.dup
+      updated_board[posn] = @next_mover_mark
+      next_mark = @next_mover_mark == :x ? :o : :x
+      new_node = TicTacToeNode.new(updated_board, next_mark, posn) 
+      nodes << new_node
     end
-    
+
     nodes
   end
 
-end
+  #Helper method to return all empty positions on the board
+  def get_empty_posns 
+    empty_posns = []
 
+    @board.rows.each_with_index do |row, idx1| 
+      row.each_with_index do |block, idx2| 
+        if @board.empty?([idx1, idx2])
+          empty_posns << [idx1, idx2]
+        end
+      end
+    end
+
+    empty_posns
+  end
+
+end
