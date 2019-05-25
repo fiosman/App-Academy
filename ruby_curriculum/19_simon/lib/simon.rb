537cc63d9b79408314be8292d92cdc6b122dbc8b
Fares Osman <fares.osman@outlook.com>
2019-05-25 17:37:27 -0400

    Finish init and add_random_color methods

--- ruby_curriculum/19_simon/lib/simon.rb
+++ ruby_curriculum/19_simon/lib/simon.rb
@@ -4,7 +4,9 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
@@ -24,7 +26,7 @@ class Simon
   end
 
   def add_random_color
-
+    self.seq << COLORS.sample
   end
 
   def round_success_message
