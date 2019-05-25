e51fca979a73f06eef0357dc9c2ef516a62f18ed
Fares Osman <fares.osman@outlook.com>
2019-05-25 18:22:31 -0400

    Finish Simon game

--- ruby_curriculum/19_simon/lib/simon.rb
+++ ruby_curriculum/19_simon/lib/simon.rb
@@ -11,7 +11,8 @@ class Simon
 
   def play
     until game_over 
-      self.take_turn 
+      self.take_turn
+      system('clear') 
     end
 
     self.game_over_message 
@@ -20,6 +21,8 @@ class Simon
 
   def take_turn
     self.show_sequence 
+    sleep(1)
+    system('clear')
     self.require_sequence 
 
     unless @game_over
@@ -29,11 +32,17 @@ class Simon
   end
 
   def show_sequence
-    self.add_random_color
+    p 'Watch the sequence closely!'
+    p self.add_random_color
   end
 
   def require_sequence
-
+    p "Type in each color then press Enter"
+    
+    seq.each do |color| 
+      user_guess = gets.chomp 
+      @game_over = true if user_guess != color 
+    end
   end
 
   def add_random_color
@@ -53,4 +62,9 @@ class Simon
     @game_over = false 
     @seq = []
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  game = Simon.new
+  game.play
 end
