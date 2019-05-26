fbc31619e7201ab4d140b0f098682fd648f93271
Fares Osman <fares.osman@outlook.com>
2019-05-26 18:59:00 -0400

    First commit

---
+++ ruby_curriculum/20_error_handling_funtime/super_useful.rb
@@ -0,0 +1,46 @@
+# PHASE 2
+def convert_to_int(str)
+  Integer(str)
+end
+
+# PHASE 3
+FRUITS = ["apple", "banana", "orange"]
+
+def reaction(maybe_fruit)
+  if FRUITS.include? maybe_fruit
+    puts "OMG, thanks so much for the #{maybe_fruit}!"
+  else 
+    raise StandardError 
+  end 
+end
+
+def feed_me_a_fruit
+  puts "Hello, I am a friendly monster. :)"
+
+  puts "Feed me a fruit! (Enter the name of a fruit:)"
+  maybe_fruit = gets.chomp
+  reaction(maybe_fruit) 
+end  
+
+# PHASE 4
+class BestFriend
+  def initialize(name, yrs_known, fav_pastime)
+    @name = name
+    @yrs_known = yrs_known
+    @fav_pastime = fav_pastime
+  end
+
+  def talk_about_friendship
+    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+  end
+
+  def do_friendstuff
+    puts "Hey bestie, let's go #{@fav_pastime}. Wait, why don't you choose. 😄"
+  end
+
+  def give_friendship_bracelet
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+  end
+end
+
+
---
+++ ruby_curriculum/20_error_handling_funtime/user_script.rb
@@ -0,0 +1,11 @@
+require_relative 'super_useful'
+
+puts "'five' == #{convert_to_int('five')}"
+
+feed_me_a_fruit
+
+sam = BestFriend.new('', 1, '')
+
+sam.talk_about_friendship
+sam.do_friendstuff
+sam.give_friendship_bracelet
