4a78fb2ff5c8584c55119062fd37b779f999a9e6
Fares Osman <fares.osman@outlook.com>
2019-05-26 19:47:45 -0400

    Finished Phase 2 - convert_to_int

--- ruby_curriculum/20_error_handling_funtime/super_useful.rb
+++ ruby_curriculum/20_error_handling_funtime/super_useful.rb
@@ -1,6 +1,9 @@
 # PHASE 2
 def convert_to_int(str)
-  Integer(str)
+    Integer(str)
+  rescue ArgumentError => e
+    p "Cannot convert #{str} to an integer!"
+    p "Error was #{e.message}"
 end
 
 # PHASE 3
