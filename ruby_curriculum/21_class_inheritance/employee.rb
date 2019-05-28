6599a82a8e7ee2fd1583b2c68d20b3e1c5275958
Fares Osman <fares.osman@outlook.com>
2019-05-27 21:25:45 -0400

    Finished setting up instance variables

---
+++ ruby_curriculum/21_class_inheritance/employee.rb
@@ -0,0 +1,12 @@
+class Employee
+  attr_accessor :name, :title, :salary, :boss
+
+  def initialize(name, title, salary, boss)
+    @name = name 
+    @title = title 
+    @salary = salary
+    @boss = boss
+  end
+
+  
+end
