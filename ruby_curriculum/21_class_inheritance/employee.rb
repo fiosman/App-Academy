35cb506eb5c4e8a67001c26ba419137127958e25
Fares Osman <fares.osman@outlook.com>
2019-05-28 21:20:05 -0400

    Added instance variables for Manager, implemented super in initialize method

--- ruby_curriculum/21_class_inheritance/employee.rb
+++ ruby_curriculum/21_class_inheritance/employee.rb
@@ -5,8 +5,18 @@ class Employee
     @name = name 
     @title = title 
     @salary = salary
-    @boss = boss
+    @boss = boss.upcase
   end
 
+end
+
+class Manager < Employee
+  def initialize(name, title, salary, boss)
+    super(name, title, salary, boss)
+    @employees = []
+  end
   
 end
+
+#p manager_1 = Manager.new('f', 't', 'h', 'g')
+#p manager_1.name
