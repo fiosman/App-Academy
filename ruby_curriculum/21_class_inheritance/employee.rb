935d70a89be5a9512b3130c028810c4ada502480
Fares Osman <fares.osman@outlook.com>
2019-05-29 20:51:11 -0400

    Finished implementing bonus salary, adding employees to Manager

--- ruby_curriculum/21_class_inheritance/employee.rb
+++ ruby_curriculum/21_class_inheritance/employee.rb
@@ -5,18 +5,28 @@ class Employee
     @name = name 
     @title = title 
     @salary = salary
-    @boss = boss.upcase
+    @boss = boss
+  end
+
+  def boss=(boss)
+    @boss = boss
+    boss.add_emp(self) unless boss.nil? 
+  end
+
+  def bonus(multiplier)
+    @salary * multiplier
   end
 
 end
 
 class Manager < Employee
   def initialize(name, title, salary, boss)
-    super
+    super 
     @employees = []
   end
   
+  def add_emp(employee) 
+    @employees << employee
+  end
 end
 
-#manager_1 = Manager.new('f', 't', 'h', 'g')
-#p manager_1.name
