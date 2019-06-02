e407f212e3008130917e5e801961b66d0fe9882f
Fares Osman <fares.osman@outlook.com>
2019-06-01 21:56:43 -0400

    Populate board with piece instances

--- ruby_curriculum/22_chess_part_one/Board.rb
+++ ruby_curriculum/22_chess_part_one/Board.rb
@@ -1,10 +1,32 @@
+require_relative 'piece.rb'
+
 class Board
-  def initialize(rows)
+  def initialize
     @rows = Array.new(8) { Array.new (8) }
-    
+    #populate_board
   end
 
   def move_piece(start_pos, end_pos)
 
   end
+
+  def populate_board 
+    @rows.each do |row| 
+      row.each do |col| 
+        col = Piece.new
+      end
+    end
+    
+    @rows
+  end
+
+  def [](pos)
+
+  end
+
+  def pos=(pos, val)
+
+  end
 end
+
+
