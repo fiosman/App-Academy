6ac180ca519f182d0f29d96b27cace8558571e00
Fares Osman <fares.osman@outlook.com>
2019-06-04 19:30:14 -0400

    Set up instance variables for Display class

--- ruby_curriculum/22_chess_part_one/display.rb
+++ ruby_curriculum/22_chess_part_one/display.rb
@@ -1,8 +1,12 @@
 require 'colorize'
+require_relative 'cursor'
 require_relative 'board'
 
 class Display
-  def initialize
-    
+  attr_reader :cursor, :board
+
+  def initialize(board)
+    @board = board
+    @cursor = Cursor.new([0,0], board)
   end
 end
