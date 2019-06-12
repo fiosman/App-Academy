06c3e96370df67283ac9cbcd51487c69c17c3911
Fares Osman <fares.osman@outlook.com>
2019-06-12 12:47:26 -0400

    Start adding the pieces to chess board -- requiring file paths

--- ruby_curriculum/22_chess_part_one/pieces.rb
+++ ruby_curriculum/22_chess_part_one/pieces.rb
@@ -3,3 +3,7 @@ require_relative './pieces/null_piece'
 require_relative './pieces/knight'
 require_relative './pieces/queen'
 require_relative './pieces/king'
+require_relative './pieces/pawn'
+require_relative './pieces/rook'
+require_relative './pieces/bishop'
+
