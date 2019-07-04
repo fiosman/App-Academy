26361f7bf32c1f571a569d7d77394370db02da3a
Fares Osman <fares.osman@outlook.com>
2019-07-03 23:00:01 -0400

    Add file paths

--- ruby_curriculum/22_chess/pieces/bishop.rb
+++ ruby_curriculum/22_chess/pieces/bishop.rb
@@ -1,4 +1,5 @@
 require_relative 'piece'
+require_relative './modules/slideable'
 require 'colorize'
 
 class Bishop < Piece 
--- ruby_curriculum/22_chess/pieces/knight.rb
+++ ruby_curriculum/22_chess/pieces/knight.rb
@@ -1,4 +1,5 @@
 require_relative 'piece'
+require_relative './modules/stepable'
 require 'colorize'
 
 class Knight < Piece 
