777d253ced668bd2c695b027a5b97ce0a66207de
Fares Osman <fares.osman@outlook.com>
2019-07-05 12:36:25 -0400

    Add directions for sliding pieces

--- ruby_curriculum/22_chess/pieces/modules/slideable.rb
+++ ruby_curriculum/22_chess/pieces/modules/slideable.rb
@@ -1,2 +1,17 @@
 module Slideable 
+
+  VERTICAL_AND_HORIZONTAL_DIRS = [
+    [1, 0], 
+    [-1, 0], 
+    [0, 1], 
+    [0, -1]
+  ]
+
+  DIAGONAL_DIRS = [ 
+    [1, 1], 
+    [-1, 1], 
+    [1, -1],
+    [-1, -1]
+  ]
+  
 end
