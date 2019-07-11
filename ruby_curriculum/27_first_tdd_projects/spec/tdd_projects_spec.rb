dbf2c14bf5ed016d1e1ef93fde99c2ba6ad2aef6
Fares Osman <fares.osman@outlook.com>
2019-07-10 23:03:02 -0400

    Pending specs

--- ruby_curriculum/27_first_tdd_projects/spec/tdd_projects_spec.rb
+++ ruby_curriculum/27_first_tdd_projects/spec/tdd_projects_spec.rb
@@ -1,1 +1,12 @@
+require 'rspec'
 require 'tdd_projects'
+
+describe Array do 
+  subject(:non_uniq_arr) { Array.new([1,2,1,3,3]) }
+  subject(:two_sum_arr) { Array.new([-1, 0, 2, -2, 1]) }
+
+  describe "#my_uniq" do 
+    it "removes duplicate elements"
+    it "receives an Array instance"
+  end
+end
