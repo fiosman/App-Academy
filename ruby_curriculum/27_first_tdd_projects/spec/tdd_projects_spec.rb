254bbe71868927d2bbd70d0c4e95a370f20290fe
Fares Osman <fares.osman@outlook.com>
2019-07-12 22:27:11 -0400

    First unit test added

--- ruby_curriculum/27_first_tdd_projects/spec/tdd_projects_spec.rb
+++ ruby_curriculum/27_first_tdd_projects/spec/tdd_projects_spec.rb
@@ -3,10 +3,11 @@ require 'tdd_projects'
 
 describe Array do 
   subject(:non_uniq_arr) { Array.new([1,2,1,3,3]) }
-  subject(:two_sum_arr) { Array.new([-1, 0, 2, -2, 1]) }
+  subject(:two_sum_arr) { Array.new([-1,0,2,-2,1]) }
 
   describe "#my_uniq" do 
-    it "receives an Array instance"
-    it "returns an array with duplicate elements removed"
+    it 'removes duplicate elements' do 
+      expect(non_uniq_arr.my_uniq).to eq([1,2,3])
+    end
   end
 end
