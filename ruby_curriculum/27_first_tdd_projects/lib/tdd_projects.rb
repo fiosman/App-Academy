098c86bb1cd9f9bd87888504b6c6485925055eb7
Fares Osman <fares.osman@outlook.com>
2019-07-13 17:44:21 -0400

    Almost have working specs. Issues with ordering

--- ruby_curriculum/27_first_tdd_projects/lib/tdd_projects.rb
+++ ruby_curriculum/27_first_tdd_projects/lib/tdd_projects.rb
@@ -1,4 +1,4 @@
-#require 'byebug'
+require 'set'
 
 class Array 
   
@@ -8,7 +8,18 @@ class Array
     uniq_arr
   end
 
-  def two_sum  
+  def two_sum    
+    set = Set.new
+    sum_pairs = []
+
+    self.each do |current_num| 
+      target_num = (0-current_num)
+      if set.include?(target_num)
+        sum_pairs << [self.index(target_num), self.index(current_num)]
+      end
+      set << current_num
+    end
+    sum_pairs
   end
 
   def my_transpose 
@@ -19,3 +30,5 @@ class Array
 
 end
 
+arr = [-1, 0, 2, -2, 1]
+p arr.two_sum
