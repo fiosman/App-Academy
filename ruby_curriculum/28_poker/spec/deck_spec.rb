8050508366c8f5386eecee51c9cfb31ee0c1a010
Fares <fares.osman@outlook.com>
2019-08-02 13:09:40 -0400

    Add #initialize spec

--- ruby_curriculum/28_poker/spec/deck_spec.rb
+++ ruby_curriculum/28_poker/spec/deck_spec.rb
@@ -2,10 +2,11 @@ require 'rspec'
 require 'deck'
 
 describe Deck do
+  subject(:sample_deck) { Deck.new }
 
   describe "#initialize" do
     it 'sets up a deck of five cards' do 
-
+      expect(sample_deck.deck.count).to eq(5)
     end
   end
 end
