93f2f563dc5b983837de9499bf84724300b94ece
Fares <fares.osman@outlook.com>
2019-08-03 16:11:23 -0400

    Populate a random card of a random suit

--- ruby_curriculum/28_poker/lib/card.rb
+++ ruby_curriculum/28_poker/lib/card.rb
@@ -1,5 +1,15 @@
 class Card
-  def initialize
+  attr_reader :value
 
+  def self.cards 
+    [:ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king]
+  end
+
+  def self.suits 
+    [:spades, :diamonds, :clubs, :hearts ]
+  end
+
+  def initialize
+    @value = { Card.suits.sample => Card.cards.sample }
   end
 end
--- ruby_curriculum/28_poker/spec/card_spec.rb
+++ ruby_curriculum/28_poker/spec/card_spec.rb
@@ -1,2 +1,14 @@
 require 'rspec'
 require 'card'
+
+describe Card do 
+  subject(:card) { Card.new }
+
+  describe "#initialize" do 
+    it 'assigns a random card of a certain suit from the deck' do 
+      expect(Card.cards).to include(card.value.values.join.to_i)
+      expect(Card.suits).to include(card.value.keys.first)
+    end
+  end
+
+end
