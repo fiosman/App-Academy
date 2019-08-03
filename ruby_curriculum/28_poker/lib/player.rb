54b0c9f56d8044b5461b8d1d6480384878f80138
Fares <fares.osman@outlook.com>
2019-08-02 22:42:17 -0400

    Set up deck for each player

--- ruby_curriculum/28_poker/lib/player.rb
+++ ruby_curriculum/28_poker/lib/player.rb
@@ -1,8 +1,12 @@
+require_relative 'deck'
+
 class Player
+  attr_reader :deck 
   attr_accessor :chips 
 
   def initialize  
     @chips = 100
+    @deck = Deck.new
   end
 
 end
--- ruby_curriculum/28_poker/spec/player_spec.rb
+++ ruby_curriculum/28_poker/spec/player_spec.rb
@@ -3,10 +3,14 @@ require 'player'
 
 describe Player do 
   subject(:player) { Player.new }
+
   describe "#initialize" do 
     it 'provides a player with 100 chips' do 
       expect(player.chips).to eq(100)
     end
+    it 'provides a player with a deck of cards' do 
+      expect(player.deck.deck.all? { |card| card.is_a?(Card) }).to be_truthy
+    end 
   end
 
 end
