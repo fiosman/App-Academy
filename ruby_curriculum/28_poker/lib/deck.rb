6dc896f1fdd34fa300f30c69ad56f30cd48227cf
Fares <fares.osman@outlook.com>
2019-08-04 23:32:05 -0400

    add #populate_deck, #shuffle_deck, ::ranks, ::suits

--- ruby_curriculum/28_poker/lib/deck.rb
+++ ruby_curriculum/28_poker/lib/deck.rb
@@ -3,8 +3,24 @@ require_relative 'card'
 class Deck
   attr_reader :deck 
 
+  def self.ranks
+    [:ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king]
+  end
+
+  def self.suits 
+    [:spades, :diamonds, :clubs, :hearts]
+  end
+
   def initialize
-    @deck = Array.new(5) { Card.new }
+    @deck = populate_deck
+  end
+
+  def populate_deck
+    Deck.suits.map { |suit| Deck.ranks.map { |rank| Card.new(rank, suit) } }.flatten
+  end
+
+  def shuffle_deck
+    @deck.shuffle! 
   end
 
 end
