fae1119a376dfeb746f633d6ed3fc2c8b7d5cdc7
Fares <fares.osman@outlook.com>
2019-08-05 22:25:43 -0400

    Pending specs for #fold, #raise, #call. Added specs for getting name, and modifying chip count during game

--- ruby_curriculum/28_poker/spec/player_spec.rb
+++ ruby_curriculum/28_poker/spec/player_spec.rb
@@ -2,15 +2,31 @@ require 'rspec'
 require 'player'
 
 describe Player do 
-  subject(:player) { Player.new }
+  subject(:player) { Player.new('Fares') }
 
   describe "#initialize" do 
     it 'provides a player with 100 chips' do 
       expect(player.chips).to eq(100)
     end
-    it 'provides a player with a deck of cards' do 
-      expect(player.deck.deck.all? { |card| card.is_a?(Card) }).to be_truthy
-    end 
+    it 'sets a player name' do 
+      expect(player.name).to eq('Fares')
+    end
+    it 'modifies the chip amount throughout the game' do 
+      player.chips -= 10 
+      expect(player.chips).to eq(90)
+    end
+  end
+
+  describe "#fold" do 
+    it 'allows player to fold' 
+  end
+
+  describe "#raise" do 
+    it 'allows a player to raise'
+  end
+
+  describe "#call" do 
+    it 'allows player to call' 
   end
 
 end
