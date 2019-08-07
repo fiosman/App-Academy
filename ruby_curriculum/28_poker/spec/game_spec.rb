bb31774a05e9eae02f4f03970a3e58f0f182d043
Fares <fares.osman@outlook.com>
2019-08-06 22:38:26 -0400

    Write specs for initializing Deck

--- ruby_curriculum/28_poker/spec/game_spec.rb
+++ ruby_curriculum/28_poker/spec/game_spec.rb
@@ -1,2 +1,14 @@
 require 'rspec'
 require 'game'
+
+describe Game do 
+  subject(:game) { Game.new }
+
+  describe "#initialize" do 
+    it 'sets a deck of cards' do 
+      deck = Deck.new
+      expect(game.deck).to eq(deck)
+    end
+  end
+
+end
