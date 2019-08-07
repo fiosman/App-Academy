b90b4935ce01ace0fcc34d284b6345987a2715f8
Fares <fares.osman@outlook.com>
2019-08-06 23:12:23 -0400

    Hash implementation of player ID

--- ruby_curriculum/28_poker/lib/player.rb
+++ ruby_curriculum/28_poker/lib/player.rb
@@ -1,12 +1,13 @@
 require_relative 'deck'
 
 class Player
-  attr_reader :name, :hand
+  attr_reader :name, :hand, :id
   attr_accessor :chips 
 
   def initialize(name, hand)
     @chips = 100
     @name, @hand = name, hand 
+    @id = { @name => [@hand, @chips] }
   end
 
 end
