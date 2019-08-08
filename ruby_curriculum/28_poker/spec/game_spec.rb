110c6f538f1323e037389043d529d0af2aee0117
Fares <fares.osman@outlook.com>
2019-08-07 22:30:39 -0400

    Add specs for assigning @players

--- ruby_curriculum/28_poker/spec/game_spec.rb
+++ ruby_curriculum/28_poker/spec/game_spec.rb
@@ -2,13 +2,25 @@ require 'rspec'
 require 'game'
 
 describe Game do 
-  subject(:game) { Game.new }
+  
+  let(:fares_hand) { [[:ace, :spades], [3, :diamonds],
+               [4, :spades], [:king, :hearts], 
+               [:ace, :clubs]] }
+  let(:chris_hand) { [[:ace, :spades], [3, :diamonds],
+               [4, :spades], [:king, :hearts], 
+               [:ace, :clubs]] }
+  let(:competitors) { [{'Fares' => [fares_hand, 100]}, 
+                       {'Chris' => [chris_hand, 100]}] 
+                    }
+  subject(:game) { Game.new(Player.new('Fares', fares_hand), Player.new('Chris', chris_hand)) }
 
   describe "#initialize" do 
     it 'sets a deck of cards' do  
       expect(game.deck.is_a?(Deck)).to be_truthy
     end
-    it 'should take in players and return an array of their IDs'
+    it 'should take in players and return an array of their IDs' do 
+      expect(game.players).to eq(competitors)
+    end
   end
 
 end
