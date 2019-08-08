20e296b3d7a3ef4f99f443bc5d996bc57ca3cbb4
Fares <fares.osman@outlook.com>
2019-08-07 22:00:13 -0400

    Implement #generate_ID

--- ruby_curriculum/28_poker/lib/player.rb
+++ ruby_curriculum/28_poker/lib/player.rb
@@ -1,13 +1,16 @@
 require_relative 'deck'
 
 class Player
-  attr_reader :name, :hand, :id
+  attr_reader :name, :hand
   attr_accessor :chips 
 
   def initialize(name, hand)
     @chips = 100
     @name, @hand = name, hand 
-    @id = { @name => [@hand, @chips] }
+  end
+
+  def generate_ID 
+    { @name => [@hand, @chips] }
   end
 
 end
