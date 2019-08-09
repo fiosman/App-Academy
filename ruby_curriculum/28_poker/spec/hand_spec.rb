cff8e18e1e52d6cd7075a3e52767edf7a3166222
Fares <fares.osman@outlook.com>
2019-08-08 20:46:17 -0400

    Add specs for setting deck

--- ruby_curriculum/28_poker/spec/hand_spec.rb
+++ ruby_curriculum/28_poker/spec/hand_spec.rb
@@ -1,2 +1,16 @@
 require 'rspec'
 require 'hand'
+require 'deck'
+
+describe Hand do 
+  let(:deck) { Deck.new }
+  subject(:hand) { Hand.new(deck) }
+
+  describe "#initialize" do 
+    it 'has a deck from which cards are picked' do
+      expect(hand.deck).to eq(deck)
+    end
+  end
+
+end
+
