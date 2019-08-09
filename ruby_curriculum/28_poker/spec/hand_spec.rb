874d91d9c9c04d9ba90c421f89f8f50af6799f94
Fares <fares.osman@outlook.com>
2019-08-08 21:45:22 -0400

    Add hand getter specs

--- ruby_curriculum/28_poker/spec/hand_spec.rb
+++ ruby_curriculum/28_poker/spec/hand_spec.rb
@@ -3,5 +3,15 @@ require 'hand'
 require 'deck'
 
 describe Hand do 
+  let(:sample_hand) { [[:ace, :spades], [3, :diamonds],
+                      [4, :spades], [:king, :hearts], 
+                      [:ace, :clubs]] }
+  subject(:hand) { Hand.new(sample_hand) }
+
+  describe "#initialize" do 
+    it 'takes in a hand' do 
+      expect(hand.hand).to eq(sample_hand)
+    end
+  end
 end
 
