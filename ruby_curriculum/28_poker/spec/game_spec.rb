3eb52607b8ebf4fe26896386b25757b107ee5954
Fares <fares.osman@outlook.com>
2019-08-10 01:10:38 -0400

    Modify specs for #initialize

--- ruby_curriculum/28_poker/spec/game_spec.rb
+++ ruby_curriculum/28_poker/spec/game_spec.rb
@@ -2,29 +2,29 @@ require 'rspec'
 require 'game'
 
 describe Game do 
-  
-  let(:fares_hand) { [[:ace, :spades], [3, :diamonds],
-               [4, :spades], [:king, :hearts], 
-               [:ace, :clubs]] }
-  let(:chris_hand) { [[:ace, :spades], [3, :diamonds],
-               [4, :spades], [:king, :hearts], 
-               [:ace, :clubs]] }
-  let(:competitors) { [{'Fares' => [fares_hand, 100]}, 
-                       {'Chris' => [chris_hand, 100]}] 
-                    }
-  subject(:game) { Game.new(Player.new('Fares', fares_hand), Player.new('Chris', chris_hand)) }
+
+  let(:chris) { Player.new('Chris') }
+  let(:andrew) { Player.new('Andrew') }
+  let(:fares) { Player.new('Fares') }
+  let(:deck) { Deck.new }
+  subject(:game) { Game.new(chris, andrew, fares) }
 
   describe "#initialize" do 
     it 'sets a deck of cards' do  
-      expect(game.deck.is_a?(Deck)).to be_truthy
+      expect(game.deck.all? { |card| card.is_a?(Card) }).to be_truthy
     end
-    it 'should take in players and return an array of their IDs' do 
-      expect(game.players).to eq(competitors)
+    it 'sets up players for the game' do 
+      expect(game.players[0].name).to eq('Chris')
+      expect(game.players[1].name).to eq('Andrew')
+      expect(game.players[2].name).to eq('Fares')
     end
   end
 
   describe "#deal_cards" do 
-    it 'assigns 5 cards to each player'
+    it 'assigns 5 cards to each player' do 
+      expect(game.players)
+    end
+
   end
 
 end
