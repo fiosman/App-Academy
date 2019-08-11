f4c8a0323039cc0c2e68f335e99d1da5360f9116
Fares <fares.osman@outlook.com>
2019-08-11 16:19:54 -0400

    Incorporate hand class modifications, add pot to initialize method

--- ruby_curriculum/28_poker/lib/game.rb
+++ ruby_curriculum/28_poker/lib/game.rb
@@ -1,19 +1,22 @@
 require_relative 'deck'
 require_relative 'player'
+require_relative 'hand'
 
 class Game
   attr_reader :deck, :players
+  attr_accessor :pot
 
   def initialize(*players)
     @deck = Deck.new.shuffle_deck
     @players = players
+    @pot = 0
   end
 
   def deal_cards
-    until @players.all? { |player| player.hand.length == 5 } 
+    until @players.all? { |player| player.hand.cards.length == 5 } 
       @players.each do |player|
         dealt_card = deck.pop
-        player.hand << dealt_card
+        player.hand.cards << dealt_card
       end
     end
   end
