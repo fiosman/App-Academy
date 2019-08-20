5b37a0393b8fe6defec926d16b262a04f0e43886
Fares <fares.osman@outlook.com>
2019-08-19 20:00:26 -0400

    Implemented O(n log n) version

--- ruby_curriculum/29_octopus_problems/octopus_exercises.rb
+++ ruby_curriculum/29_octopus_problems/octopus_exercises.rb
@@ -14,4 +14,49 @@ def sluggish_octopus(fish_arr)
   longest
 end
 
-def 
+def merge(left_arr, right_arr) 
+  merged_arr = []
+  left_arr_idx, right_arr_idx = 0, 0
+
+  while left_arr_idx < left_arr.length && right_arr_idx < right_arr.length 
+    if left_arr[left_arr_idx].length <= right_arr[right_arr_idx].length
+      merged_arr << left_arr[left_arr_idx] 
+      left_arr_idx += 1 
+    else
+      merged_arr << right_arr[right_arr_idx]
+      right_arr_idx += 1
+    end 
+  end
+
+  while left_arr_idx < left_arr.length 
+    merged_arr << left_arr[left_arr_idx] 
+    left_arr_idx += 1
+  end
+
+  while right_arr_idx < right_arr.length 
+    merged_arr << right_arr[right_arr_idx]
+    right_arr_idx += 1
+  end
+
+  merged_arr
+end
+
+def merge_sort(arr)
+  return arr if arr.length <= 1 
+
+
+  mid = arr.length / 2 
+  right_arr = arr[mid..-1]
+  left_arr = arr[0...mid]
+
+  sorted_right_arr = merge_sort(right_arr) 
+  sorted_left_arr = merge_sort(left_arr) 
+
+  merge(sorted_left_arr, sorted_right_arr)
+end
+
+#O(n log n) time
+def dominant_octopus(fish_arr) 
+  sorted_fish_arr = merge_sort(fish_arr) 
+  return sorted_fish_arr[-1]
+end
