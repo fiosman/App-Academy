f363522d1e49eeca402c414137b5af372a527bd4
Fares <fares.osman@outlook.com>
2019-08-20 20:39:39 -0400

    Implement O(n^3) version of #largest_contiguous_subsum

--- ruby_curriculum/31_execution_time_difference/execution_time.rb
+++ ruby_curriculum/31_execution_time_difference/execution_time.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 #O(n) time 
 def my_min(arr)
   min = arr.first 
@@ -8,3 +10,14 @@ def my_min(arr)
   return min
 end
 
+#O(n^3) time
+def largest_contiguous_subsum(list)
+  sub_arrs = []
+
+  list.each_index do |idx1| 
+    (idx1..list.length-1).each do |idx2| 
+      sub_arrs << list[idx1..idx2]
+    end
+  end
+  sub_arrs.map(&:sum).max
+end
