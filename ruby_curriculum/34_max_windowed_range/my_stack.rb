a3ba5278b64659205d60444a487312510c699f88
Fares <fares.osman@outlook.com>
2019-08-22 20:38:56 -0400

    Finish implementing MyStack

---
+++ ruby_curriculum/34_max_windowed_range/my_stack.rb
@@ -0,0 +1,27 @@
+class MyStack
+
+  def initialize
+    @store = []
+  end
+
+  def peek 
+    @store[-1]
+  end
+
+  def size
+    @store.size
+  end
+
+  def empty? 
+    @store.empty? 
+  end
+
+  def pop 
+    @store.pop
+  end
+  
+  def push(ele)
+    @store.push(ele)
+  end
+
+end
