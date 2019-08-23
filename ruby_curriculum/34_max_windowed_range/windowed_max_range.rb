c2bbce2fd15ae15856eb2dffe82ddbb4e9eaf703
Fares <fares.osman@outlook.com>
2019-08-23 19:03:25 -0400

    Finish exercise

--- ruby_curriculum/34_max_windowed_range/windowed_max_range.rb
+++ ruby_curriculum/34_max_windowed_range/windowed_max_range.rb
@@ -1,3 +1,5 @@
+require_relative "min_max_stack_queue"
+
 #O(a * b) time 
 #O(n) space === dependant on window size 
 
@@ -14,3 +16,19 @@ def first_windowed_max_range(arr, window_size)
 end
 
 
+def second_windowed_max_range(arr, window_size) 
+  queue = MinMaxStackQueue.new 
+  max_range = 0 
+
+  arr.each_index do |num_index| 
+    queue.enqueue(arr[idx])
+    queue.dequeue if queue.size > window_size
+
+    if queue.size == window_size 
+      current_max = queue.max - queue.min 
+      max_range = current_max if current_max > max_range
+    end
+  end
+
+  max_range
+end
