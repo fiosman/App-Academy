a28fd5ba21e2b78f6d2d90550c2ce2e7c20b57e4
Fares <fares.osman@outlook.com>
2019-08-23 16:41:04 -0400

    Added #max and #min in O(1) time.

--- ruby_curriculum/34_max_windowed_range/my_stack.rb
+++ ruby_curriculum/34_max_windowed_range/my_stack.rb
@@ -2,9 +2,10 @@ class MyStack
 
   def initialize
     @store = []
+    @max_stack = []
   end
 
-  def peek 
+  def peek
     @store[-1]
   end
 
@@ -12,16 +13,32 @@ class MyStack
     @store.size
   end
 
-  def empty? 
+  def empty?
     @store.empty? 
   end
 
-  def pop 
+  def pop
     @store.pop
+    @max_stack.pop
   end
   
   def push(ele)
+    max = ele 
+    
+    if (!@max_stack.empty? && max < @max_stack[-1])
+      max = @max_stack[-1]
+    end
+
     @store.push(ele)
+    @max_stack.push(max)
+  end
+
+  def max 
+    @max_stack[-1]
+  end
+
+  def min 
+    @max_stack[0]
   end
 
 end
