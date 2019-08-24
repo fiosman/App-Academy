fcec5281d7eddd6e11383f5a18ab83ba9b44ba97
Fares <fares.osman@outlook.com>
2019-08-23 20:28:27 -0400

    Finish benchmarking

--- ruby_curriculum/35_benchmarking/benchmarking.rb
+++ ruby_curriculum/35_benchmarking/benchmarking.rb
@@ -1,2 +1,9 @@
 require 'benchmark'
+require_relative 'execution_time'
 
+arr = (1..10000).map { rand(100) } 
+
+Benchmark.bmbm do |benchmark| 
+  benchmark.report("O(n^3):" "#{arr.size} elements") { largest_contiguous_subsum(arr) }
+  benchmark.report("O(n):" "#{arr.size} elements") { better_largest_contiguous_subsum(arr) } 
+end
