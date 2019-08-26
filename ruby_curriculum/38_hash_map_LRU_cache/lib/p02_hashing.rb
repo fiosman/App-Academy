f650596aea6751bc8631e68674f373af92db1fa5
Fares <fares.osman@outlook.com>
2019-08-26 12:58:25 -0400

    Finish Hashing

--- ruby_curriculum/38_hash_map_LRU_cache/lib/p02_hashing.rb
+++ ruby_curriculum/38_hash_map_LRU_cache/lib/p02_hashing.rb
@@ -4,18 +4,34 @@ end
 
 class Array
   def hash
+    res = 0
+    self.each_with_index do |ele, idx|
+      res += ele.hash ^ idx 
+    end
+    
+    res.hash
   end
 end
 
 class String
   def hash
+    alphabet = ('a'..'z').to_a 
+    integer_string = ""
+    self.downcase.each_char do |char| 
+      integer_string += alphabet.index(char).to_s
+    end
+    
+    integer_string.to_i.hash
   end
 end
 
 class Hash
-  # This returns 0 because rspec will break if it returns nil
-  # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    res = 0 
+    self.to_a.flatten.each do |ele| 
+      res += ele.hash
+    end
+
+    res
   end
 end
