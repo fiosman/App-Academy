a76e7f44799a9debeef13f758bdd044b3ebb64b5
Fares <fares.osman@outlook.com>
2019-08-26 14:15:18 -0400

    Finish HashSet

--- ruby_curriculum/38_hash_map_LRU_cache/lib/p03_hash_set.rb
+++ ruby_curriculum/38_hash_map_LRU_cache/lib/p03_hash_set.rb
@@ -1,5 +1,5 @@
 class HashSet
-  attr_reader :count
+  attr_accessor :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,24 +7,38 @@ class HashSet
   end
 
   def insert(key)
+    bucket = key.hash % num_buckets
+    self.store[bucket] << key.hash 
+    self.count += 1 
+
+    if num_buckets < self.count 
+      resize!
+    end
   end
 
   def include?(key)
+    bucket = self.store[key.hash % num_buckets]
+    bucket.include?(key.hash)
   end
 
   def remove(key)
+    if self.include?(key)
+      bucket = self.store[key.hash % num_buckets] 
+      bucket.delete(key.hash)
+      self.count -= 1
+    end
   end
 
   private
 
-  def [](num)
-    # optional but useful; return the bucket corresponding to `num`
-  end
-
   def num_buckets
-    @store.length
+    self.store.length
   end
 
   def resize!
+    old_store = @store 
+    self.count = 0 
+    self.store = Array.new(old_store.length * 2) { Array.new }
+    old_store.flatten.each { |ele| insert(ele) }
   end
 end
