56fb6ce892791c8c3f8da63ab5b7e9b85b1ec32b
Fares <fares.osman@outlook.com>
2019-08-26 21:50:11 -0400

    Finish LRU_Cache

--- ruby_curriculum/38_hash_map_LRU_cache/lib/p06_lru_cache.rb
+++ ruby_curriculum/38_hash_map_LRU_cache/lib/p06_lru_cache.rb
@@ -2,6 +2,8 @@ require_relative 'p05_hash_map'
 require_relative 'p04_linked_list'
 
 class LRUCache
+  attr_reader :store, :count, :prc, :map 
+
   def initialize(max, prc)
     @map = HashMap.new
     @store = LinkedList.new
@@ -14,6 +16,13 @@ class LRUCache
   end
 
   def get(key)
+    if self.map.include?(key)
+      update_node!(key)
+    else
+      eject! if count >= @max
+      calc!(key)
+    end
+    @map[key].val
   end
 
   def to_s
@@ -24,12 +33,21 @@ class LRUCache
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    val = self.prc.call(key)
+    node = self.store.append(key, val)
+    self.map[key] = node
   end
 
   def update_node!(node)
     # suggested helper method; move a node to the end of the list
+    node = @map.get(node)
+    self.store.remove(node.key)
+    self.store.append(node.key, node.val)
   end
 
   def eject!
+    lru_key = self.store.first.key 
+    self.store.remove(lru_key)
+    self.map.delete(lru_key)
   end
 end
