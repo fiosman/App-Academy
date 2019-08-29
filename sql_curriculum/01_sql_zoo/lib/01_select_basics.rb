16e6f781d3073a1bd30e696c91da0b3a14617157
Fares <fares.osman@outlook.com>
2019-08-28 21:57:54 -0400

    First commit

--- sql_curriculum/01_sql_zoo/lib/01_select_basics.rb
+++ sql_curriculum/01_sql_zoo/lib/01_select_basics.rb
@@ -35,8 +35,8 @@ end
 def per_capita_gdp
   # Show the name and per capita gdp (gdp/population) for each country where
   # the area is over 5,000,000 km^2
-  execute(<<-SQL)
-  SQL
+  # execute(<<-SQL)
+  # SQL
 end
 
 def small_and_wealthy
