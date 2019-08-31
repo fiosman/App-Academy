ad9100edb30f01fc219e7bc363f5705fa9086251
Fares <fares.osman@outlook.com>
2019-08-31 01:28:19 -0400

    Finish #united_we_stand

--- sql_curriculum/01_sql_zoo/lib/02_select_countries.rb
+++ sql_curriculum/01_sql_zoo/lib/02_select_countries.rb
@@ -76,5 +76,11 @@ end
 def united_we_stand
   # Show the countries that have a name that includes the word 'United'
   execute(<<-SQL)
+    SELECT 
+      name 
+    FROM 
+      countries 
+    WHERE 
+      name LIKE '%United%'
   SQL
 end
