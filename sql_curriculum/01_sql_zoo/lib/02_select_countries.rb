8b537b28d51f36c64cd1add446772e4ccd24b872
Fares <fares.osman@outlook.com>
2019-08-31 01:25:07 -0400

    Finish #name_and_population

--- sql_curriculum/01_sql_zoo/lib/02_select_countries.rb
+++ sql_curriculum/01_sql_zoo/lib/02_select_countries.rb
@@ -64,6 +64,12 @@ end
 def name_and_population
   # Show the name and population for 'France', 'Germany', and 'Italy'
   execute(<<-SQL)
+    SELECT 
+      name, population 
+    FROM 
+      countries 
+    WHERE 
+      name IN ('France', 'Germany', 'Italy')
   SQL
 end
 
