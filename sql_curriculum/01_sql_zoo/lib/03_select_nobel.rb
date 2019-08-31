e33221a44f2e04769850decca0a522bd82a38571
Fares <fares.osman@outlook.com>
2019-08-31 01:42:38 -0400

    Finish #millenial_peace_prizes

--- sql_curriculum/01_sql_zoo/lib/03_select_nobel.rb
+++ sql_curriculum/01_sql_zoo/lib/03_select_nobel.rb
@@ -63,6 +63,12 @@ end
 def millennial_peace_prizes
   # Give the name of the 'Peace' winners since the year 2000, including 2000.
   execute(<<-SQL)
+    SELECT 
+      winner
+    FROM 
+      nobels 
+    WHERE 
+      subject = 'Peace' AND yr >= 2000
   SQL
 end
 
