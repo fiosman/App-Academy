180e764b79e6725776458f65f253e99e6bde4c7d
Fares <fares.osman@outlook.com>
2019-08-31 20:21:46 -0400

    Finish #populous_continents

--- sql_curriculum/01_sql_zoo/lib/05_aggregates.rb
+++ sql_curriculum/01_sql_zoo/lib/05_aggregates.rb
@@ -94,5 +94,13 @@ end
 def populous_continents
   # List the continents that have a total population of at least 100 million.
   execute(<<-SQL)
+    SELECT 
+      continent
+    FROM 
+      countries
+    GROUP BY 
+      continent 
+    HAVING 
+      SUM(population) >= 100000000
   SQL
 end
