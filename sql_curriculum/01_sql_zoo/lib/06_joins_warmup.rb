8e6d421d33bcae6a129ffe12eba567e52c426b5a
Fares <fares.osman@outlook.com>
2019-08-31 20:33:45 -0400

    Finish #trek_films

--- sql_curriculum/01_sql_zoo/lib/06_joins_warmup.rb
+++ sql_curriculum/01_sql_zoo/lib/06_joins_warmup.rb
@@ -63,6 +63,16 @@ def trek_films
   # these movies include the words Star Trek in the title). Order results by
   # year.
   execute(<<-SQL)
+    SELECT 
+      id, 
+      title, 
+      yr
+    FROM 
+      movies 
+    WHERE 
+      title LIKE '%Star Trek%' 
+    ORDER BY 
+      yr ASC
   SQL
 end
 
