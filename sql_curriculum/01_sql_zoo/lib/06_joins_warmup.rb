491f9e61b98802c1b53e368b790f02e928cf392a
Fares <fares.osman@outlook.com>
2019-08-31 20:43:21 -0400

    Finish #glenn_close_id

--- sql_curriculum/01_sql_zoo/lib/06_joins_warmup.rb
+++ sql_curriculum/01_sql_zoo/lib/06_joins_warmup.rb
@@ -91,6 +91,12 @@ end
 def glenn_close_id
   # What id number does the actress 'Glenn Close' have?
   execute(<<-SQL)
+    SELECT 
+      id 
+    FROM 
+      actors 
+    WHERE 
+      name = 'Glenn Close' 
   SQL
 end
 
