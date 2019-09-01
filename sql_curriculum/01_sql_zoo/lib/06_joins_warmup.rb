a9720fc539549552c124d4c00d9a8a3a4dc93d89
Fares <fares.osman@outlook.com>
2019-08-31 23:05:31 -0400

    Finish #casablanca_cast

--- sql_curriculum/01_sql_zoo/lib/06_joins_warmup.rb
+++ sql_curriculum/01_sql_zoo/lib/06_joins_warmup.rb
@@ -116,6 +116,14 @@ def casablanca_cast
   # Obtain the cast list for 'Casablanca'. Use the id value that you obtained
   # in the previous question directly in your query (for example, id = 1).
   execute(<<-SQL)
+    SELECT 
+      name 
+    FROM 
+      actors 
+    JOIN 
+      castings ON actors.id = castings.actor_id 
+    WHERE 
+      castings.movie_id = 27
   SQL
 end
 
