a4330aed3e89b1b5a7d1d7b26426d26d2372dd2f
Fares <fares.osman@outlook.com>
2019-09-02 23:51:54 -0400

    Finish #physics_no_chemistry

--- sql_curriculum/01_sql_zoo/lib/03a_nobel_bonus.rb
+++ sql_curriculum/01_sql_zoo/lib/03a_nobel_bonus.rb
@@ -14,5 +14,19 @@ require_relative './sqlzoo.rb'
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
+    SELECT DISTINCT
+      yr
+    FROM 
+      nobels
+    WHERE
+      yr NOT IN ( 
+        SELECT 
+          yr
+        FROM 
+          nobels
+        WHERE 
+          subject = 'Chemistry'
+      )
+      AND subject = 'Physics'
   SQL
 end
