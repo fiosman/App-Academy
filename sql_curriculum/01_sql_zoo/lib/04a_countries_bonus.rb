c702bec88004cd614d92872f7f69fa751962be00
Fares <fares.osman@outlook.com>
2019-09-03 00:16:05 -0400

    Finish large_neighbors

--- sql_curriculum/01_sql_zoo/lib/04a_countries_bonus.rb
+++ sql_curriculum/01_sql_zoo/lib/04a_countries_bonus.rb
@@ -59,5 +59,20 @@ def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+    SELECT 
+      c1.name, 
+      c1.continent 
+    FROM 
+      countries c1 
+    WHERE 
+      c1.population > 3 * ( 
+        SELECT 
+          MAX(c2.population)
+        FROM 
+          countries c2 
+        WHERE 
+          c1.continent = c2.continent AND 
+          c1.name != c2.name
+      )
   SQL
 end
