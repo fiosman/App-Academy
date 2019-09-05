c3d2c02b7db6b50f060570ad5c2589cd1216f1bd
Fares <fares.osman@outlook.com>
2019-09-04 22:18:10 -0400

    First commit

---
+++ sql_curriculum/02_plays-playwrights_ORM/plays.rb
@@ -0,0 +1,51 @@
+require 'sqlite3'
+require 'singleton'
+
+class PlayDBConnection < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('plays.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class Play
+  attr_accessor :id, :title, :year, :playwright_id
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM plays")
+    data.map { |datum| Play.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @year = options['year']
+    @playwright_id = options['playwright_id']
+  end
+
+  def create
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.title, self.year, self.playwright_id)
+      INSERT INTO
+        plays (title, year, playwright_id)
+      VALUES
+        (?, ?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.title, self.year, self.playwright_id, self.id)
+      UPDATE
+        plays
+      SET
+        title = ?, year = ?, playwright_id = ?
+      WHERE
+        id = ?
+    SQL
+  end
+end
