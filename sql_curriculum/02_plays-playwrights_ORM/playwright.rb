edfd4e015d1a1d03d48e095ed5d0dea63a5ba1ed
Fares <fares.osman@outlook.com>
2019-09-08 23:51:28 -0400

    Add skeleton for Playwright class

---
+++ sql_curriculum/02_plays-playwrights_ORM/playwright.rb
@@ -0,0 +1,21 @@
+class Playwright
+
+  def self.all 
+  end
+
+  def self.find_by_name(name)
+  end
+
+  def initialize
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def get_plays
+  end
+
+end
