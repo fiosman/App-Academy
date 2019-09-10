88f7011bfe66cfc6f22ed1548359f8cf2089559e
Fares <fares.osman@outlook.com>
2019-09-09 22:16:51 -0400

    finish ::find_by_name & #initialize

--- sql_curriculum/02_plays-playwrights_ORM/playwright.rb
+++ sql_curriculum/02_plays-playwrights_ORM/playwright.rb
@@ -9,10 +9,23 @@ class Playwright
   end
 
   def self.find_by_name(name)
+    playwright = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT 
+        *
+      FROM 
+        playwrights
+      WHERE 
+        playwrights.name = ? 
+    SQL
+    return nil unless playwright.length > 0 
 
+    playwright = Playwright.new(playwrights.first)
   end
 
-  def initialize
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
   end
 
   def create
