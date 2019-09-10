7b480deecdb7c145d7558ca700319657afa56c73
Fares <fares.osman@outlook.com>
2019-09-09 21:50:18 -0400

    Finish ::all

--- sql_curriculum/02_plays-playwrights_ORM/playwright.rb
+++ sql_curriculum/02_plays-playwrights_ORM/playwright.rb
@@ -1,9 +1,15 @@
+require_relative 'plays'
+require 'sqlite3'
+
 class Playwright
 
   def self.all 
+    data = PlayDBConnection.instance.execute("SELECT * FROM Playwrights")
+    data.map { |datum| Playwright.new(datum) }
   end
 
   def self.find_by_name(name)
+
   end
 
   def initialize
