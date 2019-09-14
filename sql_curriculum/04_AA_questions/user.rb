80e2a34976b81f0b51bf704905fb0fe25a0f6646
Fares <fares.osman@outlook.com>
2019-09-13 21:39:54 -0400

    Finish User:;find_by_name

--- sql_curriculum/04_AA_questions/user.rb
+++ sql_curriculum/04_AA_questions/user.rb
@@ -16,6 +16,19 @@ class User
     users.map { |user| User.new(user) }
   end
 
+  def self.find_by_name(fname, lname)
+    user = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+      SELECT 
+        * 
+      FROM 
+        users
+      WHERE 
+        fname = ? AND lname = ? 
+    SQL
+
+    User.new(user.first)
+  end
+
   def initialize(options)
     @fname = options['fname']
     @lname = options['lname']
