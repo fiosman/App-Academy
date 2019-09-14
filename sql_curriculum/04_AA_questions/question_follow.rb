8b1566a3a6a4a671b88f9951a38f18902e4a8a70
Fares <fares.osman@outlook.com>
2019-09-13 23:10:22 -0400

    Finish QuestionFollow::followers_for_question_id

--- sql_curriculum/04_AA_questions/question_follow.rb
+++ sql_curriculum/04_AA_questions/question_follow.rb
@@ -1,4 +1,5 @@
 require_relative 'questions_database'
+require_relative 'user'
 
 class QuestionFollow
   attr_accessor :user_id, :question_id
@@ -14,6 +15,24 @@ class QuestionFollow
     SQL
 
     question_follows.map { |question_follow| QuestionFollow.new(question_follow) }
+  end
+
+  def self.followers_for_question_id(question_id)
+    #return followers for a question 
+    users = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT 
+        fname, lname
+      FROM 
+        question_follows
+      JOIN 
+        users ON users.id = question_follows.user_id
+      JOIN 
+        questions ON questions.id = question_follows.question_id
+      WHERE 
+        question_id = ?
+    SQL
+
+    users.map { |user| User.new(user) }
   end
 
   def initialize(options)
