c322295b90192258de16b3cd295c4e0a12ad23c0
Fares <fares.osman@outlook.com>
2019-09-15 18:27:11 -0400

    Finished Person Model and people table

--- sql_curriculum/05_intro_to_rails/DemoProject/app/models/person.rb
+++ sql_curriculum/05_intro_to_rails/DemoProject/app/models/person.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id       :bigint           not null, primary key
+#  name     :string           not null
+#  house_id :integer          not null
+#
+
 class Person < ApplicationRecord 
+  validates :name, :house_id, presence: true
 end
--- sql_curriculum/05_intro_to_rails/DemoProject/db/migrate/20190915212521_create_people.rb
+++ sql_curriculum/05_intro_to_rails/DemoProject/db/migrate/20190915212521_create_people.rb
@@ -1,6 +1,8 @@
 class CreatePeople < ActiveRecord::Migration[5.2]
   def change
     create_table :people do |t|
+      t.string :name, null: false
+      t.integer :house_id, null: false
     end
   end
 end
