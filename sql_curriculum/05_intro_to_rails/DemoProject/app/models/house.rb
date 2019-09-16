008d823a1b215811c7fcee92e0e9d0f016988256
Fares <fares.osman@outlook.com>
2019-09-15 20:15:14 -0400

    Finish exercise

--- sql_curriculum/05_intro_to_rails/DemoProject/app/models/house.rb
+++ sql_curriculum/05_intro_to_rails/DemoProject/app/models/house.rb
@@ -8,4 +8,9 @@
 
 class House < ApplicationRecord 
   validates :address, presence: true
+  
+  has_many :residents, 
+    primary_key: :id,
+    foreign_key: :house_id, 
+    class_name: :Person
 end
--- sql_curriculum/05_intro_to_rails/DemoProject/app/models/person.rb
+++ sql_curriculum/05_intro_to_rails/DemoProject/app/models/person.rb
@@ -9,4 +9,9 @@
 
 class Person < ApplicationRecord 
   validates :name, :house_id, presence: true
+
+  belongs_to :house, 
+    primary_key: :id, 
+    foreign_key: :house_id, 
+    class_name: :House
 end
