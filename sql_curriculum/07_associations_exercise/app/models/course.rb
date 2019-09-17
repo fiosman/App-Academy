c55f4befa43cbe0aeb8f5706f7abb8c0d43e25e4
Fares <fares.osman@outlook.com>
2019-09-17 11:10:36 -0400

    Finish enrolled_students association for Course

--- sql_curriculum/07_associations_exercise/app/models/course.rb
+++ sql_curriculum/07_associations_exercise/app/models/course.rb
@@ -11,5 +11,12 @@
 #
 
 class Course < ApplicationRecord
-  
+  has_many :enrollments, 
+    primary_key: :id, 
+    foreign_key: :course_id, 
+    class_name: :Enrollment
+
+  has_many :enrolled_students, 
+    through: :enrollments, 
+    source: :user
 end
