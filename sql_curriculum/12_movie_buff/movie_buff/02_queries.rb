e0a171512e73f501bcdf5f7427806a22a150f03d
Fares <fares.osman@outlook.com>
2019-10-02 23:10:13 -0400

    Finish exercise

--- sql_curriculum/12_movie_buff/movie_buff/02_queries.rb
+++ sql_curriculum/12_movie_buff/movie_buff/02_queries.rb
@@ -31,11 +31,21 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
-
+  Movie 
+    .select(:id, :title, 'actors.name') 
+    .joins(:actors)
+    .where({ castings: { ord: 1 } })
+    .where('castings.actor_id = movies.director_id')
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
-
+  Actor. 
+    select(:id, :name, 'COUNT(actor_id) as roles')
+    .joins(:castings)
+    .where.not({ castings: { ord: 1 } })
+    .group(:id)
+    .order('roles DESC')
+    .limit(2)
 end
