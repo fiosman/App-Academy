f24be79c7e6261631f2e3679887cccb4e7d70528
Fares <fares.osman@outlook.com>
2019-10-04 23:01:19 -0400

    Finish #starring

--- sql_curriculum/12_movie_buff/movie_buff/03_queries.rb
+++ sql_curriculum/12_movie_buff/movie_buff/03_queries.rb
@@ -49,6 +49,12 @@ def starring(whazzername)
   # ex. "Sylvester Stallone" is like "sylvester" and "lester stone" but
   # not like "stallone sylvester" or "zylvester ztallone"
 
+  letters = "%#{whazzername.split("").join('%')}%"
+
+  Movie 
+    .select(:id, :title)
+    .joins(:actors)
+    .where('LOWER(actors.name) LIKE LOWER(?)', letters)
 end
 
 def longest_career
