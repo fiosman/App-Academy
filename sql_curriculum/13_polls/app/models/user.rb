f116deba8372c25a00fd99dbb7673ea977321e4c
Fares <fares.osman@outlook.com>
2019-10-05 20:46:38 -0400

    Finish associations for User

--- sql_curriculum/13_polls/app/models/user.rb
+++ sql_curriculum/13_polls/app/models/user.rb
@@ -10,4 +10,14 @@
 
 class User < ApplicationRecord 
   validates :username, presence: true, uniqueness: true
+
+  has_many :authored_polls, 
+    primary_key: :id, 
+    foreign_key: :user_id, 
+    class_name: :Poll
+
+  has_many :responses, 
+    primary_key: :id, 
+    foreign_key: :user_id, 
+    class_name: :Response 
 end
