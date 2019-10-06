caa282c306144dd51cae14c226fdfe0d0588be1d
Fares <fares.osman@outlook.com>
2019-10-05 20:53:31 -0400

    Finish associations for Poll

--- sql_curriculum/13_polls/app/models/poll.rb
+++ sql_curriculum/13_polls/app/models/poll.rb
@@ -11,4 +11,14 @@
 
 class Poll < ApplicationRecord
   validates :title, :user_id, presence: true
+
+  belongs_to :author, 
+    primary_key: :id, 
+    foreign_key: :user_id, 
+    class: :User 
+
+  has_many :questions, 
+    primary_key: :id, 
+    foreign_key: :question_id, 
+    class: :Question
 end
