3cab3d169062f720d5630133083bc1f20807d2f1
Fares <fares.osman@outlook.com>
2019-10-05 20:58:12 -0400

    Finish associations for Question

--- sql_curriculum/13_polls/app/models/question.rb
+++ sql_curriculum/13_polls/app/models/question.rb
@@ -11,4 +11,14 @@
 
 class Question < ApplicationRecord 
   validates :question, :poll_id, presence: true
+
+  has_many :answer_choices, 
+    primary_key: :id, 
+    foreign_key: :question_id, 
+    class: :Question 
+
+  belongs_to :poll, 
+    primary_key: :id, 
+    foreign_key: :poll_id, 
+    class: :Poll 
 end
