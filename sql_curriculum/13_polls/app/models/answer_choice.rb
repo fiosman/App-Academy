388ddcc39d3ce16b5a81a0b0c856dfafcde68982
Fares <fares.osman@outlook.com>
2019-10-05 21:00:00 -0400

    Finish associations for AnswerChoice

--- sql_curriculum/13_polls/app/models/answer_choice.rb
+++ sql_curriculum/13_polls/app/models/answer_choice.rb
@@ -11,4 +11,14 @@
 
 class AnswerChoice < ApplicationRecord 
   validates :answer_choice, :question_id, presence: true
+
+  belongs_to :question, 
+    primary_key: :id, 
+    foreign_key: :question_id, 
+    class: :Question 
+    
+  has_many :responses, 
+    primary_key: :id, 
+    foreign_key: :answer_choice_id, 
+    class: :Response 
 end
