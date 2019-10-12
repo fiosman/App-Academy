29afea5b8cb06b5b78a2688f05bede984e8101b2
Fares <fares.osman@outlook.com>
2019-10-12 14:01:00 -0400

    Finish exercise

--- sql_curriculum/14_metacorgis/meta_corgis.rb
+++ sql_curriculum/14_metacorgis/meta_corgis.rb
@@ -107,18 +107,25 @@ class MetaCorgiSnacks
   def initialize(snack_box, box_id)
     @snack_box = snack_box
     @box_id = box_id
+    
+    snack_box.methods.grep(/^get_(.*)_info$/) { MetaCorgiSnacks.define_snack $1 }
   end
 
-  def method_missing(name, *args)
-    # Your code goes here...
-    info = @snack_box.send("get_#{name}_info", @box_id) 
-    tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
-    result = "#{name.to_s.capitalize}: #{info}: #{tastiness}"
-    tastiness > 30 ? "* #{result}" : result
-  end
-
+  # def method_missing(name, *args)
+  #   info = @snack_box.send("get_#{name}_info", @box_id) 
+  #   tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
+  #   result = "#{name.to_s.capitalize}: #{info}: #{tastiness}"
+  #   tastiness > 30 ? "* #{result}" : result
+  # end
 
   def self.define_snack(name)
-    # Your code goes here... 
+    define_method(name) do 
+      info = @snack_box.send("get_#{name}_info", @box_id) 
+      tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
+      result = "#{name.to_s.capitalize}: #{info}: #{tastiness}"
+      tastiness > 30 ? "* #{result}" : result
+    end
+
+    nil
   end
 end
