cd1331462ee77dbe0b8a4903446e061fb891de17
Fares <fares.osman@outlook.com>
2019-10-13 18:39:57 -0400

    Finish Searchable

--- sql_curriculum/16_active_record_lite/lib/02_searchable.rb
+++ sql_curriculum/16_active_record_lite/lib/02_searchable.rb
@@ -3,10 +3,20 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map { |key| "#{key} = ?" }.join('AND ')
+    res = DBConnection.execute(<<-SQL, *params.values)
+      SELECT 
+        * 
+      FROM 
+        #{self.table_name}
+      WHERE 
+        #{where_line}
+    SQL
+
+    parse_all(res)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable 
 end
