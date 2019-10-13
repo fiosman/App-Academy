8e53a579456f1224911ca2605b80d9026106f99f
Fares <fares.osman@outlook.com>
2019-10-12 22:39:02 -0400

    Finish AttrAccessorObject

--- sql_curriculum/16_active_record_lite/lib/00_attr_accessor_object.rb
+++ sql_curriculum/16_active_record_lite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,13 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name| 
+      define_method(name) do 
+        self.instance_variable_get("@#{name}")
+      end
+      
+      define_method("#{name}=") do |value| 
+        self.instance_variable_set("@#{name}", value)
+      end
+    end
   end
 end
