78ee4498a6c54e808c4ff072638be991631e2ed0
Fares <fares.osman@outlook.com>
2019-10-14 01:21:50 -0400

    Finished BelongsToOptions and HasManyOptions

--- sql_curriculum/16_active_record_lite/lib/03_associatable.rb
+++ sql_curriculum/16_active_record_lite/lib/03_associatable.rb
@@ -20,13 +20,30 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+  
+    defaults = { 
+      foreign_key: "#{name}_id".to_sym,
+      class_name: name.to_s.camelcase,
+      primary_key: :id
+    }
+  
+    defaults.keys.each do |attr| 
+      self.send("#{attr}=", options[attr] || defaults[attr])
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = { 
+      foreign_key: "#{self_class_name.underscore}_id".to_sym,
+      primary_key: :id, 
+      class_name: name.to_s.singularize.camelcase
+    }
+
+    defaults.keys.each do |attr| 
+      self.send("#{attr}=", options[attr] || defaults[attr])
+    end
   end
 end
 
