46ec7adeb60c84004535e2232f7731186b0f494e
Fares <fares.osman@outlook.com>
2019-10-14 21:33:01 -0400

    Finish exercise

--- sql_curriculum/16_active_record_lite/lib/04_associatable2.rb
+++ sql_curriculum/16_active_record_lite/lib/04_associatable2.rb
@@ -5,6 +5,31 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) do 
+      through_options = self.class.assoc_options[through_name] 
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      through_foreign_val = send(through_options.foreign_key)
+      through_primary_key = through_options.primary_key 
+      through_table = through_options.table_name 
+
+      source_foreign_key = source_options.foreign_key 
+      source_primary_key = source_options.primary_key 
+      source_table = source_options.table_name 
+
+      res = DBConnection.execute(<<-SQL, through_foreign_val)
+        SELECT 
+          #{source_table}.*
+        FROM 
+          #{through_table}
+        JOIN 
+          #{source_table}
+        ON 
+          #{through_table}.#{source_foreign_key} = #{source_table}.#{source_primary_key}
+        WHERE 
+          #{through_table}.#{through_primary_key} = ?
+      SQL 
+
+      source_options.model_class.parse_all(res).first
   end
 end
