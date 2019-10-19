c6ab2f7207ea02423c24726507e21c2c7fd8d065
Fares <fares.osman@outlook.com>
2019-10-19 18:35:57 -0400

    Add artist association

--- rails_curriculum/02_art_share_api/app/models/artwork.rb
+++ rails_curriculum/02_art_share_api/app/models/artwork.rb
@@ -13,5 +13,10 @@
 class Artwork < ApplicationRecord 
   validates :title, :artist_id, :image_url, presence: true 
   validates :title, uniqueness: { scope: :artist_id }
-  validates :image_url, uniqueness: true
+  validates :image_url, uniqueness: true 
+
+  belongs_to :artist, 
+    primary_key: :id, 
+    foreign_key: :artist_id, 
+    class_name: :User
 end
