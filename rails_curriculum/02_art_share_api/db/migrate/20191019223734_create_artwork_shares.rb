b652fc595aaec2f1442213ccf549facde951aa83
Fares <fares.osman@outlook.com>
2019-10-20 23:40:35 -0400

    Ensure user cannot have a single artwork shared with them more than once

--- rails_curriculum/02_art_share_api/db/migrate/20191019223734_create_artwork_shares.rb
+++ rails_curriculum/02_art_share_api/db/migrate/20191019223734_create_artwork_shares.rb
@@ -7,6 +7,6 @@ class CreateArtworkShares < ActiveRecord::Migration[5.2]
 
     add_index :artwork_shares, :artwork_id 
     add_index :artwork_shares, :viewer_id 
-    
+    add_index :artwork_shares, [:viewer_id, :artwork_id], unique: true
   end
 end
