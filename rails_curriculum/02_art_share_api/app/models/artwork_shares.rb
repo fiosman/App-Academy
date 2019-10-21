620bcc2e62170a76c0f01852a54560be797cf839
Fares <fares.osman@outlook.com>
2019-10-20 23:58:13 -0400

    Add viewer association to ArtworkShare

--- rails_curriculum/02_art_share_api/app/models/artwork_shares.rb
+++ rails_curriculum/02_art_share_api/app/models/artwork_shares.rb
@@ -1,4 +1,9 @@
 class ArtworkShare < ApplicationRecord 
   validates :artwork_id, :viewer_id, presence: true 
   validates :viewer_id, uniqueness: { scope: :artwork_id }
+
+  belongs_to :viewer, 
+    primary_key: :id, 
+    foreign_key: :viewer_id, 
+    class_name: :User
 end
