cc915edcf6fecc32f966133456f9df34e429e589
Fares <fares.osman@outlook.com>
2019-10-22 12:39:05 -0400

    Seed users

--- rails_curriculum/02_art_share_api/db/seeds.rb
+++ rails_curriculum/02_art_share_api/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(username: 'Megan')
+User.create!(username: 'Fares')
+User.create!(username: 'Andrew')
+User.create!(username: 'Chris')
+
