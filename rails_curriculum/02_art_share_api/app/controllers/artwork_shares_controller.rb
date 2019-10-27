bcce51602e284df260808464e26d2968208ad286
Fares <fares.osman@outlook.com>
2019-10-26 23:05:02 -0400

    Set up actions and finalize routes config for ArtworkShares

--- rails_curriculum/02_art_share_api/app/controllers/artwork_shares_controller.rb
+++ rails_curriculum/02_art_share_api/app/controllers/artwork_shares_controller.rb
@@ -1,2 +1,28 @@
 class ArtworkSharesController < ApplicationController 
+
+  def create 
+    artwork_share = ArtworkShare.new(artwork_share_params) 
+    if artwork_share.save 
+      render json: artwork_share 
+    else
+      render json: artwork_share.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy 
+    artwork_share = ArtworkShare.find(params[:id]) 
+
+    if artwork_share.destroy 
+      render jeson: artwork_share
+    else
+      render json: artwork_share.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private 
+
+  def artwork_share_params 
+    params.require(:artwork_share).permit(:artwork_id, :viewer_id)
+  end
+
 end
--- rails_curriculum/02_art_share_api/config/routes.rb
+++ rails_curriculum/02_art_share_api/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :users, :artworks, only: [:index, :show, :update, :create, :destroy]
-  resources :artwork_shares, only: [:create]
+  resources :artwork_shares, only: [:create, :destroy]
 end
