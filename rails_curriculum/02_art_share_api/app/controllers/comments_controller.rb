34e7efa359af32a7bebe1c8f22c0fd9f44b9c6ec
Fares <fares.osman@outlook.com>
2019-10-27 23:48:39 -0400

    Finish updating index for CommentsController

--- rails_curriculum/02_art_share_api/app/controllers/comments_controller.rb
+++ rails_curriculum/02_art_share_api/app/controllers/comments_controller.rb
@@ -1,12 +1,39 @@
 class CommentsController < ApplicationController 
   
   def create 
+    comment = Comment.new(comments_params) 
+    
+    if comment.save 
+      render json: comment 
+    else
+      render json: comment.error.full_messages, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    comment = Comment.find(params[:id])
+
+    if comment.destroy 
+      render json: comment 
+    else
+      render json: comment.error.full_messages, status: :unprocessable_entity
+    end
   end
 
   def index
-    render json: Comment.all 
+    if params[:user_id] 
+      render json: Comment.where(user_id: params[:user_id]) 
+    elsif params[:artwork_id] 
+      render json: Comment.where(artwork_id: params[:artwork_id]) 
+    else
+      render json: Comment.all 
+    end
   end
+
+  private 
+
+  def comments_params 
+    params.require(:comment).permit(:user_id, :comment_body, :artwork_id)
+  end
+
 end
--- rails_curriculum/02_art_share_api/config/routes.rb
+++ rails_curriculum/02_art_share_api/config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   resources :users do 
     resources :artworks
-  end
+  end 
 end
