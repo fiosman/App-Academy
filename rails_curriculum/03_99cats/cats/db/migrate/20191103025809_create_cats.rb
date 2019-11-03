bbddea2b82b766c588e714355d68554a539ee61b
Fares <fares.osman@outlook.com>
2019-11-02 23:09:16 -0400

    Create migration for cats

---
+++ rails_curriculum/03_99cats/cats/db/migrate/20191103025809_create_cats.rb
@@ -0,0 +1,13 @@
+class CreateCats < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cats do |t|
+      t.date :birth_date, null: false 
+      t.string :color, null: false 
+      t.string :name, null: false 
+      t.string :sex, null: false, limit: 1
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
