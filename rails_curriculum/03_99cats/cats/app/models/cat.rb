4784de8f2516122e17d39e151ecddf05541bad6d
Fares <fares.osman@outlook.com>
2019-11-03 13:13:20 -0500

    Add model level validations and inclusions

--- rails_curriculum/03_99cats/cats/app/models/cat.rb
+++ rails_curriculum/03_99cats/cats/app/models/cat.rb
@@ -1,2 +1,6 @@
 class Cat < ApplicationRecord 
+  COLORS = %w(red green blue purple black white)
+  validates :birth_date, :name, presence: true 
+  validates :color, presence: true, inclusion: { in: COLORS }
+  validates :sex, presence: true, inclusion: { in: %w(M F) }
 end
