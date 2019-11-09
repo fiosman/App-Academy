11152dbfb00aa94ba86c4c1e559ef45a97a2e216
Fares <fares.osman@outlook.com>
2019-11-09 12:27:22 -0500

    Add annotations to Cat and CatRentalRequest

--- rails_curriculum/03_99cats/cats/app/models/cat.rb
+++ rails_curriculum/03_99cats/cats/app/models/cat.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :integer          not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Cat < ApplicationRecord 
   include ActionView::Helpers::DateHelper 
   
--- rails_curriculum/03_99cats/cats/app/models/cat_rental_request.rb
+++ rails_curriculum/03_99cats/cats/app/models/cat_rental_request.rb
@@ -1,6 +1,28 @@
+# == Schema Information
+#
+# Table name: cat_rental_requests
+#
+#  id         :integer          not null, primary key
+#  cat_id     :integer          not null
+#  start_date :date             not null
+#  end_date   :date             not null
+#  status     :string           default("PENDING"), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class CatRentalRequest < ApplicationRecord 
   validates :cat_id, :start_date, :end_date, presence: true
   validates :status, presence: true, inclusion: { in: %w(PENDING APPROVED DENIED) }
 
   belongs_to :cat
+
+  def overlapping_requests 
+    CatRentalRequest.where(id: 4)
+  end
 end
+
+#A cat rental request starting on 02/25/17 and ending on 02/27/17.
+#There is a overlap if another cat rental also starts on the same day (02/25/17).
+#There is a overlap if another cat rental request starts on the return day (02/27/17).
+#There is a overlap if another cat rental request starts between the start and end dates (02/26/17).
