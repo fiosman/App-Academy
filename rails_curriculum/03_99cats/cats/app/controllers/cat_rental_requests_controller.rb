51e493c6a3941976e090ac2b868caffbf5437985
Fares <fares.osman@outlook.com>
2019-11-09 22:48:14 -0500

    Finish view for new

--- rails_curriculum/03_99cats/cats/app/controllers/cat_rental_requests_controller.rb
+++ rails_curriculum/03_99cats/cats/app/controllers/cat_rental_requests_controller.rb
@@ -1,3 +1,21 @@
 class CatRentalRequestsController < ApplicationController 
-  
+
+  def index 
+  end 
+
+  def show 
+  end 
+
+  def new
+    @cats = Cat.all 
+  end
+
+  def create
+  end
+
+  private 
+
+  def cat_rental_requests_params 
+    params.require(:cat_rental_request).permit(:cat_id, :start_date, :end_date, :status)
+  end
 end
