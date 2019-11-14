4ea317856dfd47480e9dee0c4f13af9b401efe81
Fares <fares.osman@outlook.com>
2019-11-13 23:22:47 -0500

    Finish exercise

--- rails_curriculum/03_99cats/cats/config/routes.rb
+++ rails_curriculum/03_99cats/cats/config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :cats
   resources :cat_rental_requests, only: [:create, :new] do 
     member do 
-      post :approve
-      post :deny
+      patch :approve
+      patch :deny
     end
   end
 end
