4ed22cee326f02aa4f4edbf39df0618560ac0a46
Fares <fares.osman@outlook.com>
2019-11-16 23:06:13 -0500

    Validate password digest

--- rails_curriculum/04_rails_auth/rails_auth/app/models/user.rb
+++ rails_curriculum/04_rails_auth/rails_auth/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :username, :session_token, presence: true 
   validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
 end
