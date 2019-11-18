cfec269a0a4d53b3305b76fc3e17ce95178decd3
Fares <fares.osman@outlook.com>
2019-11-17 23:43:23 -0500

    Boilerplate for class and method definitions

--- rails_curriculum/04_rails_auth/rails_auth/app/models/user.rb
+++ rails_curriculum/04_rails_auth/rails_auth/app/models/user.rb
@@ -1,5 +1,23 @@
 class User < ApplicationRecord
+
+  attr_reader :password 
+  
   validates :username, :session_token, presence: true 
   validates :password, length: { minimum: 6, allow_nil: true }
   validates :password_digest, presence: { message: 'Password can\'t be blank' }
+
+  def self.find_by_credentials 
+  end
+
+  def self.generate_session_token 
+  end
+
+  def reset_session_token! 
+  end
+
+  def ensure_session_end 
+  end
+
+  def password= 
+  end
 end
