8b6c712407d132c8b0ed07fdcdcc33d17bd724d2
Fares <fares.osman@outlook.com>
2019-11-20 23:23:10 -0500

    Annotate User

--- rails_curriculum/04_rails_auth/rails_auth/test/models/user_test.rb
+++ rails_curriculum/04_rails_auth/rails_auth/test/models/user_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
