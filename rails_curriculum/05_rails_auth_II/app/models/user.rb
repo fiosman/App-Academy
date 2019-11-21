bd472ed1926b7f88d6a61112b5b33551db073f20
Fares <fares.osman@outlook.com>
2019-11-20 23:37:35 -0500

    Add #reset_session_token!

--- rails_curriculum/05_rails_auth_II/app/models/user.rb
+++ rails_curriculum/05_rails_auth_II/app/models/user.rb
@@ -20,4 +20,15 @@ class User < ApplicationRecord
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
   
+  def reset_session_token! 
+    self.session_token = User.generate_session_token 
+    self.save! 
+    self.session_token 
+  end
+
+  private 
+
+  def self.generate_session_token 
+    SecureRandom::urlsafe_base64(16)
+  end
 end
