b714293b277a7243b154e409822e28b081640a53
Fares <fares.osman@outlook.com>
2019-11-20 23:34:03 -0500

    Finish User#is_password?

--- rails_curriculum/05_rails_auth_II/app/models/user.rb
+++ rails_curriculum/05_rails_auth_II/app/models/user.rb
@@ -15,5 +15,9 @@ class User < ApplicationRecord
   validates :password_digest, presence: { message: 'Password can\'t be blank' }
   validates :password, length: { minimum: 6, allow_nil: true } 
   after_initialize :ensure_session_token 
+
+  def is_password?(password) 
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
   
 end
