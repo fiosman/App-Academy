1e1632180475056128a0e96055afcb9c13e52335
Fares <fares.osman@outlook.com>
2019-11-20 23:12:16 -0500

    Write all model level validations

--- rails_curriculum/05_rails_auth_II/app/models/user.rb
+++ rails_curriculum/05_rails_auth_II/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :user_name, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
+  validates :password, length: { minimum: 6, allow_nil: true } 
+  after_initialize :ensure_session_token 
+  
 end
