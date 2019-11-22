01f99172ab1919dd64de4bf257a615931e4ef4f4
Fares <fares.osman@outlook.com>
2019-11-21 22:31:18 -0500

    Add routes for users resource

--- rails_curriculum/05_rails_auth_II/config/routes.rb
+++ rails_curriculum/05_rails_auth_II/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cats, except: :destroy
+  resources :users, only: [:new, :create]
   resources :cat_rental_requests, only: [:new, :create] do
     member do
       post :approve
