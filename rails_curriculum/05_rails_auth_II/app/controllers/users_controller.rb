fa9ad6e638c31a4e397e615bf79c0f6e6191322d
Fares <fares.osman@outlook.com>
2019-11-22 23:35:19 -0500

    Write UsersController#create and UsersController#new

--- rails_curriculum/05_rails_auth_II/app/controllers/users_controller.rb
+++ rails_curriculum/05_rails_auth_II/app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController 
+  def create 
+    @user = User.new(user_params) 
+    if @user.save 
+      redirect_to :cats_url 
+    else
+      render :new
+    end
+  end
+
+  def new 
+    @user = User.new
+    render :new
+  end
+
+
+  def user_params 
+    params.require(:user).permit(:username, :password)
+  end
 end
