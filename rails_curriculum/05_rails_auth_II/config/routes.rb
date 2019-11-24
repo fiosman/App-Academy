245fed6316c32ff4a54a190b22dff64dc26791a4
Fares <fares.osman@outlook.com>
2019-11-23 23:37:55 -0500

    Add singular session route

--- rails_curriculum/05_rails_auth_II/config/routes.rb
+++ rails_curriculum/05_rails_auth_II/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cats, except: :destroy
   resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
   resources :cat_rental_requests, only: [:new, :create] do
     member do
       post :approve
