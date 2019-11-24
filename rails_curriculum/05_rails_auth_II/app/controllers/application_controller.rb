8c7a9d4a16cb5007fa7e3bc791add7faa593e1e3
Fares <fares.osman@outlook.com>
2019-11-24 12:35:55 -0500

    Finish ApplicationController#current_user

--- rails_curriculum/05_rails_auth_II/app/controllers/application_controller.rb
+++ rails_curriculum/05_rails_auth_II/app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user 
+  #looks up user with the current session token 
+
+  private 
+
+  def current_user
+    return nil if !session[:session_token]
+    User.find_by(session_token: session[:session_token])
+  end
+
 end
