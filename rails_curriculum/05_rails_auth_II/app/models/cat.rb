a6187935943715bc36b472eb769a0724d6ef42a8
Fares <fares.osman@outlook.com>
2019-11-25 23:37:14 -0500

    Validate presence of user_id in Cat model

--- rails_curriculum/05_rails_auth_II/app/models/cat.rb
+++ rails_curriculum/05_rails_auth_II/app/models/cat.rb
@@ -22,7 +22,7 @@ class Cat < ApplicationRecord
 
   validates :color, inclusion: CAT_COLORS
   validates :sex, inclusion: %w(M F)
-  validates :birth_date, :color, :name, :sex, presence: true
+  validates :birth_date, :color, :name, :sex, :user_id, presence: true
 
   # Remember, has_many is just a method where the first argument is
   # the name of the association, and the second argument is an options
--- rails_curriculum/05_rails_auth_II/db/schema.rb
+++ rails_curriculum/05_rails_auth_II/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_042922) do
+ActiveRecord::Schema.define(version: 2019_11_26_043008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2019_11_26_042922) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_cats_on_user_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
