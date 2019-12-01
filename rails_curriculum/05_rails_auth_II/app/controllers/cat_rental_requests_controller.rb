1730626b18a4ed5b1a4d61c497eadb0d20f2357d
Fares <fares.osman@outlook.com>
2019-11-30 19:11:54 -0500

    Finish project

--- rails_curriculum/05_rails_auth_II/app/controllers/cat_rental_requests_controller.rb
+++ rails_curriculum/05_rails_auth_II/app/controllers/cat_rental_requests_controller.rb
@@ -9,7 +9,7 @@ class CatRentalRequestsController < ApplicationController
 
   def create
     @rental_request = CatRentalRequest.new(cat_rental_request_params)
-    @rental_request.user = current_user.id
+    @rental_request.user_id = current_user.id
     if @rental_request.save
       redirect_to cat_url(@rental_request.cat)
     else
