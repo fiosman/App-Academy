a541fcee197523454ade37b753a518bfca9131c0
Fares <fares.osman@outlook.com>
2019-12-01 15:49:18 -0500

    Implement auth_token view helper

--- rails_curriculum/06_helpers_layouts_partials/app/helpers/application_helper.rb
+++ rails_curriculum/06_helpers_layouts_partials/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def auth_token 
+    html = '<input type="hidden"'
+    html += 'name="authenticity_token"'
+    html += "value=#{h(form_authenticity_token)}"
+    html += '/>'
+
+    html.html_safe
+  end
 end
