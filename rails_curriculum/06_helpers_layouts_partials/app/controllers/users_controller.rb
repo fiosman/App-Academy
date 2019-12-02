2f6f44afd0b1afcf71460669617b9673891b1aaa
Fares <fares.osman@outlook.com>
2019-12-01 20:16:42 -0500

    Finish exercise

--- rails_curriculum/06_helpers_layouts_partials/app/controllers/users_controller.rb
+++ rails_curriculum/06_helpers_layouts_partials/app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       login_user!(@user)
+      email = UserMailer.welcome_email(@user) 
+      email.deliver
       redirect_to cats_url
     else
       flash.now[:errors] = @user.errors.full_messages
