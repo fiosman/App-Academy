44003bbb59cc869c09101c090cceca9847a32a62
Fares <fares.osman@outlook.com>
2019-12-02 21:34:11 -0500

    Add skeleton for auth methods

--- rails_curriculum/07_music_app/music_app/app/models/user.rb
+++ rails_curriculum/07_music_app/music_app/app/models/user.rb
@@ -4,4 +4,27 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6, allow_nil: true } 
 
   attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token 
+  end 
+
+  def self.find_by_credentials(email, password) 
+  end
+
+  def reset_session_token! 
+  end 
+
+  def password=(password)
+  end 
+
+  def is_password?(password)
+  end
+
+  private 
+
+  def ensure_session_token 
+  end 
+
 end
