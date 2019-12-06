ad07e39949cd6a467f2062b9aca253c428984d81
Fares <fares.osman@outlook.com>
2019-12-05 22:21:21 -0500

    Write SessionsController#create and SessionsController#new

--- rails_curriculum/07_music_app/music_app/app/controllers/session_controller.rb
+++ rails_curriculum/07_music_app/music_app/app/controllers/session_controller.rb
@@ -1,9 +1,25 @@
 class SessionsController < ApplicationController 
 
   def create
+    user = User.find_by_credentials(
+      params[:user][:email], 
+      params[:user][:password]
+    )
+
+    if user 
+      log_in_user!(user)
+      redirect_to user_url(user.id)
+    else
+      flash.now[:errors] = ['Incorrect password/username entered']
+      render :new
+    end
   end 
 
   def destroy
   end
-  
+
+  def new 
+    render :new 
+  end
+
 end
