4c9139175598b37c93957f2afaa263c3ee04eba2
Fares <fares.osman@outlook.com>
2019-12-05 22:38:46 -0500

    Finish writing application_controller helpers

--- rails_curriculum/07_music_app/music_app/app/controllers/application_controller.rb
+++ rails_curriculum/07_music_app/music_app/app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
-  
+  helper_method :current_user
+
+  private
+
+  def log_in_user!(user) 
+    user.reset_session_token 
+    session[:session_token] = user.session_token
+  end 
+
+  def log_out_user! 
+    current_user.reset_session_token
+    session[:session_token] = nil 
+  end 
+
+  def current_user 
+    return nil if !session[:session_token] 
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end 
+
+  def logged_in?
+    return false if !current_user 
+    true
+  end
+
 end
