837f798ae73bba780fc8a7e84997138e75eb25e7
Fares <fares.osman@outlook.com>
2019-12-08 17:05:39 -0500

    Set up migration for albums

--- rails_curriculum/07_music_app/music_app/db/schema.rb
+++ rails_curriculum/07_music_app/music_app/db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_08_010135) do
+ActiveRecord::Schema.define(version: 2019_12_08_220342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "year", null: false
+    t.integer "band_id", null: false
+    t.boolean "live", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["band_id", "title"], name: "index_albums_on_band_id_and_title", unique: true
+  end
 
   create_table "bands", force: :cascade do |t|
     t.string "name", null: false
