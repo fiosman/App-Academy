5b3cf56b3bb50d46bf18bc104e51906a0d3b555e
Fares <fares.osman@outlook.com>
2019-12-07 20:13:22 -0500

    Create routes for bands resource

--- rails_curriculum/07_music_app/music_app/config/routes.rb
+++ rails_curriculum/07_music_app/music_app/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
   resources :users, only: [:create, :show, :new]
-  resource :session, only: [:create, :destroy, :new]
+  resource :session, only: [:create, :destroy, :new] 
+  resources :bands
 end
