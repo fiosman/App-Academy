0c67b05b16b2023fdc5a818726ed68f8634d4bd7
Fares <fares.osman@outlook.com>
2019-12-08 18:18:43 -0500

    Finish routes for albums

--- rails_curriculum/07_music_app/music_app/config/routes.rb
+++ rails_curriculum/07_music_app/music_app/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
   resources :users, only: [:create, :show, :new]
   resource :session, only: [:create, :destroy, :new] 
-  resources :bands
+  resources :bands do  
+    resources :albums, only: [:new] 
+  end
+  resources :albums, except: [:new, :index]
 end
