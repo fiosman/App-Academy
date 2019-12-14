3a1625b5971e467b0f9e0dc0489aca7a6d6f33cf
Fares <fares.osman@outlook.com>
2019-12-14 12:02:54 -0500

    Add routing for tracks

--- rails_curriculum/07_music_app/music_app/config/routes.rb
+++ rails_curriculum/07_music_app/music_app/config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :bands do  
     resources :albums, only: [:new] 
   end
-  resources :albums, except: [:new, :index]
+  resources :albums, except: [:new, :index] do 
+    resources :tracks, only: [:new]
+  end
+  resources :tracks, except: [:index, :new]
 end
