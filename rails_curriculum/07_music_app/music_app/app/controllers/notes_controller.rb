607c379a4ed64427eecc64c8d7024a4b2d0b447d
Fares <fares.osman@outlook.com>
2019-12-15 18:23:34 -0500

    Write NotesController#create and #destroy

---
+++ rails_curriculum/07_music_app/music_app/app/controllers/notes_controller.rb
@@ -0,0 +1,33 @@
+class NotesController < ApplicationController 
+
+  before_action :require_log_in
+  
+  def create
+    note = Note.new(note_params) 
+    
+    if note.save! 
+      flash[:notice] = "Note created!"
+      redirect_to track_url(note.track_id) 
+    else 
+      flash.now[:errors] = note.errors.full_messages
+      redirect_to track_url(note.track_id)
+    end
+  end 
+
+  def destroy 
+    note = Note.find(params[:id]) 
+
+    if current_user.id == note.user_id
+      note.destroy  
+      redirect_to track_url(note.track_id)
+    else 
+      render text: '403 FORBIDDEN'
+  end
+
+  private 
+
+  def note_params 
+    params.require(;note).permit(:user_id, :track_id, :note)
+  end
+end 
+
