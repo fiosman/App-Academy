3781130b8868dacfce3deb8c7a706718067e344f
Fares <fares.osman@outlook.com>
2019-12-17 01:01:34 -0500

    Write ugly_lyrics

--- rails_curriculum/07_music_app/music_app/app/helpers/application_helper.rb
+++ rails_curriculum/07_music_app/music_app/app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 module ApplicationHelper
-
-  def ugly_lyrics(lyrics) 
-  end 
   
+  def ugly_lyrics(lyrics) 
+    html = '<pre>'
+    lyrics.lines.each do |line| 
+      html += "&#9835;#{h(line)}"
+    end
+    
+    html+= '</pre>' 
+    html.html_safe
+  end
+
 end
