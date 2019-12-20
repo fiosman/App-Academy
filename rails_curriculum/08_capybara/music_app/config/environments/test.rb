6c879b893e2cdcc93ebc731b997f5cb85d7d1eb2
Fares <fares.osman@outlook.com>
2019-12-19 23:58:59 -0500

    Add configs for testing

--- rails_curriculum/08_capybara/music_app/config/environments/test.rb
+++ rails_curriculum/08_capybara/music_app/config/environments/test.rb
@@ -43,4 +43,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #configure default mail server
+  Rails.application.routes.default_url_options[:host] = 'domain.com'
 end
