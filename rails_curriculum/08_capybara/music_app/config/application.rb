59743fd0dcb3fbc87ac66908044ebedf22d06e2f
Fares <fares.osman@outlook.com>
2019-12-22 20:27:17 -0500

    Setup Rspec and shoulda-matchers

--- rails_curriculum/08_capybara/music_app/config/application.rb
+++ rails_curriculum/08_capybara/music_app/config/application.rb
@@ -15,5 +15,15 @@ module MusicApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g| 
+      g.test_framework :rspec,
+        :fixtures => false,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => false
+      end
   end
 end
--- rails_curriculum/08_capybara/music_app/spec/rails_helper.rb
+++ rails_curriculum/08_capybara/music_app/spec/rails_helper.rb
@@ -1,6 +1,6 @@
-  # This file is copied to spec/ when you run 'rails generate rspec:install'
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'shoulda_matchers'
+require 'shoulda-matchers'
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
@@ -61,11 +61,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-end
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
   end
 end
