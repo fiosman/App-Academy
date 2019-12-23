940acc6978e4d730f82a7a1e6964a3cc6c3e5f76
Fares <fares.osman@outlook.com>
2019-12-22 20:45:23 -0500

    Finish writing tests for validations

--- rails_curriculum/08_capybara/music_app/spec/models/user_spec.rb
+++ rails_curriculum/08_capybara/music_app/spec/models/user_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do   
     it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) } 
+    it { should validate_length_of(:password).is_at_least(6) }
   end
 end
