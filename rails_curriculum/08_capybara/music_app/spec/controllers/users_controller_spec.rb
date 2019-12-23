e37b3acc11fd99d0e5ba41dbaf4a6eac72c45d15
Fares <fares.osman@outlook.com>
2019-12-23 14:53:56 -0500

    Finish writing tests for #create

--- rails_curriculum/08_capybara/music_app/spec/controllers/users_controller_spec.rb
+++ rails_curriculum/08_capybara/music_app/spec/controllers/users_controller_spec.rb
@@ -5,8 +5,30 @@ RSpec.describe UsersController, type: :controller do
   describe "GET #new" do
     it "renders the new template" do
       get :new, {}
-      expect(response).to render_template("new")
+      expect(response).to render_template(:new)
     end
   end
-  
+
+  describe "POST #create" do
+    context "with invalid params" do
+      it "validates the presence of the user's email and password" do  
+        user = post :create, params: { user: { email: '' }} 
+        expect(flash.now[:errors]).to be_present
+        expect(response).to render_template(:new) 
+      end
+      it "validates that the password is at least 6 characters long" do  
+        user = post :create, params: { user: { email: 'fares@gmail.com', password: '123' }}
+        expect(flash.now[:errors]).to be_present
+        expect(response).to render_template(:new)
+      end
+    end
+
+    context "with valid params" do
+      it "redirects user to bands index on success" do   
+        post :create, params: { user: { email: 'blah@gmail.com', password: '1234567789' }}  
+        expect(response).to redirect_to(user_url(User.last)) 
+      end 
+    end
+  end
+
 end
