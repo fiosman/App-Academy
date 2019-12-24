321f65a3b71399902fdde46f4d77dcbe7cb4673f
Fares <fares.osman@outlook.com>
2019-12-24 16:45:16 -0500

    Write tests for User validations

--- rails_curriculum/09_goal_app/goalApp/app/models/user.rb
+++ rails_curriculum/09_goal_app/goalApp/app/models/user.rb
@@ -1,3 +1,28 @@
 class User < ApplicationRecord 
-  
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true 
+  validates :password, length: { minimum: 6, allow_nil: true } 
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+  end
+
+  def self.generate_session_token 
+  end 
+
+  def reset_session_token 
+  end 
+
+  def password=(password) 
+  end 
+
+  def is_password?(password)
+  end 
+
+  private 
+
+  def ensure_session_token 
+  end 
+
 end
---
+++ rails_curriculum/09_goal_app/goalApp/spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject(:user) { User.new(username: 'fares', password: '123456') }
+
+  describe 'validations' do  
+    it { should validate_presence_of(:username) }  
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_presence_of(:session_token) }
+    it { should validate_presence_of(:password_digest) } 
+    it { should validate_length_of(:password).is_at_least(6) }
+  end
+end
