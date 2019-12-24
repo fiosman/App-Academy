dff0212a41953f214ba555890f3dc3702889b2d8
Fares <fares.osman@outlook.com>
2019-12-24 13:39:49 -0500

    Add configuration for shoulda_matchers

--- rails_curriculum/09_goal_app/goalApp/spec/rails_helper.rb
+++ rails_curriculum/09_goal_app/goalApp/spec/rails_helper.rb
@@ -60,4 +60,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
