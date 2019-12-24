e2efd208acf7adc9f9d971b9fa07ab5b0f7153ce
Fares <fares.osman@outlook.com>
2019-12-24 15:10:21 -0500

    Add user migration

---
+++ rails_curriculum/09_goal_app/goalApp/app/controllers/users_controller.rb
@@ -0,0 +1,3 @@
+class UsersController < ApplicationController 
+
+end
--- rails_curriculum/09_goal_app/goalApp/config/application.rb
+++ rails_curriculum/09_goal_app/goalApp/config/application.rb
@@ -15,15 +15,15 @@ module GoalApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
 
-  config.generators do |g|
-    g.test_framework :rspec,
+    config.generators do |g|
+      g.test_framework :rspec,
       :fixtures => false,
       :view_specs => false,
       :helper_specs => false,
       :routing_specs => false,
       :controller_specs => true,
       :request_specs => false
+    end
   end
 end
