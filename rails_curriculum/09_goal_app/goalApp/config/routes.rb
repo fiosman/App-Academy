a1c1817585d3b03e2dbb03b1cd338a270d0ecedb
Fares <fares.osman@outlook.com>
2019-12-24 21:01:34 -0500

    Add routes to session and users

--- rails_curriculum/09_goal_app/goalApp/config/routes.rb
+++ rails_curriculum/09_goal_app/goalApp/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :users, only: [:create, :new, :show]
+resource :session, only: [:create, :destroy, :new]
 end
