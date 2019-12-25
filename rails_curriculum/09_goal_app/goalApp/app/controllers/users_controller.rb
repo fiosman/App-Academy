6194e2b4d85590ac473202c72718247d0abb7b2d
Fares <fares.osman@outlook.com>
2019-12-24 22:12:32 -0500

    Write method/test for controller action new

--- rails_curriculum/09_goal_app/goalApp/app/controllers/users_controller.rb
+++ rails_curriculum/09_goal_app/goalApp/app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
 class UsersController < ApplicationController 
+  
+  def create
+    @user = User.new(user_params) 
+
+    if @user.save
+      redirect_to user_url(@user) 
+    else 
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  private 
+
+  def user_params 
+    params.require(:user).permit(:username, :password)
+  end
 
 end
--- rails_curriculum/09_goal_app/goalApp/spec/controllers/users_controller_spec.rb
+++ rails_curriculum/09_goal_app/goalApp/spec/controllers/users_controller_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do  
+
+  describe "POST #create" do   
+    context 'when valid params are provided' do   
+      it 'redirects user to user show page on success' do 
+        post :create, params: { user: { username: 'Christopher', password: 'god12345 ' }}
+        expect(response).to redirect_to(user_url(User.last))
+      end
+    end
+    context 'when invalid params are provided' do   
+      it 'renders new template if username/password is not provided' do   
+        post :create, params: { user: { username: 'Christopher' } }
+        expect(flash.now[:errors]).to be_present 
+        expect(response).to render_template(:new) 
+      end
+    end
+  end
 end
