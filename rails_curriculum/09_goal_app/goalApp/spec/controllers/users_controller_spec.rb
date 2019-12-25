b351f46aeb2e863daf4d6bbb3d64c3071ac5cb96
Fares <fares.osman@outlook.com>
2019-12-24 22:42:19 -0500

    Fix escaping

--- rails_curriculum/09_goal_app/goalApp/spec/controllers/users_controller_spec.rb
+++ rails_curriculum/09_goal_app/goalApp/spec/controllers/users_controller_spec.rb
@@ -27,13 +27,13 @@ RSpec.describe UsersController, type: :controller do
 
   describe "GET #show" do    
     context "if the user exists" do 
-      it "renders the user\'s' show template" do 
+      it "renders the user's show template" do 
         user = User.create!(username: 'Christopher', password: 'god12345') 
         get :show, params: { id: User.last.id } 
         expect(response).to render_template(:show)
       end
     end
-    context "if the user doesn\'t exist" do    
+    context "if the user doesn't exist" do    
       it "does not render the show template" do    
         begin
           get :show, params: { id: -1 }
