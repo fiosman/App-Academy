b65843e3baa217173ed8d8b02383bbcfd9ee8f94
Fares <fares.osman@outlook.com>
2019-12-28 19:49:31 -0500

    Finish writing template for #index

--- rails_curriculum/09_goal_app/goalApp/config/routes.rb
+++ rails_curriculum/09_goal_app/goalApp/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :users, only: [:create, :new, :show, :index]
 resource :session, only: [:create, :destroy, :new]
+
+root 'users#index'
 end
