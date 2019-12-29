126e22c66c565b79c8648ef1ec8d48a38cff9b74
Fares <fares.osman@outlook.com>
2019-12-28 23:29:24 -0500

    add route for goals

--- rails_curriculum/09_goal_app/goalApp/config/routes.rb
+++ rails_curriculum/09_goal_app/goalApp/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :users, only: [:create, :new, :show, :index]
 resource :session, only: [:create, :destroy, :new]
+resources :goals
 
 root 'users#index'
 end
