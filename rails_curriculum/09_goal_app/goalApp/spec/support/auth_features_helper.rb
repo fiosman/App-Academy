11889b9e9e5b08bd9c24890dd79703a28e737d30
Fares <fares.osman@outlook.com>
2019-12-28 19:56:29 -0500

    Modify login_as helper method

--- rails_curriculum/09_goal_app/goalApp/spec/support/auth_features_helper.rb
+++ rails_curriculum/09_goal_app/goalApp/spec/support/auth_features_helper.rb
@@ -10,6 +10,6 @@ module AuthFeaturesHelper
     visit new_session_url
     fill_in "Username", with: user.username
     fill_in "Password", with: user.password
-    click_button "Login"
+    click_on "Login"
   end
 end
