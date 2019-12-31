69efc0a2e092d32332b55672c2bc968117f5563f
Fares <fares.osman@outlook.com>
2019-12-30 23:39:52 -0500

    Set up specs template for goals CRUD feature

---
+++ rails_curriculum/09_goal_app/goalApp/spec/features/goals_crud_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "GoalsCrud", type: :feature do
+  feature 'creating goals' do   
+  end
+
+  feature 'reading goals' do    
+  end
+
+  feature 'updating goals' do    
+  end
+
+  feature 'deleting goals' do  
+  end 
+end
