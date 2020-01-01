0eb582dd505fba1d2dd5c871ecc8a514aba789fc
Fares <fares.osman@outlook.com>
2019-12-31 20:39:00 -0500

    Write test to check if goals new page exists

--- rails_curriculum/09_goal_app/goalApp/spec/features/goals_crud_spec.rb
+++ rails_curriculum/09_goal_app/goalApp/spec/features/goals_crud_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature "GoalsCrud", type: :feature do
+  given(:test_user) { create(:user) }
+
+  background(:each) do    
+    login_as(test_user)
+  end 
+
   feature 'creating goals' do   
+    scenario 'has a new goal page' do   
+      visit new_goal_url  
+      expect(page).to have_content("New Goal")
+    end
   end
 
   feature 'reading goals' do    
