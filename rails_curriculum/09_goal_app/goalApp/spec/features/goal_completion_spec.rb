9352b6196903555b11843b6917d43444b6076651
Fares <fares.osman@outlook.com>
2020-01-02 22:08:40 -0500

    Setup specs for goal completion

--- rails_curriculum/09_goal_app/goalApp/spec/features/goal_completion_spec.rb
+++ rails_curriculum/09_goal_app/goalApp/spec/features/goal_completion_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.feature "GoalCompletion", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  feature 'completing goal' do    
+    scenario 'on the goals index page' do   
+    end
+
+    scenario "on the logged in user's goals show page" do   
+    end
+
+    scenario "on another user's goals show page" do   
+    end 
+
+    scenario "on the logged in user's show page" do    
+    end
+
+    scenario "on another user's show page" do   
+    end 
+
+  end
 end
