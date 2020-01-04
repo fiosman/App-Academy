da2fe6e2c9feb95986cda06e0d8ae9af4c793b7d
Fares <fares.osman@outlook.com>
2020-01-04 15:30:09 -0500

    Finish writing tests for public goals

--- rails_curriculum/09_goal_app/goalApp/spec/features/goal_privacy_spec.rb
+++ rails_curriculum/09_goal_app/goalApp/spec/features/goal_privacy_spec.rb
@@ -6,25 +6,52 @@ RSpec.feature "GoalPrivacy", type: :feature do
     given!(:main_user) { create(:user) } 
     given!(:alt_user) { create(:user) } 
     given!(:main_goal) { create(:goal, user_id: main_user.id) }
-    given!(:alt_goal) { create(:goal, user_id: alt_user.id, visible: true) }
-    feature 'modifying visibility status' do  
-      scenario "does not allow user to edit visibility status of other users" do  
-        login_as(main_user) 
-        visit goal_url(alt_goal) 
-        save_and_open_page
+    given!(:alt_goal_private) { create(:goal, user_id: alt_user.id) }
+    given!(:alt_goal_public) { create(:goal, user_id: alt_user.id, visible: true) }
+
+    feature 'public goals' do 
+      background(:each) { login_as(main_user) } 
+
+      scenario 'does not allow user to edit other users public goals visibility' do
+        visit edit_goal_url(alt_goal_public) 
+        expect(page).to have_content('Not your goal to edit!')
+      end
+
+      scenario 'private status upon creation of a goal' do    
+       visit goal_url(main_goal) 
+       expect(page).to have_content('Private')
+      end
+
+      scenario 'allow user to edit own visibility status' do  
+        visit edit_goal_url(main_goal) 
+        check 'Public?'
+        click_on 'Update'
+        expect(page).to have_content('Goal updated!') 
+        expect(page).to have_content('Public') 
       end 
 
-      scenario 'allow user to edit own visibility status' do   
-      end 
+      scenario 'user can see other users goals' do   
+        visit user_url(alt_user) 
+        save_and_open_page
+        expect(page).to have_content('Public')
+        expect(page).to have_content(alt_goal_public.title)
+      end
+
     end
   
-    feature 'viewing goals' do   
+    feature 'private goals' do   
       scenario 'does not allow user to view other users private goals' do  
       end 
 
       scenario 'user can view own private goals' do    
       end
     end
+
+      # scenario "does not allow user to edit others private goals visibility" do  
+      #   visit edit_goal_url(alt_goal_private) 
+      #   expect(page).to have_content("Not your goal to edit!")
+      #   expect(page).to have_current_path("/users/#{alt_user.id}")
+      # end 
 
   end
 end
