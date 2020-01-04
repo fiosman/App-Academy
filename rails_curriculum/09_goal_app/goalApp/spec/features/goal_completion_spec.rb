9cfa425692250cabdc957c9f1a44382f202d0b82
Fares <fares.osman@outlook.com>
2020-01-03 21:19:08 -0500

    Test that goals are incomplete when they are initially created

--- rails_curriculum/09_goal_app/goalApp/spec/features/goal_completion_spec.rb
+++ rails_curriculum/09_goal_app/goalApp/spec/features/goal_completion_spec.rb
@@ -1,8 +1,28 @@
 require 'rails_helper'
 
 RSpec.feature "GoalCompletion", type: :feature do
-  feature 'completing goal' do    
-    scenario 'on the goals index page' do   
+  feature 'completing goal' do   
+    given!(:main_user) { create(:user) }
+    given!(:alt_user) { create(:user) } 
+    given!(:test_goal) { create(:goal, user_id: main_user.id) }
+
+    background(:each) do   
+      login_as(main_user) 
+    end
+
+    feature 'starts off incomplete' do  
+      scenario 'on the goals index page' do   
+        visit goals_url 
+        expect(page).to have_content('Ongoing')
+      end
+      scenario "on the logged in user's show page" do
+        visit user_url(main_user)
+        expect(page).to have_content('Ongoing')
+      end
+      scenario "on the logged in user's edit goal page" do  
+        visit edit_goal_url(test_goal) 
+        expect(page).to have_unchecked_field('goal_completed')
+      end
     end
 
     scenario "on the logged in user's goals show page" do   
@@ -13,7 +33,7 @@ RSpec.feature "GoalCompletion", type: :feature do
 
     scenario "on the logged in user's show page" do    
     end
-
+    
     scenario "on another user's show page" do   
     end 
 
--- rails_curriculum/09_goal_app/goalApp/spec/features/goal_privacy_spec.rb
+++ rails_curriculum/09_goal_app/goalApp/spec/features/goal_privacy_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature "GoalPrivacy", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  feature 'modifying visibility status' do   
+  end
+  
+  feature 'viewing goals' do   
+    scenario 'does not allow user to view other users private goals' do  
+    end 
+
+    scenario 'user can view own private goals' do    
+    end
+  end
 end
