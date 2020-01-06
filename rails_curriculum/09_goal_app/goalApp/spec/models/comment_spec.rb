d9a6a60d1e15ad1edf90178d309ecb8e7700e0c4
Fares <fares.osman@outlook.com>
2020-01-05 22:30:08 -0500

    Write tests for comment validations/associations

--- rails_curriculum/09_goal_app/goalApp/spec/models/comment_spec.rb
+++ rails_curriculum/09_goal_app/goalApp/spec/models/comment_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   
+  describe 'associations' do    
+    it { should belong_to(:commentable) }
+  end
+
+  describe 'validations' do    
+    it { should validate_presence_of(:comment) }
+  end
 end
