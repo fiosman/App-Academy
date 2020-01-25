175416a5f4ec357587617c6a67b9a45f761e2d27
Fares <fares.osman@outlook.com>
2020-01-24 23:47:10 -0500

    Write User#password

---
+++ rails_curriculum/17_reddit_clone/reddit_clone/app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ApplicationRecord 
+  validates :username, :session_token, presence: true, uniqueness: true 
+  validates :password_digest, presence: true 
+  validates :password, length: { minimum: 6, allow_nil: true } 
+
+  after_initialize :ensure_session_token 
+
+  attr_reader :password  
+  
+  def self.find_by_credentials(username, password) 
+  end   
+  
+  def self.generate_session_token 
+    SecureRandom::urlsafe_base64
+  end 
+
+  def self.reset_session_token! 
+    
+  end 
+
+  def password=(password)
+    self.passwor_digest = BCrypt::Password.create(password)
+  end 
+
+  def is_password?(password) 
+  end 
+
+  private 
+
+  def ensure_session_token 
+  end 
+
+
+
+end
