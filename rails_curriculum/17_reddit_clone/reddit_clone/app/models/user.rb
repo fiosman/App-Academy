a61f1909a953915b0d25f21a455926549a6aa143
Fares <fares.osman@outlook.com>
2020-01-25 18:27:47 -0500

    Set up routes

--- rails_curriculum/17_reddit_clone/reddit_clone/app/models/user.rb
+++ rails_curriculum/17_reddit_clone/reddit_clone/app/models/user.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  username        :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_session_token  (session_token) UNIQUE
+#  index_users_on_username       (username) UNIQUE
+#
+
 class User < ApplicationRecord 
   validates :username, :session_token, presence: true, uniqueness: true 
   validates :password_digest, presence: true 
--- rails_curriculum/17_reddit_clone/reddit_clone/config/routes.rb
+++ rails_curriculum/17_reddit_clone/reddit_clone/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :user, only: [:create, :new, :show, :index]
+  resource :session, only: [:destroy, :new, :create]
 end
