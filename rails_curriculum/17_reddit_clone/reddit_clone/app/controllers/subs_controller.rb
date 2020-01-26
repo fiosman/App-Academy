174d2ba8e682b0fac63240a30d3579f0fb7ae526
Fares <fares.osman@outlook.com>
2020-01-26 17:31:23 -0500

    Configure routing for Sub and template for controller

--- rails_curriculum/17_reddit_clone/reddit_clone/app/controllers/subs_controller.rb
+++ rails_curriculum/17_reddit_clone/reddit_clone/app/controllers/subs_controller.rb
@@ -1,2 +1,21 @@
 class SubsController < ApplicationController 
+
+  def index 
+  end 
+
+  def show 
+  end 
+
+  def create
+  end 
+  
+  def new 
+  end 
+
+  def edit 
+  end 
+
+  def update 
+  end
+  
 end 
