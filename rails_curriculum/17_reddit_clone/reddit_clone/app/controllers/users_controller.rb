f985fb444733cc56c40cc62add12a935c32d72ed
Fares <fares.osman@outlook.com>
2020-01-26 13:12:32 -0500

    Finish new template for user

--- rails_curriculum/17_reddit_clone/reddit_clone/app/controllers/users_controller.rb
+++ rails_curriculum/17_reddit_clone/reddit_clone/app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params) 
 
     if @user.save 
-      log_in_user(@user)
+      log_in_user!(@user)
       redirect_to user_url(@user) 
       flash[:notices] = ['Sign up successful!']  
     else 
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   end 
 
   def show 
-    User.find(id: params[:id]) 
+    @user = User.find(params[:id]) 
     render :show 
   end 
 
--- rails_curriculum/17_reddit_clone/reddit_clone/app/models/user.rb
+++ rails_curriculum/17_reddit_clone/reddit_clone/app/models/user.rb
@@ -40,6 +40,7 @@ class User < ApplicationRecord
   end 
 
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end 
 
