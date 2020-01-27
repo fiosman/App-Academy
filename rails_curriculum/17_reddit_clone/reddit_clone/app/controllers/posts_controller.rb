88b9ee3fde81f1588cdd9d59bcdb1e68bdf7153d
Fares <fares.osman@outlook.com>
2020-01-26 19:54:00 -0500

    Write controller actions for PostsController

---
+++ rails_curriculum/17_reddit_clone/reddit_clone/app/controllers/posts_controller.rb
@@ -0,0 +1,68 @@
+class Post < ApplicationController 
+
+  before_action :verify_author, only: [:edit, :update]
+
+  def create 
+    @post = current_user.posts.new(post_params) 
+    
+    if @post.save 
+      redirect_to post_url(@post) 
+      flash[:notices] = ['Post successfully added!']
+    else 
+      flash.now[:errors] = @post.errors.full_messages
+      render :new
+    end
+  end 
+
+  def new
+    @post = Post.new 
+    render :new 
+  end 
+
+  def show 
+    @post = Post.find(params[:id]) 
+    render :show 
+  end 
+
+  def edit 
+    @post = Post.find(params[:id]) 
+    render :edit 
+  end 
+
+  def update  
+    @post = Post.find(params[:id]) 
+
+    if @post.update_attributes(post_params)
+      redirect_to post_url(@post)  
+      flash[:notices] = ['Post successfully updated'] 
+    else 
+      flash.now[:errors] = @post.errors.full_messages 
+      render :edit 
+    end
+  end 
+
+  def destroy 
+    @post = Post.find(params[:id]) 
+    
+    if @post.destroy 
+      flash[:notices] = ['Post deleted!']
+    end 
+
+    redirect_to subs_url
+  end
+
+  private 
+
+  def post_params 
+    params.require(:post).permit(:title, :content, :url, :sub_id)
+  end 
+
+  def verify_author 
+    post = Post.find(params[:id]) 
+    if current_user != post.author 
+      flash[:notices] = ['Cannot edit a post that is not your own!']
+      redirect_to edit_post_url(post)
+    end
+  end 
+
+end
--- rails_curriculum/17_reddit_clone/reddit_clone/config/routes.rb
+++ rails_curriculum/17_reddit_clone/reddit_clone/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :new, :show, :index]
   resource :session, only: [:destroy, :new, :create]
   resources :subs, except: [:destroy]
+  resources :posts, except: [:index]
 end
