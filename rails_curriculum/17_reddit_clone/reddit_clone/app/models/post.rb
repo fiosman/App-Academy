e54908d068a4443899f842e8c4e33b78195fd2fe
Fares <fares.osman@outlook.com>
2020-01-29 01:38:12 -0500

    Write associations for Post

--- rails_curriculum/17_reddit_clone/reddit_clone/app/models/post.rb
+++ rails_curriculum/17_reddit_clone/reddit_clone/app/models/post.rb
@@ -19,7 +19,10 @@
 class Post < ApplicationRecord
   validates :title, :sub_id, :author_id, presence: true
 
-  belongs_to :sub
+  has_many :post_subs 
+  has_many :subs, 
+    through: :post_subs 
+    source: :sub 
   belongs_to :author, 
     class_name: :User
 end 
