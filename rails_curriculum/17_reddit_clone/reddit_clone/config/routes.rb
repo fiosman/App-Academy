0440639b89cdfdee45e6ef241d148d2d156df2f7
Fares <fares.osman@outlook.com>
2020-01-30 11:08:00 -0500

    Set up routes for comments

--- rails_curriculum/17_reddit_clone/reddit_clone/config/routes.rb
+++ rails_curriculum/17_reddit_clone/reddit_clone/config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :new, :show, :index]
   resource :session, only: [:destroy, :new, :create]
   resources :subs, except: [:destroy]
-  resources :posts, except: [:index]
+  resources :comments, only: [:create]
+  resources :posts, except: [:index] do   
+    resources :comments, only: [:new] 
+  end 
 end
