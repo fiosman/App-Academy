691843c9ca7bc83f66fd03ff331cb358313323fe
Fares <fares.osman@outlook.com>
2020-01-31 23:50:25 -0500

    Update comments associations to include child_comments

--- rails_curriculum/17_reddit_clone/reddit_clone/app/models/comment.rb
+++ rails_curriculum/17_reddit_clone/reddit_clone/app/models/comment.rb
@@ -26,4 +26,9 @@ class Comment < ApplicationRecord
     class_name: :User  
     
   belongs_to :post 
+
+  has_many :child_comments, 
+    primary_key: :id,   
+    foreign_key: :parent_comment_id, 
+    class_name: :Comment 
 end
