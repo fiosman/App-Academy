9a39fdcba0cef9e0af889b5f62d1b8f72905d2b8
Fares <fares.osman@outlook.com>
2020-02-03 23:56:17 -0500

    Write first part of p01

--- rails_curriculum/18_rails_lite/bin/p01_basic_server.rb
+++ rails_curriculum/18_rails_lite/bin/p01_basic_server.rb
@@ -1,1 +1,14 @@
 require 'rack'
+
+app = Proc.new do |env| 
+  req = Rack::Request.new(env) 
+  res = Rack::Response.new 
+  res['Content-Type'] = 'text/html'
+  res.write("Hello world!")
+  res.finish
+end
+
+Rack::Server.start( 
+  app: app, 
+  Port: 3000
+)
