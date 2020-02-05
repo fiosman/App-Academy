27963d62be8a877c968a7110a3fe490df68c2b5a
Fares <fares.osman@outlook.com>
2020-02-04 23:53:44 -0500

    Change output to requested url

--- rails_curriculum/18_rails_lite/bin/p01_basic_server.rb
+++ rails_curriculum/18_rails_lite/bin/p01_basic_server.rb
@@ -4,7 +4,7 @@ app = Proc.new do |env|
   req = Rack::Request.new(env) 
   res = Rack::Response.new 
   res['Content-Type'] = 'text/html'
-  res.write("Hello world!")
+  res.write(env["PATH_INFO"])
   res.finish
 end
 
