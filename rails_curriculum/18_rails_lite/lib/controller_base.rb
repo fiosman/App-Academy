f1585d0b4684ab1562e21bf63f605c00a0f28d9a
Fares <fares.osman@outlook.com>
2020-02-07 21:31:39 -0500

    Construct template path

--- rails_curriculum/18_rails_lite/lib/controller_base.rb
+++ rails_curriculum/18_rails_lite/lib/controller_base.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 require_relative './session'
 require 'rack'
@@ -41,6 +42,8 @@ class ControllerBase
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    template_file = File.dirname(File.join
+    ("views", "#{self.to_s.underscore}_controller", "#{template_name}")."html.erb")
   end
 
   # method exposing a `Session` object
