8139f6ed5a51427e6bbc817a3c24df8198e9d581
Fares <fares.osman@outlook.com>
2020-02-08 23:55:13 -0500

    Write Session#Initialize, #[] and #[]=

--- rails_curriculum/18_rails_lite/lib/session.rb
+++ rails_curriculum/18_rails_lite/lib/session.rb
@@ -4,12 +4,20 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    req_cookie = req.cookies['_rails_lite_app']
+    if req_cookie
+      @cookie_data = JSON.parse(req_cookie)
+    else 
+      @cookie_data = {}
+    end
   end
 
   def [](key)
+    @cookie_data[key]
   end
 
   def []=(key, val)
+    @cookie_data[key] = val 
   end
 
   # serialize the hash into json and save in a cookie
