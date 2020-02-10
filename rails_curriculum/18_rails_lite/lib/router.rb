1b026ad5581e465ae42f44b6f144efb0530ae04d
Fares <fares.osman@outlook.com>
2020-02-09 20:52:08 -0500

    Write Route#initialize

--- rails_curriculum/18_rails_lite/lib/router.rb
+++ rails_curriculum/18_rails_lite/lib/router.rb
@@ -2,6 +2,10 @@ class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern = pattern 
+    @http_method = http_method 
+    @controller_class = controller_class
+    @action_name = action_name
   end
 
   # checks if pattern matches path and method matches request method
