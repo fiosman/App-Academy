8d15b5c6b713f05d9a6566d11ff9d64d950248f5
Fares <fares.osman@outlook.com>
2020-02-12 12:33:33 -0500

    Add boiler plate to creating flash middleware

--- rails_curriculum/18_rails_lite/lib/flash.rb
+++ rails_curriculum/18_rails_lite/lib/flash.rb
@@ -1,8 +1,17 @@
 require 'json'
 
 class Flash
-
   def initialize(req)
+    req_cookie = req.cookies['_rails_lite_app_flash']
   end 
 
-end
+  def [](key) 
+  end 
+
+  def []=(key, val) 
+  end 
+
+  def store_flash(res) 
+  end
+
+ end
