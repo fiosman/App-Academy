7373a6f154e8eccd636eb08289954bd52dbe599b
Fares <fares.osman@outlook.com>
2020-02-13 23:48:11 -0500

    Write Flash#[] and Flash#[]=

--- rails_curriculum/18_rails_lite/lib/flash.rb
+++ rails_curriculum/18_rails_lite/lib/flash.rb
@@ -3,12 +3,15 @@ require 'json'
 class Flash
   def initialize(req)
     req_cookie = req.cookies['_rails_lite_app_flash']
+    @flash_store= {}
   end 
 
   def [](key) 
+    @flash_store[key]
   end 
 
   def []=(key, val) 
+    @flash_store[key] = val
   end 
 
   def store_flash(res) 
