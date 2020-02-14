4e58e64414bdecb5827fa2c2e19876ccdab056eb
Fares <fares.osman@outlook.com>
2020-02-14 00:51:07 -0500

    Finish Flash

--- rails_curriculum/18_rails_lite/lib/flash.rb
+++ rails_curriculum/18_rails_lite/lib/flash.rb
@@ -1,20 +1,33 @@
 require 'json'
 
 class Flash
+  attr_reader :now
+
   def initialize(req)
     req_cookie = req.cookies['_rails_lite_app_flash']
-    @flash_store= {}
+    @flash_store = {}
+    @now = {} 
+
+    if req_cookie 
+      @now = JSON.parse(req_cookie)
+    end
   end 
 
   def [](key) 
-    @flash_store[key]
+    flashes = @flash_store.merge(@now) 
+    flashes[key.to_s]
   end 
 
   def []=(key, val) 
-    @flash_store[key] = val
+    @flash_store[key] = val || @now[key] = val
   end 
 
   def store_flash(res) 
+    res.set_cookie(
+      '_rails_lite_app_flash', 
+        value: @flash_store.to_json, 
+        path: "/"
+    )
   end
 
  end
