839d7827370326e575b4642f26346ec13fad7f15
Fares <fares.osman@outlook.com>
2020-02-16 20:16:28 -0500

    dummy testing

--- rails_curriculum/18_rails_lite/lib/static.rb
+++ rails_curriculum/18_rails_lite/lib/static.rb
@@ -1,7 +1,19 @@
+require 'rack'
+
 class Static
   def initialize(app)
+    @app = app 
   end
 
   def call(env)
+    req = Rack::Request.new(env) 
+    res = Rack::Response.new 
+
+    path = '/public/hello.txt'
+    if req.path == path    
+      res.write(File.read('./public/hello.txt'))
+      res.finish
+    end
   end
+  
 end
