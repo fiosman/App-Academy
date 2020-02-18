edc477cce4709dc39d0c5e11c9d7ef106c2c9614
Fares <fares.osman@outlook.com>
2020-02-17 19:01:44 -0500

    Add Mime Types to Content-Type headers

--- rails_curriculum/18_rails_lite/lib/static.rb
+++ rails_curriculum/18_rails_lite/lib/static.rb
@@ -10,15 +10,23 @@ class Static
     res = Rack::Response.new 
 
     file_path = (File.join(File.dirname(__FILE__), '..', "#{req.path}")) 
+    file_extension = get_file_extension(req.path).captures.first
 
     begin 
       file_content = File.read(file_path)
     rescue SystemCallError => e         
       res.status = 404 
     else 
-      res.write(file_content) 
+      res['Content-Type'] = Rack::Mime.mime_type(file_extension)
+      res.write(file_content)
     end
     res.finish
   end
 
+
+  private 
+
+  def get_file_extension(route_pattern) 
+    route_pattern.match(/(\..*)/)
+  end
 end
