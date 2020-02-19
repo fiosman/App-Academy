ee7770314648716eb2a5699d716cdca5a89fc9f6
Fares <fares.osman@outlook.com>
2020-02-19 00:09:49 -0500

    Finish Phase I

--- javascript_curriculum/01_intro_javascript_problems_p1/problems.js
+++ javascript_curriculum/01_intro_javascript_problems_p1/problems.js
@@ -1,54 +1,54 @@
-// // Phase I 
+// Phase I 
 
-// function mysteryScoping1() {
-//     var x = 'out of block';
-//     if (true) {
-//         var x = 'in block';
-//         console.log(x);
-//     }
-//     console.log(x);
-// }
+function mysteryScoping1() {
+    var x = 'out of block';
+    if (true) {
+        var x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
 
-// function mysteryScoping2() {
-//     const x = 'out of block';
-//     if (true) {
-//         const x = 'in block';
-//         console.log(x);
-//     }
-//     console.log(x);
-// }
+function mysteryScoping2() {
+    const x = 'out of block';
+    if (true) {
+        const x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
 
-// function mysteryScoping3() {
-//     const x = 'out of block';
-//     if (true) {
-//         var x = 'in block';
-//         console.log(x);
-//     }
-//     console.log(x);
-// }
+function mysteryScoping3() {
+    const x = 'out of block';
+    if (true) {
+        var x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
 
-// function mysteryScoping4() {
-//     let x = 'out of block';
-//     if (true) {
-//         let x = 'in block';
-//         console.log(x);
-//     }
-//     console.log(x);
-// }
+function mysteryScoping4() {
+    let x = 'out of block';
+    if (true) {
+        let x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
 
-// function mysteryScoping5() {
-//     let x = 'out of block';
-//     if (true) {
-//         let x = 'in block';
-//         console.log(x);
-//     }
-//     let x = 'out of block again';
-//     console.log(x);
-// }
+function mysteryScoping5() {
+    let x = 'out of block';
+    if (true) {
+        let x = 'in block';
+        console.log(x);
+    }
+    let x = 'out of block again';
+    console.log(x);
+}
 
 const madLib = (verb, adjective, noun) => { 
     return `We shall ${verb.toUpperCase()} the ${adjective.toUpperCase()} ${noun.toUpperCase()}`;
 } 
 
-console.log(madLib('fuck', 'you', 'mike'));
+
 
