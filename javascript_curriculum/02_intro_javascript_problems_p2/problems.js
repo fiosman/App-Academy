c5f88d41a18c5ef27976650d7dde4f65d4114416
Fares <fares.osman@outlook.com>
2020-02-20 23:56:39 -0500

    Finish Phase II

--- javascript_curriculum/02_intro_javascript_problems_p2/problems.js
+++ javascript_curriculum/02_intro_javascript_problems_p2/problems.js
@@ -9,4 +9,15 @@ const printNames = (names) => {
   names.forEach(name => console.log(name));
 }
 
+// Phase II 
 
+function Elephant(name, height, tricks) { 
+  this.name = name; 
+  this.height = height; 
+  this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = () => `${this.name} the elephant goes phrRRRRRRRRRRR!!!!!!!`;
+Elephant.prototype.grow = () => this.height += 12; 
+Elephant.prototype.addTrick = (trick) => this.tricks.push(trick); 
+Elephant.prototype.play = () => `${this.name} is ${this.tricks[Math.floor(Math.random() * this.tricks.length)]}`
