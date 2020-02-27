446dfe1ad0a761f10c766cd3d9f427239ba21ccd
Fares <fares.osman@outlook.com>
2020-02-27 00:10:34 -0500

    Fix Phase II

--- javascript_curriculum/05_intro_javascript_exercises/phase_2_enumerable.js
+++ javascript_curriculum/05_intro_javascript_exercises/phase_2_enumerable.js
@@ -1,25 +1,15 @@
-function Array(array) { 
-  this.array = array;
-}
-
-Array.prototype.myEach = function(callback) { 
-  for (let i = 0; i < this.array.length; i++) { 
-    callback(this.array[i]); 
+Array.prototype.myEach = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i]);
   }
 }
 
-Array.prototype.myMap = function(callback) { 
-  let newArr = []; 
+Array.prototype.myMap = function (callback) {
+  let newArr = [];
 
-  this.array.myEach(function(ele) { 
-    newArr.push(callback(ele)); 
+  this.myEach(function (ele) {
+    newArr.push(callback(ele));
   })
 
-  return newArr; 
+  return newArr;
 }
-
-arr = new Array(['i', 'hate', 'you']) 
-
-console.log(arr.myMap(function(ele) { 
-  return ele.upcase; 
-}))
