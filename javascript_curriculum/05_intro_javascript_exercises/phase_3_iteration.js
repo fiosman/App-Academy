58d3549d26317c226cf2819cb8ff47abc9244c32
Fares <fares.osman@outlook.com>
2020-02-29 23:01:07 -0500

    Write String#substrings

---
+++ javascript_curriculum/05_intro_javascript_exercises/phase_3_iteration.js
@@ -0,0 +1,11 @@
+String.prototype.substrings = function () {
+  let substrings = [];
+
+  for (let i = 0; i < this.length; i++) {
+    for (let j = i + 1; j <= this.length; j++) {
+      substrings.push(this.slice(i, j));
+    }
+  }
+
+  return substrings;
+}
