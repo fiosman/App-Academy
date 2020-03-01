f8d5467f2d5b55d69e7d0f8135d8dae8380f915e
Fares <fares.osman@outlook.com>
2020-03-01 00:09:59 -0500

    Finish Array#bubbleSort

--- javascript_curriculum/05_intro_javascript_exercises/phase_3_iteration.js
+++ javascript_curriculum/05_intro_javascript_exercises/phase_3_iteration.js
@@ -9,3 +9,22 @@ String.prototype.substrings = function () {
 
   return substrings;
 }
+
+Array.prototype.bubbleSort = function() { 
+  let swapped; 
+
+  do {
+   swapped = false;
+   for (let i = 0; i < this.length; i++) {
+     if (this[i] > this[i + 1]) {
+       const temp = this[i];
+       this[i] = this[i + 1];
+       this[i + 1] = temp;
+       swapped = true;
+     }
+    }
+  }
+  while(swapped === true );
+  
+  return this;
+}
