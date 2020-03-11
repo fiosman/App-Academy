5e9ef854111142483c1fdd983110a35821887f11
Fares <fares.osman@outlook.com>
2020-03-10 23:52:58 -0400

    Incorrect tea and biscuits solution

--- javascript_curriculum/07_callbacks_function_calls/tea_biscuits.js
+++ javascript_curriculum/07_callbacks_function_calls/tea_biscuits.js
@@ -5,3 +5,21 @@ const reader = readline.createInterface({
   output: process.stdout
 }); 
 
+function teaAndBiscuits() { 
+  let teaAns, biscuitAns; 
+  reader.question('Would you like some tea?', res => { 
+    teaAns = res; 
+    console.log(`You replied ${teaAns}`);
+  });
+  reader.question('Would you like some biscuits?', res => { 
+    biscuitAns = res;
+    console.log(`You replied ${biscuitAns}`);
+  });
+
+  let biscuitOrder = biscuitAns === 'yes' ? 'do' : 'don\'t';
+  let teaOrder = teaAns === 'yes' ? 'do' : 'don\'t';
+  console.log(`So you ${teaOrder} want some tea and you ${biscuitOrder} want biscuits.`)
+  reader.close();
+}
+
+teaAndBiscuits();
