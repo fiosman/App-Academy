a05dd0a892aa8ab6c69d105d670fc49b81112891
Fares <fares.osman@outlook.com>
2020-03-15 22:44:31 -0400

    Finish myBind

---
+++ javascript_curriculum/10_intro_callbacks_exercises/myBind.js
@@ -0,0 +1,23 @@
+Function.prototype.myBind = function(context) { 
+  return () => this.apply(context, null); 
+}
+
+class Lamp {
+  constructor() {
+    this.name = "a lamp";
+  }
+}
+
+const turnOn = function () {
+  console.log("Turning on " + this.name);
+};
+
+const lamp = new Lamp();
+
+turnOn(); // should not work the way we want it to
+
+const boundTurnOn = turnOn.bind(lamp);
+const myBoundTurnOn = turnOn.myBind(lamp);
+
+boundTurnOn(); // should say "Turning on a lamp"
+myBoundTurnOn(); // should say "Turning on a lamp"
