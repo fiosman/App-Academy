f3150fa32079683450abf0dee143163ea9285735
Fares <fares.osman@outlook.com>
2020-03-17 22:14:35 -0400

    Write #promptMove

--- javascript_curriculum/11_towers_of_hanoi/game.js
+++ javascript_curriculum/11_towers_of_hanoi/game.js
@@ -1,6 +1,24 @@
-const readline = require('readline'); 
+// This is for playScript.js
+/* const readline = require('readline'); 
 const reader = readline.createInterface({ 
   input: process.stdin, 
   output: process.stdout
 }); 
+ */
 
+class Game { 
+  constructor() { 
+    this.towers = [[3,2,1], [], []]; 
+  }
+
+  promptMove(reader, callback) { 
+    console.log(this.towers); 
+    reader.question('Which tower are you moving disk from (0, 1, 2)?', start => { 
+      const startTowerIdx = parseInt(start);
+      reader.question('Which tower are you moving disk to (0, 1, 2)?', end => { 
+        const endTowerIdx = parseInt(end);
+        callback(startTowerIdx, endTowerIdx);
+      });
+    });
+  }
+}
