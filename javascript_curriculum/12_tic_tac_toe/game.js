ee7731aa829a9b1fd1c0d49416354f0f107826be
Fares <fares.osman@outlook.com>
2020-03-22 16:39:22 -0400

    Set up structure for Game class

--- javascript_curriculum/12_tic_tac_toe/game.js
+++ javascript_curriculum/12_tic_tac_toe/game.js
@@ -3,9 +3,32 @@ const Board = require('./board')
 class Game { 
   constructor(reader) { 
    this.reader = reader; 
+   this.currentPlayer = 'X'; 
+   this.board = new Board();
   }
 
   run(reader, completionCallback) {
+
+  }
+
+  announceWinner() { 
+
+  }
+
+  promptMove() { 
+
+  }
+
+  swapTurns() { 
+
+  }
+
+  playMove(pos) { 
+
+  }
+
+  gameOver() { 
+    return this.board.isOver();
   }
 }
 
