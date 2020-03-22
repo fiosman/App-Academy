1cd49f42b17625d786d3171e115a9cef6f76ca02
Fares <fares.osman@outlook.com>
2020-03-21 20:48:07 -0400

    Write #placeMark and #empty

--- javascript_curriculum/12_tic_tac_toe/board.js
+++ javascript_curriculum/12_tic_tac_toe/board.js
@@ -1,26 +1,42 @@
-class Board { 
-  constructor() { 
+class Board {
+  constructor() {
     this.board = Board.makeGrid();
   }
 
-  static makeGrid() { 
-    let grid = []; 
-    for (let i = 0; i < 3; i++) { 
+  placeMark(pos, mark) { 
+    const [row, col] = pos; 
+
+    if (!this.empty(pos)) { 
+      console.log('Invalid position!');
+    } else { 
+      this.board[row][col] = mark;
+    }
+  }
+
+  empty(pos) { 
+    const [row, col] = pos; 
+    return this.board[row][col] === null; 
+  }
+
+  printGrid() {
+    this.board.forEach(function(row) {
+      console.log(row.join("   "));
+    });
+  }
+
+  static makeGrid() {
+    let grid = [];
+    for (let i = 0; i < 3; i++) {
       grid.push([]);
-      for (let j = 0; j < 3; j++) { 
-        grid[i].push("*"); 
+      for (let j = 0; j < 3; j++) {
+        grid[i].push("*");
       }
     }
     return grid;
   }
-
-  printGrid() { 
-    this.board.forEach(function(row) { 
-      console.log(row.join("   "));
-    })
-  }
-
 }
 
-board = new Board(); 
+board = new Board();
+board.printGrid();
+board.placeMark([1,2], 'X');
 board.printGrid();
