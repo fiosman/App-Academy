9a2db42247605ac65ff5493ee0bfad6c8434b800
Fares <fares.osman@outlook.com>
2020-03-22 00:56:24 -0400

    write #winner and #isOver

--- javascript_curriculum/12_tic_tac_toe/board.js
+++ javascript_curriculum/12_tic_tac_toe/board.js
@@ -18,6 +18,47 @@ class Board {
     return this.board[row][col] === null; 
   }
 
+  winner() { 
+    //check horizontal win 
+    for (let row = 0; row < 3; row++) { 
+      if (this.board[row][0] === this.board[row][1] && 
+        this.board[row][1] === this.board[row][2]) { 
+          return this.board[row][0];
+        }
+      }
+    //check vertical win 
+    for (let col = 0; col < 2; col++) { 
+      if (this.board[0][col] === this.board[1][col] && 
+        this.board[1][col] === this.board[2][col])
+        return this.board[0][col]; 
+    }
+    //check diagonal win 
+    if ((this.board[0][0] === this.board[1][1] && 
+      this.board[2][2] === this.board[0][0]) || 
+      (this.board[2][0] === this.board[1][1] && 
+      this.board[0][2] === this.board[2][0])) { 
+        return this.board[0][0];
+      }
+
+      return null;
+  }
+
+  isOver() { 
+    if (this.winner()) { 
+      return true; 
+    }
+
+    for (let row = 0; row < 3; row++) { 
+      for (let col = 0; col < 3; col++) { 
+        if (this.grid[row][col].empty()) { 
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
   printGrid() {
     this.board.forEach(function(row) {
       console.log(row.join("   "));
