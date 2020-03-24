fa0ba9be63a83deb5a8dd17b97c24f6d6eb53701
Fares <fares.osman@outlook.com>
2020-03-23 20:26:30 -0400

    Finish Phase i

--- javascript_curriculum/13_intro_to_canvas/practice.js
+++ javascript_curriculum/13_intro_to_canvas/practice.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+  const myCanvas = document.getElementById('mycanvas'); 
+  myCanvas.width = 500; 
+  myCanvas.height = 500;
+  const ctx = myCanvas.getContext('2d');
 });
