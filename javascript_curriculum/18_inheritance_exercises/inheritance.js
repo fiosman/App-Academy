721c2d19ed1be6e7712b4cfc9a0bc927a7d3ab86
Fares <fares.osman@outlook.com>
2020-03-26 00:58:31 -0400

    Implement inheritance using Object.create approach

--- javascript_curriculum/18_inheritance_exercises/inheritance.js
+++ javascript_curriculum/18_inheritance_exercises/inheritance.js
@@ -1,7 +1,14 @@
+//Surrogate approach
 Function.prototype.inherits = function(parent) { 
   function Surrogate() {}; 
   Surrogate.prototype = parent.prototype; 
   this.prototype = new Surrogate(); 
   this.prototype.constructor = this; 
 }
+
+//Object.create approach
+Function.prototype.inherits = function(parent) {
+  this.prototype = Object.create(parent.prototype);
+  this.prototype.constructor = this;
+};
 
