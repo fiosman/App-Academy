ba7938e51b99313f9f559bc4ff7742a0116d8ffa
Fares <fares.osman@outlook.com>
2020-03-28 02:08:32 -0400

    Write Game#addAsteroids

--- javascript_curriculum/19_asteroids/src/game.js
+++ javascript_curriculum/19_asteroids/src/game.js
@@ -2,14 +2,19 @@ const Asteroid = require("./asteroid.js");
 const MovingObject = require("./moving_object.js");
 
 function Game() { 
+  this.asteroids = addAsteroids(); 
 }
 
 Game.DIM_X = 1000;
 Game.DIM_Y = 600;
 Game.NUM_ASTEROIDS = 50;
 
-Game.prototype.addAsteroids = function() { 
+Game.prototype.addAsteroids = function() {
+  let asteroids = [];  
 
+  for (let i = 0; i < Game.NUM_ASTEROIDS; i++) { 
+    asteroids.push(new Asteroid({pos: this.randomPosition()})); 
+  }
 }
 
 Game.prototype.randomPosition = function() { 
@@ -17,4 +22,10 @@ Game.prototype.randomPosition = function() {
     Math.floor(Math.random() * Game.DIM_X), 
     Math.floor(Math.random() * Game.DIM_Y)
   ];
+}
+
+Game.prototype.draw = function(ctx) { 
+}
+
+Game.prototype.moveObjects = function() { 
 }
