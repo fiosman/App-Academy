45b81d50984952c2045558cc558653ff34659e02
Fares <fares.osman@outlook.com>
2020-03-29 02:25:41 -0400

    Write logic to move ship

--- javascript_curriculum/19_asteroids/src/game_view.js
+++ javascript_curriculum/19_asteroids/src/game_view.js
@@ -10,7 +10,27 @@ GameView.prototype.start = function() {
   setInterval(function() { 
     game.step();  
     game.draw(self.ctx); 
+    self.bindKeyHandlers();
   }, 20)
+}
+
+GameView.prototype.bindKeyHandlers = function() { 
+  const impulseKeys = { 
+    'w': [0, -0.08], 
+    's': [0, 0.08], 
+    'a': [-0.08, 0], 
+    'd': [0.08, 0]
+  }; 
+
+  if (key.isPressed('w')) { 
+    game.ship.power(impulseKeys['w']);
+  } else if (key.isPressed('s')) { 
+    game.ship.power(impulseKeys["s"]);
+  } else if (key.isPressed('a')) { 
+    game.ship.power(impulseKeys["a"]);
+  } else if (key.isPressed('d')) { 
+    game.ship.power(impulseKeys["d"]);
+  }
 }
 
 module.exports = GameView;
