5b4fa60c842fda070280028129f43f86e071620e
Fares <fares.osman@outlook.com>
2020-04-10 20:22:05 -0400

    Write Snake#turnand set up constructor

--- javascript_curriculum/25_jquery_snake/src/snake.js
+++ javascript_curriculum/25_jquery_snake/src/snake.js
@@ -1,5 +1,25 @@
 class Snake {
-  constructor() {}
+  constructor() {
+    this.direction = DIRS.R; 
+    this.segments = []; 
+  }
+
+  move() { 
+
+  }
+
+  turn(direction) { 
+    this.direction = direction; 
+  }
+  
 }
+
+const DIRS = { 
+  R: [1, 0], 
+  L: [-1, 0], 
+  U: [0, 1], 
+  D: [0, -1]
+}; 
+
 
 module.exports = Snake;
