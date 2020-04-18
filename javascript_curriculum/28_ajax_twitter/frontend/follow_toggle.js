747b3acf521540528f7ea88cc3c82f783a7c42e9
Fares <fares.osman@outlook.com>
2020-04-18 19:23:34 -0400

    write FollowToggle#handleClick

--- javascript_curriculum/28_ajax_twitter/frontend/follow_toggle.js
+++ javascript_curriculum/28_ajax_twitter/frontend/follow_toggle.js
@@ -1,17 +1,53 @@
-class FollowToggle { 
-  constructor($el) { 
-    this.userId = $el.data("user-id"); 
-    this.followState = $el.data("initial-follow-state"); 
-    this.$el = $el; 
-    this.render(); 
+class FollowToggle {
+  constructor($el) {
+    this.userId = $el.data("user-id");
+    this.followState = $el.data("initial-follow-state");
+    this.$el = $el;
+    this.render();
+    this.handleClick();
   }
 
-  render() { 
-    if (this.followState === 'unfollowed') { 
-      this.$el.text('Follow!'); 
-    } else if (this.followState === "followed") { 
-      this.$el.text('Unfollow'); 
+  render() {
+    if (this.followState === "unfollowed") {
+      this.$el.text("Follow!");
+    } else if (this.followState === "followed") {
+      this.$el.text("Unfollow");
     }
+  }
+
+  handleClick() {
+    let self = this; 
+
+    this.$el.on("click", function (e) {
+      e.preventDefault();
+      if (self.followState === "unfollowed") {
+        $.ajax({
+          url: `/users/${self.userId}/follow`,
+          type: "POST",
+          dataType: "json",
+          success: function () {
+            self.followState = "followed";
+            self.render();
+          },
+          error: function (e) {
+            console.log(e);
+          },
+        });
+      } else {
+        $.ajax({
+          url: `/users/${self.userId}/follow`,
+          type: "DELETE",
+          dataType: "json",
+          success: function () {
+            self.followState = "unfollowed";
+            self.render();
+          },
+          error: function (e) {
+            console.log(e);
+          },
+        });
+      }
+    })
   }
 }
 
