a91846e77ec094acdefae5edc8ba49979ed77939
Fares <fares.osman@outlook.com>
2020-04-18 14:44:33 -0400

    write FollowToggle#render

--- javascript_curriculum/28_ajax_twitter/app/assets/javascripts/bundle.js
+++ javascript_curriculum/28_ajax_twitter/app/assets/javascripts/bundle.js
@@ -98,6 +98,15 @@ class FollowToggle {
     this.userId = $el.data("user-id"); 
     this.followState = $el.data("initial-follow-state"); 
     this.$el = $el; 
+    this.render(); 
+  }
+
+  render() { 
+    if (this.followState === 'unfollowed') { 
+      this.$el.text('Follow!'); 
+    } else if (this.followState === "followed") { 
+      this.$el.text('Unfollow'); 
+    }
   }
 }
 
