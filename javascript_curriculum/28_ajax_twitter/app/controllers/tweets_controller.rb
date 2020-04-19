4d89135ccb8314518351f89340c2b772237d0f23
Fares <fares.osman@outlook.com>
2020-04-19 15:03:46 -0400

    Update create action for tweets controller

--- javascript_curriculum/28_ajax_twitter/app/controllers/tweets_controller.rb
+++ javascript_curriculum/28_ajax_twitter/app/controllers/tweets_controller.rb
@@ -7,8 +7,11 @@ class TweetsController < ApplicationController
 
     @tweet = current_user.tweets.build(tweet_params)
 
-    if @tweet.save
-      redirect_to request.referrer
+    if @tweet.save 
+      respond_to do |format| 
+        format.html { redirect_to request.referrer} 
+        format.json { render :show}
+      end
     else
       # Lazy: even respond with JSON to invalid HTML request.
       render json: @tweet.errors.full_messages, status: 422
