ca4b845b93473b1fc29aac701269912bf1060a38
Fares <fares.osman@outlook.com>
2020-04-25 23:05:52 -0400

    Write Phase II

--- javascript_curriculum/29_vanilla_dom_manipulation/script.js
+++ javascript_curriculum/29_vanilla_dom_manipulation/script.js
@@ -14,18 +14,22 @@ document.addEventListener("DOMContentLoaded", () => {
     li.addEventListener("click", toggleLi);
   });
 
-
-
   // adding SF places as list items
 
-  // --- your code here!
+  const submitButton = document.querySelector(".favorite-submit");
+  const inputField = document.querySelector(".favorite-input");
+  const restaurants = document.getElementById("restaurants");
 
-
+  submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const input = document.createTextNode(`${inputField.value}`);
+    const newItem = document.createElement("li");
+    newItem.appendChild(input);
+    restaurants.appendChild(newItem);
+    inputField.value = "";
+  });
 
   // adding new photos
 
   // --- your code here!
-
-
-
 });
