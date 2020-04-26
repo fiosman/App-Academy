90644e1df8f24b8896d2bb1afe8412d008f98097
Fares <fares.osman@outlook.com>
2020-04-25 22:25:17 -0400

    First commit

---
+++ javascript_curriculum/29_vanilla_dom_manipulation/script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // toggling restaurants
+
+  const toggleLi = (e) => {
+    const li = e.target;
+    if (li.className === "visited") {
+      li.className = "";
+    } else {
+      li.className = "visited";
+    }
+  };
+
+  document.querySelectorAll("#restaurants li").forEach((li) => {
+    li.addEventListener("click", toggleLi);
+  });
+
+
+
+  // adding SF places as list items
+
+  // --- your code here!
+
+
+
+  // adding new photos
+
+  // --- your code here!
+
+
+
+});
