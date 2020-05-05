1fdf5ccd7994a67463e98bc4c7df4479bddcc370
Fares <fares.osman@outlook.com>
2020-05-04 20:58:17 -0400

    First commit

---
+++ react_curriculum/01_npm/congrats.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Congrats = () => <h1>Congratulations, you did it!</h1>;
+
+export default Congrats;
---
+++ react_curriculum/01_npm/entry.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Congrats from './congrats';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<Congrats/>, root);
+});
