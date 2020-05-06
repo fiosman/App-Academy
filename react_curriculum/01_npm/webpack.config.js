9c28b0b8c1eb5906c48f2b0b364e1a7333552eef
Fares <fares.osman@outlook.com>
2020-05-05 23:58:04 -0400

    Finish exercise

---
+++ react_curriculum/01_npm/webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: "./entry.jsx",
+  output: {
+    filename: "./bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: ["@babel/env", "@babel/react"],
+          },
+        },
+      },
+    ],
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", "*"],
+  },
+};
