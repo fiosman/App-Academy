d2a58cbe767a8e83df5d4f9b6a9265a52a1ccefe
Fares <fares.osman@outlook.com>
2020-05-07 00:03:43 -0400

    Start Calculator component

---
+++ react_curriculum/02_react_calculator/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+import Calculator from './calculator.jsx'; 
+
+document.addEventListener("DOMContentLoaded", () => { 
+  const root = document.getElementById("root"); 
+  ReactDOM.render(<Calculator />, root); 
+})
---
+++ react_curriculum/02_react_calculator/calculator.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
