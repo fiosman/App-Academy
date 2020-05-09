bff0e1077fd845897616d6277641183dfa57ab12
Fares <fares.osman@outlook.com>
2020-05-08 22:54:30 -0400

    Finish Phase I

---
+++ react_curriculum/04_react_widgets/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./widgets.jsx",
+  output: {
+    filename: "./bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: ["@babel/env", "@babel/react"],
+          },
+        },
+      },
+    ],
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", "*"],
+  },
+};
