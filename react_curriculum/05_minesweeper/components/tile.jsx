2b69101435b01c03c05c16863ec66796f245c607
Fares <fares.osman@outlook.com>
2020-05-14 21:36:25 -0400

    Write render for tile component

--- react_curriculum/05_minesweeper/components/tile.jsx
+++ react_curriculum/05_minesweeper/components/tile.jsx
@@ -1,11 +1,31 @@
-import React from 'react'; 
+import React from "react";
 
-export default class Tile extends React.Component { 
-  constructor(props) { 
-    super(props); 
+export default class Tile extends React.Component {
+  constructor(props) {
+    super(props);
   }
 
-  render() { 
-    return "T"; 
+  render() {
+    let tileStatus, className;
+    const tile = this.props.tile;
+    const adjacentBombs = tile.adjacentBombCount();
+
+    if (tile.explored) {
+      if (tile.bombed) {
+        tileStatus = "\u2622";
+        className = "bombed";
+      } else {
+        className = "explored";
+        tileStatus = adjacentBombs > 0 ? adjacentBombs : "";
+      }
+    } else if (tile.flagged) {
+      tileStatus = "\u2691";
+      className = "flagged";
+    } else {
+      className = "unexplored";
+    }
+
+    className = `tile ${className}`;
+    return <div className={className}>{tileStatus}</div>;
   }
 }
