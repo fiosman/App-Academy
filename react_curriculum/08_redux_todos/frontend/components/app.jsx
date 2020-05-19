912ed43137550935388f04c559b43a5963ca281f
Fares <fares.osman@outlook.com>
2020-05-18 21:23:01 -0400

    Write container component

--- react_curriculum/08_redux_todos/frontend/components/app.jsx
+++ react_curriculum/08_redux_todos/frontend/components/app.jsx
@@ -1,8 +1,12 @@
 import React from 'react'; 
+import TodoListContainer from './todos/todo_list_container';  
 
 function App() { 
   return ( 
-    <h1>ToDoList App</h1>
+    <div>
+      <h1>ToDoList App</h1>
+      <TodoListContainer/>
+    </div>
   )
 }
 
---
+++ react_curriculum/08_redux_todos/frontend/components/todos/todo_list.jsx
@@ -0,0 +1,7 @@
+import React from 'react'; 
+
+const TodoList = () => {
+  return <h3>Todo List goes here!</h3>;
+};
+
+export default TodoList;
