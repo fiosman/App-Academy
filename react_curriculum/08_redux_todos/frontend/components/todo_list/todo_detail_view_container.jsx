a2fe3a2580bbaebee41298838a4af5e05f3e715c
Fares <fares.osman@outlook.com>
2020-05-23 01:38:10 -0400

    Write mapDispatchToProps for todo_detail_view

--- react_curriculum/08_redux_todos/frontend/components/todo_list/todo_detail_view_container.jsx
+++ react_curriculum/08_redux_todos/frontend/components/todo_list/todo_detail_view_container.jsx
@@ -1,12 +1,9 @@
-import { removeTodo } from '../../actions/todo_actions'; 
-import { connect } from 'react-redux'; 
-import TodoDetailView from './todo_detail_view';
+import { removeTodo } from "../../actions/todo_actions";
+import { connect } from "react-redux";
+import TodoDetailView from "./todo_detail_view";
 
-const mapDispatchToProps = (dispatch) => ({ 
-  removeTodo: (todo) => dispatch(removeTodo(todo))
-})
+const mapDispatchToProps = (dispatch) => ({
+  removeTodo: (todo) => dispatch(removeTodo(todo)),
+});
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(TodoDetailView); 
+export default connect(null, mapDispatchToProps)(TodoDetailView);
