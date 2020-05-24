0ab17da4a4061a33f23f2e5c24354f6c75c9574a
Fares <fares.osman@outlook.com>
2020-05-24 16:51:08 -0400

    Finish Middleware HW including bonus phase

--- react_curriculum/09_redux_middleware/frontend/store/store.js
+++ react_curriculum/09_redux_middleware/frontend/store/store.js
@@ -1,21 +1,27 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from '../reducers/root_reducer';
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "../reducers/root_reducer";
 
 const addLoggingToDispatch = (store) => (next) => (action) => {
   console.log(action);
   console.log(store.getState());
-  const res = next(action);
+  next(action); //hit loggingNext middleware
   console.log(store.getState());
-
-  return res;
 };
 
+const loggingNext = (store) => (next) => (action) => {
+  console.log("Action to dispatch from addLoggingToDispatch:", action);
+  return next(action); //reducer has been hit
+};
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(rootReducer, preloadedState, applyMiddleware(addLoggingToDispatch));
+  const store = createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(addLoggingToDispatch, loggingNext)
+  );
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
   return store;
-}
+};
 
 export default configureStore;
