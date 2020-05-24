cb51f5f11f61418d140a9892e750eebba358da30
Fares <fares.osman@outlook.com>
2020-05-24 12:49:48 -0400

    Finish Phase I logging

--- react_curriculum/09_redux_middleware/frontend/todo_redux.jsx
+++ react_curriculum/09_redux_middleware/frontend/todo_redux.jsx
@@ -4,11 +4,22 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
+const addLoggingToDispatch = (store) => { 
+  const storeDispatch = store.dispatch; 
+
+  return function(action) { 
+    console.log(store.getState()); 
+    console.log(action); 
+    storeDispatch(action); 
+    console.log(store.getState()); 
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
-
+  store.dispatch = addLoggingToDispatch(store)
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
