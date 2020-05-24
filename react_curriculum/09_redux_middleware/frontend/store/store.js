d379333e1259ff46d573c84d7133b5353d3bb4f2
Fares <fares.osman@outlook.com>
2020-05-24 15:06:32 -0400

    Finish Phase III

--- react_curriculum/09_redux_middleware/frontend/store/store.js
+++ react_curriculum/09_redux_middleware/frontend/store/store.js
@@ -1,8 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 
+const addLoggingToDispatch = (store) => (next) => (action) => {
+  console.log(action);
+  console.log(store.getState());
+  const res = next(action);
+  console.log(store.getState());
+
+  return res;
+};
+
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(rootReducer, preloadedState);
+  const store = createStore(rootReducer, preloadedState, applyMiddleware(addLoggingToDispatch));
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
--- react_curriculum/09_redux_middleware/frontend/todo_redux.jsx
+++ react_curriculum/09_redux_middleware/frontend/todo_redux.jsx
@@ -3,29 +3,11 @@ import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
 
-const addLoggingToDispatch = (store) => (next) => (action) => {
-  console.log(action);
-  console.log(store.getState());
-  const res = next(action);
-  console.log(store.getState());
-
-  return res;
-};
-
-const applyMiddlewares = (store, ...middlewares) => {
-  let dispatch = store.dispatch;
-  middlewares.forEach((middleware) => {
-    dispatch = middleware(store)(dispatch);
-  });
-  return Object.assign({}, store, { dispatch });
-};
-
 document.addEventListener("DOMContentLoaded", () => {
   const preloadedState = localStorage.state
     ? JSON.parse(localStorage.state)
     : {};
   let store = configureStore(preloadedState);
-  store = applyMiddlewares(store, addLoggingToDispatch);
   const root = document.getElementById("content");
   ReactDOM.render(<Root store={store} />, root);
 });
