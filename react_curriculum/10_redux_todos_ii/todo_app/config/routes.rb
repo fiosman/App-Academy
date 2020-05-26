c0c8a7d5b98fa0af1d38dc9b87c766f44f6a4891
Fares <fares.osman@outlook.com>
2020-05-26 00:02:19 -0400

    Configure routes

--- react_curriculum/10_redux_todos_ii/todo_app/config/routes.rb
+++ react_curriculum/10_redux_todos_ii/todo_app/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do 
+    resources :todos, only: [:show, :index, :create, :update, :destroy]
+  end
 end
