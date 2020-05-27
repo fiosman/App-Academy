c5d09565c846a90ae0a4e6ea1e289cfefcf282ad
Fares <fares.osman@outlook.com>
2020-05-26 22:59:08 -0400

    Write thunk middleware

--- react_curriculum/10_redux_todos_ii/todo_app/frontend/store/store.js
+++ react_curriculum/10_redux_todos_ii/todo_app/frontend/store/store.js
@@ -1,8 +1,10 @@
 import { createStore } from "redux";
 import rootReducer from "../reducers/root_reducer";
+import { applyMiddleware } from 'redux'; 
+import thunk from '../middleware/thunk'; 
 
 const configureStore = (preloadedState={}) => {
-    const store = createStore(rootReducer, preloadedState);
+    const store = createStore(rootReducer, preloadedState, applyMiddleware(thunk));
     return store;
   }
 
