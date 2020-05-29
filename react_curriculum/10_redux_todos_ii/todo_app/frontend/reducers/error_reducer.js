79d314b5ce16bf90f941f76b5d2f2541dd48787f
Fares <fares.osman@outlook.com>
2020-05-28 22:34:35 -0400

    Write error actions and reducer

---
+++ react_curriculum/10_redux_todos_ii/todo_app/frontend/reducers/error_reducer.js
@@ -0,0 +1,15 @@
+
+import {RECEIVE_ERRORS, CLEAR_ERRORS} from '../actions/error_actions'; 
+
+const errorsReducer = (state = [], action) => { 
+  switch(action.type) { 
+    case RECEIVE_ERRORS: 
+      return action.errors
+    case CLEAR_ERRORS: 
+      return [];
+    default: 
+      return state;
+  }
+}
+
+export default errorsReducer;
