d99331a824ba4e10ed37bb6acbceb0a5499d219f
Fares <fares.osman@outlook.com>
2020-05-31 17:44:47 -0400

    Configure routes for steps

--- react_curriculum/10_redux_todos_ii/todo_app/config/routes.rb
+++ react_curriculum/10_redux_todos_ii/todo_app/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json } do 
-    resources :todos, only: [:show, :index, :create, :update, :destroy]
+    resources :todos, except: [:edit, :new] do 
+      resources :steps, except: [:edit, :new] 
+    end
   end
 
   root to: 'static_pages#root'
