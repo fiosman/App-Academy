7bcc6deb36342756b5a3f7f49eeca551bb6ee591
Fares <fares.osman@outlook.com>
2020-06-01 22:52:47 -0400

    Write API utility methods for Steps

---
+++ react_curriculum/10_redux_todos_ii/todo_app/frontend/util/steps_api_util.js
@@ -0,0 +1,29 @@
+export const createStep = (step) => { 
+ return $.ajax({ 
+   method: "POST", 
+   url: `api/todos/${step.todo_id}/steps`, 
+   data: step,
+ }) 
+}
+
+export const deleteStep = (step) => { 
+  return $.ajax({ 
+    method: 'DELETE', 
+    url: `api/todos/${step.todo_id}/steps/${step.id}`, 
+  })
+}
+
+export const updateStep = (step) => { 
+  return $.ajax({ 
+    method: 'PUT', 
+    url: `api/todos/${step.todo_id}/steps/${step.id}`, 
+    data: { step } ,
+  })
+}
+
+export const fetchSteps = (todoId) => { 
+  return $.ajax({ 
+    method: 'GET', 
+    url: `api/todos/${todoId}/steps`,
+  })
+}
