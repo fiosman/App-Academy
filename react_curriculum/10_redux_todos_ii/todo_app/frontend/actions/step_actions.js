ce9e45b5afe6587ce48be47f198e6fe241abcce7
Fares <fares.osman@outlook.com>
2020-06-02 23:41:49 -0400

    Write fetchSteps

--- react_curriculum/10_redux_todos_ii/todo_app/frontend/actions/step_actions.js
+++ react_curriculum/10_redux_todos_ii/todo_app/frontend/actions/step_actions.js
@@ -1,6 +1,7 @@
 export const RECEIVE_STEPS = "RECEIVE_STEPS";
 export const RECEIVE_STEP = "RECEIVE_STEP";
 export const REMOVE_STEP = "REMOVE_STEP"; 
+import * as APIUtil from '../util/steps_api_util.js'; 
 
 export const receiveSteps = (steps) => ({ 
   type: RECEIVE_STEPS, 
@@ -16,3 +17,7 @@ export const removeStep = (step) => ({
   type: REMOVE_STEP, 
   step, 
 })
+
+export const fetchSteps = (todoId) => (dispatch) => { 
+  return APIUtil.fetchSteps(todoId).then((steps) => dispatch(receiveSteps(steps)));
+}
