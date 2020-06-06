f495445c649c0e9659735d88b4b9cecc0f6b5c1f
Fares <fares.osman@outlook.com>
2020-06-06 12:47:42 -0400

    Re-write thunk action creators

--- react_curriculum/10_redux_todos_ii/todo_app/frontend/actions/step_actions.js
+++ react_curriculum/10_redux_todos_ii/todo_app/frontend/actions/step_actions.js
@@ -1,32 +1,31 @@
 export const RECEIVE_STEPS = "RECEIVE_STEPS";
 export const RECEIVE_STEP = "RECEIVE_STEP";
-export const REMOVE_STEP = "REMOVE_STEP"; 
-import * as APIUtil from '../util/steps_api_util.js'; 
-import { receiveErrors, clearErrors } from "../actions/error_actions";
+export const REMOVE_STEP = "REMOVE_STEP";
+import * as APIUtil from "../util/steps_api_util.js";
 
-export const receiveSteps = (steps) => ({ 
-  type: RECEIVE_STEPS, 
-  steps, 
-})
+export const receiveSteps = (steps) => ({
+  type: RECEIVE_STEPS,
+  steps,
+});
 
-export const receiveStep = (step) => ({ 
-  type: RECEIVE_STEP, 
+export const receiveStep = (step) => ({
+  type: RECEIVE_STEP,
   step,
-})
+});
 
-export const removeStep = (step) => ({ 
-  type: REMOVE_STEP, 
-  step, 
-})
+export const removeStep = (step) => ({
+  type: REMOVE_STEP,
+  step,
+});
 
-export const fetchSteps = (todoId) => (dispatch) => { 
-  return APIUtil.fetchSteps(todoId).then((steps) => dispatch(receiveSteps(steps)));
-}
+export const fetchSteps = (todoId) => (dispatch) =>
+  APIUtil.fetchSteps(todoId).then((steps) => dispatch(receiveSteps(steps)));
 
-export const createStep = (step, todoId) => (dispatch) => 
-APIUtil.createStep(step, todoId).then((step) => { 
-    dispatch(receiveStep(step)); 
-    dispatch(clearErrors()); 
-  }, 
-  (err) => dispatch(receiveErrors(err.responseJSON))
-);
+export const createStep = (step, todoId) => (dispatch) =>
+  APIUtil.createStep(step, todoId).then((step) => dispatch(receiveStep(step)));
+
+export const updateStep = (step) => (dispatch) =>
+  APIUtil.updateStep(step).then((step) => dispatch(receiveStep(step)));
+
+export const destroyStep = (step) => (dispatch) =>
+  APIUtil.deleteStep(step).then((step) => dispatch(removeStep(step)));
--- react_curriculum/10_redux_todos_ii/todo_app/frontend/todo_redux.jsx
+++ react_curriculum/10_redux_todos_ii/todo_app/frontend/todo_redux.jsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
 // import {requestTodos} from './actions/todo_actions'; 
+import { fetchSteps } from './actions/step_actions'; 
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("content");
   let store = configureStore();
+  window.store = store; 
+  window.fetchSteps = fetchSteps;
+  
   ReactDOM.render(<Root store={store} />, root);
 });
