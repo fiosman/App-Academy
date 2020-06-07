e609a71f23fcdd282df5708fccf709fe28652421
Fares <fares.osman@outlook.com>
2020-06-07 14:48:44 -0400

    Setup store

--- react_curriculum/11_giphy_search/giphy_search.jsx
+++ react_curriculum/11_giphy_search/giphy_search.jsx
@@ -3,3 +3,7 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 import { fetchSearchGiphys } from './util/api_util'; 
+import { receiveSearchGiphys } from './actions/giphy_actions'
+
+
+
--- react_curriculum/11_giphy_search/store/store.js
+++ react_curriculum/11_giphy_search/store/store.js
@@ -1,3 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
+
+const configureStore = () => { 
+  return createStore(rootReducer)
+}
+
+export default configureStore;
