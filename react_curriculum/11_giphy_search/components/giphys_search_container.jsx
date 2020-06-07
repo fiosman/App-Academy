3ad6dc48d2d054121885be8b474bf8ceac397b44
Fares <fares.osman@outlook.com>
2020-06-07 15:43:00 -0400

    Write GiphysSearchContainer

--- react_curriculum/11_giphy_search/components/giphys_search_container.jsx
+++ react_curriculum/11_giphy_search/components/giphys_search_container.jsx
@@ -1,3 +1,13 @@
-import { connect } from 'react-redux';
-import GiphysSearch from './giphys_search';
-import { fetchSearchGiphys } from '../actions/giphy_actions';
+import { connect } from "react-redux";
+import GiphysSearch from "./giphys_search";
+import { fetchSearchGiphys } from "../actions/giphy_actions";
+
+const mapStateToProps = (state) => ({
+  giphys: state.giphys,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchSearchGiphys: (searchTerm) => dispatch(fetchSearchGiphys(searchTerm)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(GiphysSearch);
