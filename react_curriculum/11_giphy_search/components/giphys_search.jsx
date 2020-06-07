b35b25d05022dbbb97d4347ff0e0ce2749eada9e
Fares <fares.osman@outlook.com>
2020-06-07 16:56:15 -0400

    Render search form

--- react_curriculum/11_giphy_search/components/giphys_search.jsx
+++ react_curriculum/11_giphy_search/components/giphys_search.jsx
@@ -1,3 +1,21 @@
 import React from 'react';
 
 import GiphysIndex from './giphys_index';
+
+class GiphysSearch extends React.Component { 
+  constructor(props) { 
+    super(props);
+    this.state = { searchTerm: '' }; 
+  }
+
+  render() { 
+    return( 
+      <form>
+        <input type="text" placeholder="Search for Giphys here..."/>
+        <input type="submit" value="Submit"/>
+      </form>
+    )
+  }
+}
+
+export default GiphysSearch;
