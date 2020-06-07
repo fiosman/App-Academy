3a66bbefe21539cf3d85672f5b7b81ad33585aa1
Fares <fares.osman@outlook.com>
2020-06-07 17:21:52 -0400

    Finish GiphysSearch class component

--- react_curriculum/11_giphy_search/components/giphys_search.jsx
+++ react_curriculum/11_giphy_search/components/giphys_search.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import GiphysIndex from './giphys_index';
+import GiphysIndex from "./giphys_index";
 
-class GiphysSearch extends React.Component { 
-  constructor(props) { 
+class GiphysSearch extends React.Component {
+  constructor(props) {
     super(props);
-    this.state = { searchTerm: '' }; 
+    this.state = { searchTerm: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  render() { 
-    return( 
-      <form>
-        <input type="text" placeholder="Search for Giphys here..."/>
-        <input type="submit" value="Submit"/>
-      </form>
-    )
+  handleChange(e) {
+    this.setState({ searchTerm: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.fetchSearchGiphys(this.state.searchTerm);
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Search for Giphys here..."
+            value={this.state.searchTerm}
+            onChange={this.handleChange}
+          />
+          <button>Search Giphy!</button>
+        </form>
+        <GiphysIndex giphys={this.props.giphys} />
+      </div>
+    );
   }
 }
 
