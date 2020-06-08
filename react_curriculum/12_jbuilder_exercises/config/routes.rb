42721c5284200e76ccff446f81967f65ede9c257
Fares <fares.osman@outlook.com>
2020-06-07 21:02:06 -0400

    Setup routes

--- react_curriculum/12_jbuilder_exercises/config/routes.rb
+++ react_curriculum/12_jbuilder_exercises/config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   # Your routes here!
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do  
+    resources :guests, only: [:show, :index] do 
+      resources :gifts, only: [:index]
+    end
+    resources :invitations, only: [:show, :index] 
+    resources :parties, only: [:show, :index]
+    resources :gifts, only: [:show]
+  end
 end
