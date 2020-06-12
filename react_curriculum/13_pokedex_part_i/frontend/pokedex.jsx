63d2f1bbe3e70ed44d5f7abfcedb3ce828e236ed
Fares <fares.osman@outlook.com>
2020-06-11 21:18:01 -0400

    Finish Phase I frontend setup

---
+++ react_curriculum/13_pokedex_part_i/frontend/pokedex.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>Pokedex</h1>, root);
+});
