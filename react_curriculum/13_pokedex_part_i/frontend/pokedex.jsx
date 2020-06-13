552ebf1554067d6b20d1db16798f6386121a4907
Fares <fares.osman@outlook.com>
2020-06-13 00:15:53 -0400

    Clean up part 1; finish exercise

--- react_curriculum/13_pokedex_part_i/frontend/pokedex.jsx
+++ react_curriculum/13_pokedex_part_i/frontend/pokedex.jsx
@@ -1,21 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { configureStore } from "./store/store";
-import { fetchAllPokemon } from "./util/api_util";
-import { receiveAllPokemon } from "./actions/pokemon_actions";
-import { requestAllPokemon } from "./actions/pokemon_actions";
-import { selectAllPokemon } from "./reducers/selectors";
 import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   const store = configureStore();
-  window.store = store;
-  window.fetchAllPokemon = fetchAllPokemon;
-  window.receiveAllPokemon = receiveAllPokemon;
-  window.requestAllPokemon = requestAllPokemon;
-  window.selectAllPokemon = selectAllPokemon;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
   ReactDOM.render(<Root store={store} />, root);
 });
