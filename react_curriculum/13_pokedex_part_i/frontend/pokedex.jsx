3fe7c3cd4c902ab091849508aea5943e278e0966
Fares <fares.osman@outlook.com>
2020-06-12 22:55:27 -0400

    Write selectAllPokemon selector

--- react_curriculum/13_pokedex_part_i/frontend/pokedex.jsx
+++ react_curriculum/13_pokedex_part_i/frontend/pokedex.jsx
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom";
 import { configureStore }  from './store/store';
 import { fetchAllPokemon } from './util/api_util'; 
 import { receiveAllPokemon } from './actions/pokemon_actions';
+import { requestAllPokemon } from './actions/pokemon_actions';
+import { selectAllPokemon } from './reducers/selectors'; 
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   const store = configureStore(); 
+  window.store = store;
   window.fetchAllPokemon = fetchAllPokemon;
   window.receiveAllPokemon = receiveAllPokemon;
+  window.requestAllPokemon = requestAllPokemon; 
+  window.selectAllPokemon = selectAllPokemon;
   window.getState = store.getState;
   window.dispatch = store.dispatch; 
   ReactDOM.render(<h1>Pokedex</h1>, root);
