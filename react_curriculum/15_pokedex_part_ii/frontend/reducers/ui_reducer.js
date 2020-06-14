401db866ba486a40b2b28d324a1dfe42a23b0892
Fares <fares.osman@outlook.com>
2020-06-14 18:58:25 -0400

    reset files

---
+++ react_curriculum/15_pokedex_part_ii/frontend/reducers/ui_reducer.js
@@ -0,0 +1,8 @@
+// import { combineReducers } from 'redux'; 
+// import errorsReducer from './'
+// import loadingReducer from './'
+
+// const uiReducer = combineReducers({ 
+//   errors: errorsReducer, 
+//   loading: loadingReducer,
+// })
