50fe5528cb248e2e14ac1e9c6b7ecff95a499f94
Fares <fares.osman@outlook.com>
2020-06-14 19:15:26 -0400

    Write receiveSinglePokemon action creator

--- react_curriculum/15_pokedex_part_ii/frontend/actions/pokemon_actions.js
+++ react_curriculum/15_pokedex_part_ii/frontend/actions/pokemon_actions.js
@@ -1,10 +1,16 @@
 import * as APIUtil from '../util/api_util'; 
 
 export const RECEIVE_ALL_POKEMON = 'RECEIVE_ALL_POKEMON'; 
+export const RECEIVE_SINGLE_POKEMON = 'RECEIVE_SINGLE_POKEMON'; 
 
 //synchronous action creators
 export const receiveAllPokemon = (pokemon) => ({ 
   type: RECEIVE_ALL_POKEMON, 
+  pokemon
+})
+
+export const receiveSinglePokemon = (pokemon) => ({ 
+  type: RECEIVE_SINGLE_POKEMON, 
   pokemon
 })
 
