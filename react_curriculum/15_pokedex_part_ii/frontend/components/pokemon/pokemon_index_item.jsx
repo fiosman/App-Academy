29df1e9a4753a9905ba093271fb9f0ec06f87e7f
Fares <fares.osman@outlook.com>
2020-06-14 11:19:47 -0400

    Add react router Link components to pokemon index item

--- react_curriculum/15_pokedex_part_ii/frontend/components/pokemon/pokemon_index_item.jsx
+++ react_curriculum/15_pokedex_part_ii/frontend/components/pokemon/pokemon_index_item.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PokemonIndexItem = (props) => {
   return (
-    <li>
-      <span>{props.poke.name}</span>
-      <img src={props.poke.image_url} />
-    </li>
+    <Link to={`/pokemon/${props.poke.id}`}>
+      <li>
+        <span>{props.poke.name}</span>
+        <img src={props.poke.image_url} />
+      </li>
+    </Link>
   );
 };
 
