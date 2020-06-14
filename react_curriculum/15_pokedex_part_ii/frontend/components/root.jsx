fc3c9ea33c7b4a99b38ebaee53db8d7da71c95bc
Fares <fares.osman@outlook.com>
2020-06-14 00:41:54 -0400

    Refactor root component to include react router

--- react_curriculum/15_pokedex_part_ii/frontend/components/root.jsx
+++ react_curriculum/15_pokedex_part_ii/frontend/components/root.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Provider } from "react-redux";
-import PokemonIndexContainer from './pokemon/pokemon_index_container.jsx'
+import PokemonIndexContainer from './pokemon/pokemon_index_container.jsx';
+import { HashRouter, Route } from 'react-router-dom'; 
 
 const Root = (props) => {
   return (
     <Provider store={props.store}>
-      <PokemonIndexContainer/>
+      <HashRouter>
+        <Route path="/" component={PokemonIndexContainer} />
+      </HashRouter>
     </Provider>
   );
 };
