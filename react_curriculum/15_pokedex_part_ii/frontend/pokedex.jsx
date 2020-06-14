34b223a11614978c1cf261f99da839ff2c9d94d4
Fares <fares.osman@outlook.com>
2020-06-14 19:13:31 -0400

    Create API util that fetches single pokemon

--- react_curriculum/15_pokedex_part_ii/frontend/pokedex.jsx
+++ react_curriculum/15_pokedex_part_ii/frontend/pokedex.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { configureStore } from "./store/store";
 import Root from "./components/root";
+import { fetchPokemon } from './util/api_util'; 
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   const store = configureStore();
+  window.store = store; 
+  window.fetchPoke = fetchPokemon;
   ReactDOM.render(<Root store={store} />, root);
 });
