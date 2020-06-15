b7e9993ecbb8a08b056bc7ce3e259c60574ddbcb
Fares <fares.osman@outlook.com>
2020-06-15 13:34:33 -0400

    Write requestSinglePokemon thunk action creator

--- react_curriculum/15_pokedex_part_ii/frontend/actions/pokemon_actions.js
+++ react_curriculum/15_pokedex_part_ii/frontend/actions/pokemon_actions.js
@@ -1,20 +1,26 @@
-import * as APIUtil from '../util/api_util'; 
+import * as APIUtil from "../util/api_util";
 
-export const RECEIVE_ALL_POKEMON = 'RECEIVE_ALL_POKEMON'; 
-export const RECEIVE_SINGLE_POKEMON = 'RECEIVE_SINGLE_POKEMON'; 
+export const RECEIVE_ALL_POKEMON = "RECEIVE_ALL_POKEMON";
+export const RECEIVE_SINGLE_POKEMON = "RECEIVE_SINGLE_POKEMON";
 
 //synchronous action creators
-export const receiveAllPokemon = (pokemon) => ({ 
-  type: RECEIVE_ALL_POKEMON, 
-  pokemon
-})
+export const receiveAllPokemon = (pokemon) => ({
+  type: RECEIVE_ALL_POKEMON,
+  pokemon,
+});
 
-export const receiveSinglePokemon = (onePokemon) => ({ 
-  type: RECEIVE_SINGLE_POKEMON, 
-  onePokemon
-})
+export const receiveSinglePokemon = (onePokemon) => ({
+  type: RECEIVE_SINGLE_POKEMON,
+  onePokemon,
+});
 
 //asynchronous action creators
-export const requestAllPokemon = () => (dispatch) => ( 
-  APIUtil.fetchAllPokemon().then(pokemon => dispatch(receiveAllPokemon(pokemon)))
-)
+export const requestAllPokemon = () => (dispatch) =>
+  APIUtil.fetchAllPokemon().then((pokemon) =>
+    dispatch(receiveAllPokemon(pokemon))
+  );
+
+export const requestSinglePokemon = (pokemonId) => (dispatch) =>
+  APIUtil.fetchPokemon(pokemonId).then((pokemon) =>
+    dispatch(receiveSinglePokemon(pokemon))
+  );
--- react_curriculum/15_pokedex_part_ii/frontend/pokedex.jsx
+++ react_curriculum/15_pokedex_part_ii/frontend/pokedex.jsx
@@ -4,12 +4,14 @@ import { configureStore } from "./store/store";
 import Root from "./components/root";
 import { fetchPokemon } from './util/api_util'; 
 import { receiveSinglePokemon } from './actions/pokemon_actions'; 
+import { requestSinglePokemon } from './actions/pokemon_actions'; 
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   const store = configureStore();
   window.store = store; 
-  window.fetchPoke = fetchPokemon;
+  window.fetchPokemon = fetchPokemon;
   window.receiveSinglePokemon = receiveSinglePokemon;
+  window.requestSinglePokemon = requestSinglePokemon;
   ReactDOM.render(<Root store={store} />, root);
 });
