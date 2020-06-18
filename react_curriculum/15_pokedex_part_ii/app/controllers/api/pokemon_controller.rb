29daef28133f21261ff0c321aeece2425ca06782
Fares <fares.osman@outlook.com>
2020-06-17 20:29:21 -0400

    Write create controller action for pokemon

--- react_curriculum/15_pokedex_part_ii/app/controllers/api/pokemon_controller.rb
+++ react_curriculum/15_pokedex_part_ii/app/controllers/api/pokemon_controller.rb
@@ -8,4 +8,19 @@ class Api::PokemonController < ApplicationController
     @pokemon = Pokemon.find(params[:id])
     render :show
   end
+
+  def create 
+    @pokemon = Pokemon.new(pokemon_params)
+    if @pokemon.save 
+      render :show 
+    else 
+      render json: @pokemon.errors.full_messages, status: :unprocessable_entity
+    end
+  end 
+
+  private 
+
+  def pokemon_params
+   params.require(:pokemon).permit(:name, :attack, :defense, :poke_type, :image_url, moves: [])
+  end 
 end
--- react_curriculum/15_pokedex_part_ii/frontend/components/pokemon/pokemon_index_container.jsx
+++ react_curriculum/15_pokedex_part_ii/frontend/components/pokemon/pokemon_index_container.jsx
@@ -1,7 +1,7 @@
 import { connect } from "react-redux";
 import PokemonIndex from "./pokemon_index.jsx";
 import { selectAllPokemon } from "../../reducers/selectors";
-import { requestAllPokemon } from '../../actions/pokemon_actions';
+import { requestAllPokemon } from "../../actions/pokemon_actions";
 
 const mapStateToProps = (state) => ({
   pokemon: selectAllPokemon(state),
