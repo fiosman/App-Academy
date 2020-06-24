947b0de9894f78514d873ae41b14e812c19b918e
Fares <fares.osman@outlook.com>
2020-06-23 23:34:06 -0400

    Add errors slice of state to mapStateToProps in PokemonFormContainer

--- react_curriculum/15_pokedex_part_ii/frontend/components/pokemon/pokemon_form_container.jsx
+++ react_curriculum/15_pokedex_part_ii/frontend/components/pokemon/pokemon_form_container.jsx
@@ -2,8 +2,11 @@ import { connect } from "react-redux";
 import PokemonForm from './pokemon_form';
 import { createNewPokemon } from '../../actions/pokemon_actions';
 
+const mapStateToProps = (state) => ({ 
+  errors: state.ui.errors 
+})
 const mapDispatchToProps = (dispatch) => ({
   createNewPokemon: (pokemonData) => dispatch(createNewPokemon(pokemonData)),
 });
 
-export default connect(null, mapDispatchToProps)(PokemonForm);
+export default connect(mapStateToProps, mapDispatchToProps)(PokemonForm);
