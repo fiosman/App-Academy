09dd4b343d1c22143323da43d917dcd79d298f29
Fares <fares.osman@outlook.com>
2020-06-28 16:20:36 -0400

    Finish Part II

---
+++ react_curriculum/16_frontend_auth/frontend/actions/session.js
@@ -0,0 +1,27 @@
+import * as SessionUtil from "../utils/session";
+import * as SessionUtil from "../utils/session";
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+
+export const receiveCurrentUser = (user) => ({
+  type: RECEIVE_CURRENT_USER,
+  user,
+});
+
+export const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+});
+
+export const createNewUser = (formUser) => (dispatch) =>
+  SessionUtil.postUser(formUser).then((user) =>
+    dispatch(receiveCurrentUser(user))
+  );
+
+export const loginUser = (formUser) => (dispatch) =>
+  SessionUtil.postSession(formUser).then((user) =>
+    dispatch(receiveCurrentUser(user))
+  );
+
+export const logoutUser = () => (dispatch) =>
+  SessionUtil.deleteSession().then(() => dispatch(logoutCurrentUser()));
