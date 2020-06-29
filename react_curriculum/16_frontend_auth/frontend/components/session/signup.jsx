c2210468e02f5dc8d3c5fe733cbeeae327a42c6e
Fares <fares.osman@outlook.com>
2020-06-28 23:24:10 -0400

    Write handleInput and make Signup a controlled component

--- react_curriculum/16_frontend_auth/frontend/components/session/signup.jsx
+++ react_curriculum/16_frontend_auth/frontend/components/session/signup.jsx
@@ -3,7 +3,19 @@ import React from "react";
 class Signup extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+    };
   }
+
+  handleInput(type) {
+    return (e) => {
+      this.setState({ [type]: e.target.value });
+    };
+  }
+
   render() {
     return <div></div>;
   }
