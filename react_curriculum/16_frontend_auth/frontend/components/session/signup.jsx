15761981fb5e018de6f64f48aeee4d424a78ba32
Fares <fares.osman@outlook.com>
2020-06-29 21:05:22 -0400

    Finish creating sign up form controlled component

--- react_curriculum/16_frontend_auth/frontend/components/session/signup.jsx
+++ react_curriculum/16_frontend_auth/frontend/components/session/signup.jsx
@@ -8,6 +8,9 @@ class Signup extends React.Component {
       email: "",
       password: "",
     };
+
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInput(type) {
@@ -16,8 +19,46 @@ class Signup extends React.Component {
     };
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props
+      .createNewUser(this.state)
+      .then(() => this.props.history.push("/chirps"));
+  }
+
   render() {
-    return <div></div>;
+    return (
+      <div className="session-form">
+        <h2>Sign up!</h2>
+        <form>
+          <lablel>
+            Username:
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={this.handleInput("username")}
+            />
+          </lablel>
+          <lablel>
+            Email:
+            <input
+              type="text"
+              value={this.state.email}
+              onChange={this.handleInput("email")}
+            />
+          </lablel>
+          <lablel>
+            Password:
+            <input
+              type="password"
+              value={this.state.password}
+              onChange={this.handleInput("password")}
+            />
+          </lablel>
+          <button onClick={this.handleSubmit}>Sign Up</button>
+        </form>
+      </div>
+    );
   }
 }
 
