f7a86742548f739030343f590d84491e05fe7422
Fares <fares.osman@outlook.com>
2020-06-29 21:24:40 -0400

    Implement navbar log in/sign up

--- react_curriculum/16_frontend_auth/frontend/components/nav_bar/nav_bar.jsx
+++ react_curriculum/16_frontend_auth/frontend/components/nav_bar/nav_bar.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default ({ currentUser, logout }) => {
-  const display = (
+  const display = currentUser ? (
     <div>
-      <Link className="btn" to="/signup">Sign Up</Link>
-      <Link className="btn" to="/login">Log In</Link>
+      <p>Hello, {currentUser.username}</p>
+      <button onClick={logout}>Logout</button>
+    </div>
+  ) : (
+    <div>
+      <Link className="btn" to="/signup">
+        Sign Up
+      </Link>
+      <Link className="btn" to="/login">
+        Log In
+      </Link>
     </div>
   );
 
   return (
     <header className="nav-bar">
       <h1 className="logo">BLUEBIRD</h1>
-      <div>
-        {display}
-      </div>
+      <div>{display}</div>
     </header>
   );
 };
--- react_curriculum/16_frontend_auth/frontend/components/nav_bar/nav_bar_container.jsx
+++ react_curriculum/16_frontend_auth/frontend/components/nav_bar/nav_bar_container.jsx
@@ -4,19 +4,19 @@ import NavBar from './nav_bar';
 
 // Comment this back in after you have built the login functionality
 
-// import { logout } from '../../actions/session';
+import { logout } from '../../actions/session';
 
-// const mapStateToProps = state => ({
-//   currentUser: state.session.currentUser,
-// });
+const mapStateToProps = state => ({
+  currentUser: state.session.currentUser,
+});
 
-// const mapDispatchToProps = dispatch => ({
-//   logout: () => dispatch(logout()),
-// });
+const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch(logout()),
+});
 
 
 // Comment this out when you have built the login functionality
-const mapStateToProps = null;
-const mapDispatchToProps = null;
+// const mapStateToProps = null;
+// const mapDispatchToProps = null;
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
