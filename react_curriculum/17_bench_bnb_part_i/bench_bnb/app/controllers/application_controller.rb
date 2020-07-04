fc75238898305cce879cca671462a09edf604857
Fares <fares.osman@outlook.com>
2020-07-04 18:32:29 -0400

    update logout and login methods

--- react_curriculum/17_bench_bnb_part_i/bench_bnb/app/controllers/application_controller.rb
+++ react_curriculum/17_bench_bnb_part_i/bench_bnb/app/controllers/application_controller.rb
@@ -6,10 +6,12 @@ class ApplicationController < ActionController::Base
   def logout 
     current_user.reset_session_token!
     session[:session_token] = nil 
+    @current_user = nil
   end 
 
   def login(user) 
     session[:session_token] = user.session_token 
+    @current_user = user 
   end 
 
   def current_user 
