e681df98db585b7f3e756d72543f23e21f11fbc2
Fares <fares.osman@outlook.com>
2020-07-04 18:30:02 -0400

    Write ApplicationController methods

--- react_curriculum/17_bench_bnb_part_i/bench_bnb/app/controllers/application_controller.rb
+++ react_curriculum/17_bench_bnb_part_i/bench_bnb/app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :logged_in?, :current_user 
+
+  def logout 
+    current_user.reset_session_token!
+    session[:session_token] = nil 
+  end 
+
+  def login(user) 
+    session[:session_token] = user.session_token 
+  end 
+
+  def current_user 
+    return nil unless session[:session_token]
+
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end 
+
+  def logged_in? 
+    !!current_user
+  end 
+
 end
