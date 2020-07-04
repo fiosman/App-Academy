159671a7efa030c91227e48b58fcdcf429d420ea
Fares <fares.osman@outlook.com>
2020-07-04 16:36:29 -0400

    Configure backend routes

---
+++ react_curriculum/17_bench_bnb_part_i/bench_bnb/app/controllers/api/users_controller.rb
@@ -0,0 +1,3 @@
+class Api::UsersController < ApplicationController
+  
+end
--- react_curriculum/17_bench_bnb_part_i/bench_bnb/config/routes.rb
+++ react_curriculum/17_bench_bnb_part_i/bench_bnb/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do, defaults: { format: :json }
+    resources :users only: [:create, :new]
+    resource :session, only: [:create, :destroy]
+  end
   root to: 'static_pages#root'
 end
