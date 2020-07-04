50894aa3a1006ea549d0e82425871a2b3403cfb3
Fares <fares.osman@outlook.com>
2020-07-04 14:48:35 -0400

    Create user model

--- react_curriculum/17_bench_bnb_part_i/bench_bnb/app/models/user.rb
+++ react_curriculum/17_bench_bnb_part_i/bench_bnb/app/models/user.rb
@@ -38,7 +38,9 @@ class User < ApplicationRecord
   end
 
   def reset_session_token! 
-
+    self.session_token = User.generate_session_token
+    self.save! 
+    self.session_token
   end
 
   def is_password?(password)
