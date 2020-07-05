05cb6c3e8dfd757001c9a87eb9b20e63eb1eb233
Fares <fares.osman@outlook.com>
2020-07-05 18:56:55 -0400

    Write regular action and thunk action creators

---
+++ react_curriculum/17_bench_bnb_part_i/bench_bnb/frontend/actions/session_actions.js
@@ -0,0 +1,41 @@
+import { signup, login, logout } from "../util/session_api_util";
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+// regular action creators
+export const receiveCurrentUser = (currentUser) => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser,
+});
+
+export const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+});
+
+export const receiveErrors = (errors) => ({
+  type: RECEIVE_ERRORS,
+  errors,
+});
+
+// thunk action creators
+export const signup = (user) => (dispatch) => {
+  signup(user).then(
+    (user) => dispatch(receiveCurrentUser(user)),
+    (err) => dispatch(receiveErrors(err.responseJSON))
+  );
+};
+
+export const login = (user) => (dispatch) => {
+  login(user).then(
+    (user) => dispatch(receiveCurrentUser(user)),
+    (err) => dispatch(receiveErrors(err.responseJSON))
+  );
+};
+
+export const logout = () => (dispatch) => {
+  logout().then(
+    (res) => dispatch(logoutCurrentUser(res)),
+    (err) => dispatch(receiveErrors(err.responseJSON))
+  );
+};
