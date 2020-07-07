e072fd8d5550e143fe020d833bb32e355de30b54
Fares <fares.osman@outlook.com>
2020-07-07 00:08:25 -0400

    Update entry file to render root component

--- react_curriculum/17_bench_bnb_part_i/bench_bnb/frontend/bench_bnb.jsx
+++ react_curriculum/17_bench_bnb_part_i/bench_bnb/frontend/bench_bnb.jsx
@@ -1,19 +1,10 @@
-import React from 'react'; 
-import ReactDOM from 'react-dom'; 
-import configureStore from './store/store';
-import { 
-  signup,
-  login, 
-  logout
-} from './actions/session_actions';
+import React from "react";
+import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from './components/root';
 
-document.addEventListener('DOMContentLoaded', () => { 
-  const root = document.getElementById('root'); 
-  window.signup = signup; 
-  window.login = login; 
-  window.logout = logout;
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
   const store = configureStore();
-  window.getState = store.getState; 
-  window.dispatch = store.dispatch;
-  ReactDOM.render(<h1>Hello there</h1>, root);
-})
+  ReactDOM.render(<Root store={store} />, root);
+});
--- react_curriculum/17_bench_bnb_part_i/bench_bnb/frontend/components/root.jsx
+++ react_curriculum/17_bench_bnb_part_i/bench_bnb/frontend/components/root.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import App from "./app";
 
 const Root = (props) => (
