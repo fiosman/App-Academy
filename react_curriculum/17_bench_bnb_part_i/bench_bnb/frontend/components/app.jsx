0bb919250ba64b4cfe5794c2184888f54a414e48
Fares <fares.osman@outlook.com>
2020-07-06 23:55:45 -0400

    Write skeleton app and root components

---
+++ react_curriculum/17_bench_bnb_part_i/bench_bnb/frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+  <div>
+    <h1>Bench BnB</h1>
+  </div>
+);
+
+export default App;
---
+++ react_curriculum/17_bench_bnb_part_i/bench_bnb/frontend/components/root.jsx
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import { HashRouter } from 'react-router-dom';
+import App from './app';
+
+const Root = (props) => ( 
+  <Provider store={props.store}>
+    <HashRouter>
+      <App/>
+    </HashRouter>
+  </Provider>
+)
+
+export default Root;
