3bc8c5c136830b1d7750029250d37bff500ef6a2
Fares <fares.osman@outlook.com>
2020-07-09 23:36:57 -0400

    Add preloaded state to store

--- react_curriculum/17_bench_bnb_part_i/bench_bnb/frontend/bench_bnb.jsx
+++ react_curriculum/17_bench_bnb_part_i/bench_bnb/frontend/bench_bnb.jsx
@@ -6,8 +6,23 @@ import { login }  from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  const store = configureStore();
-  window.login = login;
-  window.dispatch = store.dispatch;
+  let store; 
+
+  if (window.currentUser) { 
+    const preloadedState = { 
+      entities: { 
+        users: { [window.currentUser.id]: window.currentUser }
+      }, 
+      session: { 
+        id: window.currentUser.id
+      }
+    }
+    store = configureStore(preloadedState);
+    delete window.currentUser; 
+  } else { 
+    store = configureStore();
+  }
+  // window.login = login;
+  // window.dispatch = store.dispatch;
   ReactDOM.render(<Root store={store} />, root);
 });
