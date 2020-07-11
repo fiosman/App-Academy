2a9cacd44b845ec5114fc27ae0d9567e88508224
Fares <fares.osman@outlook.com>
2020-07-10 23:58:30 -0400

    Write validations for bench model

---
+++ react_curriculum/18_bench_bnb_part_ii/bench_bnb/app/models/bench.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: benches
+#
+#  id          :bigint           not null, primary key
+#  description :string           not null
+#  lat         :float            not null
+#  lng         :float            not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Bench < ApplicationRecord 
+  validates :description, :lat, :lng, presence: true
+end
