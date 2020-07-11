9c12d58691ff8bf42c7bf12e6e9a54312db28d6b
Fares <fares.osman@outlook.com>
2020-07-11 00:35:58 -0400

    Write create and index controller actions for BenchesController

--- react_curriculum/18_bench_bnb_part_ii/bench_bnb/app/controllers/api/benches_controller.rb
+++ react_curriculum/18_bench_bnb_part_ii/bench_bnb/app/controllers/api/benches_controller.rb
@@ -1,8 +1,22 @@
-class BenchesController < ApplicationController 
+class Api::BenchesController < ApplicationController 
   def index
-    
+    @benches = Bench.all 
+    render "api/benches/index"
   end 
 
   def create 
+    @bench = Bench.new(bench_params) 
+
+    if @bench.save 
+      render "api/benches/show"
+    else 
+      render json: @bench.errors.full_messages, status: 422
+    end
   end 
+
+  private 
+
+  def bench_params 
+    params.require(:bench).permit(:description, :lng, :lat)
+  end
 end
