e3457724dcf17dd1035edb3ea7f2505b44043ae1
Fares <fares.osman@outlook.com>
2020-07-13 23:50:41 -0400

    Get started on Bench::in_bounds

--- react_curriculum/18_bench_bnb_part_ii/bench_bnb/app/models/bench.rb
+++ react_curriculum/18_bench_bnb_part_ii/bench_bnb/app/models/bench.rb
@@ -11,4 +11,16 @@
 #
 class Bench < ApplicationRecord 
   validates :description, :lat, :lng, presence: true
+
+  def self.in_bounds(bounds) 
+    #get the values lat and long coordinates from both hashes
+    fst_hsh_cds = bounds[bounds.keys.first].values
+    snd_hsh_cds = bounds[bounds.keys.last].values
+
+    #sort the lat aand long pairs
+    lat_pairs = fst_hsh_cds.first < snd_hsh_cds.first ? 
+      [fst_hsh_cds.first, snd_hsh_cds.first] : [fst_hsh_cds.first, snd_hsh_cds.first]
+    long_pairs = fst_hsh_cds.last < snd_hsh_cds.last ? 
+      [fst_hsh_cds.last, snd_hsh_cds.last] : [fst_hsh_cds.last, snd_hsh_cds.last]
+  end
 end
