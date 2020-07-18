f67f75d124b2b74885e39998af243cca8d04c1ed
Fares <fares.osman@outlook.com>
2020-07-18 16:29:44 -0400

    Create receiveBench regular action creator

--- react_curriculum/18_bench_bnb_part_ii/bench_bnb/frontend/actions/bench_actions.js
+++ react_curriculum/18_bench_bnb_part_ii/bench_bnb/frontend/actions/bench_actions.js
@@ -1,11 +1,16 @@
 import { fetchBenches } from "../util/bench_api_util";
 export const RECEIVE_BENCHES = 'RECEIVE_BENCHES'; 
+export const RECEIVE_BENCH = 'RECEIVE_BENCH';
 //regular action creators
 export const receiveBenches = (benches) => ({
   type: RECEIVE_BENCHES,
   benches,
 });
 
+export const receiveBench = (bench) => ({ 
+  type: RECEIVE_BENCH, 
+  bench,
+})
 //async action creators
 
 export const getBenches = (filters) => (dispatch) =>
