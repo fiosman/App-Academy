a1571c5d0d0fd0d6d2c95623a87c46cfe79840a8
Fares <fares.osman@outlook.com>
2020-07-18 16:28:30 -0400

    Write createBench util function

--- react_curriculum/18_bench_bnb_part_ii/bench_bnb/frontend/util/bench_api_util.js
+++ react_curriculum/18_bench_bnb_part_ii/bench_bnb/frontend/util/bench_api_util.js
@@ -1,9 +1,17 @@
-import { $CombinedState } from "redux"
+import { $CombinedState } from "redux";
 
-export const fetchBenches = (filters) => { 
-  return $.ajax({ 
-    method: 'GET', 
-    url: '/api/benches', 
-    data: filters
-  })
-}
+export const fetchBenches = (filters) => {
+  return $.ajax({
+    method: "GET",
+    url: "/api/benches",
+    data: filters,
+  });
+};
+
+export const createBench = (bench) => {
+  return $.ajax({
+    method: "POST",
+    url: "/api/benches",
+    data: { bench },
+  });
+};
