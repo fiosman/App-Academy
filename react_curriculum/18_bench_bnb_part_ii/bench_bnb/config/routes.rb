5f119ea67ec999aba4fbd879975ab68b5d796cd0
Fares <fares.osman@outlook.com>
2020-07-21 22:13:36 -0400

    Configure reviews routes

--- react_curriculum/18_bench_bnb_part_ii/bench_bnb/config/routes.rb
+++ react_curriculum/18_bench_bnb_part_ii/bench_bnb/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json  } do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy]
-    resources :benches, only: [:create, :index, :show]
+    resources :benches, only: [:create, :index, :show] do
+      resources :reviews, only: [:create]
+    end
   end
   root to: 'static_pages#root'
 end
