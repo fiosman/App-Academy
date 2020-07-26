1a1fd2f5428cd9c00e3f6d91ed788a598bfd7359
Fares <fares.osman@outlook.com>
2020-07-25 22:53:12 -0400

    Add reviews to bench

--- react_curriculum/18_bench_bnb_part_ii/bench_bnb/frontend/components/bench/review_index.jsx
+++ react_curriculum/18_bench_bnb_part_ii/bench_bnb/frontend/components/bench/review_index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 const ReviewIndex = (props) => {
-  const reviewIds= Object.keys(props.reviews);
+  const reviewIds = Object.keys(props.reviews);
   if (props.reviews) {
-    return reviewIds.map((reviewId => {
+    return reviewIds.map((reviewId) => {
       return (
         <ul key={reviewId}>
           <li>{props.reviews[reviewId].rating}</li>
           <li>{props.reviews[reviewId].body}</li>
         </ul>
       );
-    }));
+    });
   } else {
     return <h2>No reviews</h2>;
   }
