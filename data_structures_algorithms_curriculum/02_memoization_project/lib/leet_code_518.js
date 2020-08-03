4ebe92ca6bada7a9dab0fef316c6535d31e0f622
Fares <fares.osman@outlook.com>
2020-08-03 18:29:13 -0400

    Finish coinChange #2

--- data_structures_algorithms_curriculum/02_memoization_project/lib/leet_code_518.js
+++ data_structures_algorithms_curriculum/02_memoization_project/lib/leet_code_518.js
@@ -1,3 +1,37 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+//You are given coins of different denominations and a total amount of money.
+//Write a function to compute the number of combinations that make up that amount.
+//You may assume that you have infinite number of each kind of coin.
+
+//Examples:
+//Input: amount = 5, coins = [1, 2, 5]
+//Output: 4
+
+//Input: amount = 3, coins = [2];
+//Output: 0
+
+//Input: (amount = 10), (coins = [10]);
+//Output: 1
+
+function change(amount, coins, memo = {}) {
+  let key = amount + "-" + coins;
+  if (key in memo) return memo[key];
+
+  if (amount === 0) return 1;
+
+  let total = 0;
+
+  for (let i = coins.length - 1; i >= 0; i--) {
+    let currentCoin = coins[i];
+    if (currentCoin > amount) {
+      continue;
+    } else {
+      total += change(amount - currentCoin, coins.slice(i, coins.length), memo);
+    }
+  }
+  memo[key] = total;
+  return memo[key];
+}
+
