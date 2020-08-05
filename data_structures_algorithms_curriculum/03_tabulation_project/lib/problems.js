642d2792d950427d71f5805f8054dc2acabf7a0f
Fares <fares.osman@outlook.com>
2020-08-05 00:17:51 -0400

    Finish dp tabulation maxNonAdjacentSum

--- data_structures_algorithms_curriculum/03_tabulation_project/lib/problems.js
+++ data_structures_algorithms_curriculum/03_tabulation_project/lib/problems.js
@@ -1,12 +1,12 @@
 // Write a function, stepper(nums), that takes in an array of non negative numbers.
 // Each element of the array represents the maximum number of steps you can take from that position in the array.
-// The function should return a boolean indicating if it is possible to travel from the 
+// The function should return a boolean indicating if it is possible to travel from the
 // first position of the array to the last position.
 //
 // For Example:
 //
 // Given [3, 1, 0, 5, 10]
-//      - We begin at first position, 3. 
+//      - We begin at first position, 3.
 //      - Since the element is 3 we can take up to 3 steps from this position.
 //      - This means we can step to the 1, 0, or 5
 //      - Say we step to 1
@@ -20,9 +20,7 @@
 // stepper([3, 1, 0, 5, 10]);           // => true, because we can step through elements 3 -> 5 -> 10
 // stepper([3, 4, 1, 0, 10]);           // => true, because we can step through elements 3 -> 4 -> 10
 // stepper([2, 3, 1, 1, 0, 4, 7, 8])    // => false, there is no way to step to the end
-function stepper(nums) {
-}
-
+function stepper(nums) {}
 
 // Write a function, maxNonAdjacentSum(nums), that takes in an array of nonnegative numbers.
 // The function should return the maximum sum of elements in the array we can get if we cannot take
@@ -33,23 +31,28 @@ function stepper(nums) {
 // Examples:
 //
 // maxNonAdjacentSum([2, 7, 9, 3, 4])   // => 15, because 2 + 9 + 4
-// maxNonAdjacentSum([4,2,1,6])         // => 10, because 4 + 6 
+// maxNonAdjacentSum([4,2,1,6])         // => 10, because 4 + 6
+
+//dp tabulation method 
 function maxNonAdjacentSum(nums) {
-    let dp = new Array(nums.length); 
+  let dp = new Array(nums.length);
 
-    dp[0] = nums[0]; 
-    dp[1] = Math.max(dp[0], nums[1]);
+  if (nums.length === 0) return 0;
 
-    for (let i = 2; i < dp.length; i++) { 
-        dp[i] = nums[i] + dp[i - 2]
-    }
+  //these are our base cases; can't take the maxNonAdjacentSum of only 1 or 2 elements (can't add two elements together; illegal so take max). 
+  dp[0] = nums[0];
+  dp[1] = Math.max(dp[0], nums[1]);
 
-    return dp[nums.length - 1];
+  //my code may not be very intuitive here due to lack of variables
+  //however; basically we will be always checking if the current number in the given array is greater than the maxNonAdjacentSum we have so far
+  //We perform this operation legally; we do not compare to dp[i-1] as that would violate our non-adjacent number rule. We compare to dp [i-2]
+  //because that is the max non adjacent sum that is not adjacent to the current number we are iterating over. 
+  for (let i = 2; i < dp.length; i++) {
+    dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+  }
+
+  return dp[nums.length - 1];
 }
-
-console.log(maxNonAdjacentSum([5,5,10,100,10,5]))
-console.log(maxNonAdjacentSum([1,2,3]))
-console.log(maxNonAdjacentSum([1,20,3]))
 
 // Write a function, minChange(coins, amount), that accepts an array of coin values
 // and a target amount as arguments. The method should the minimum number of coins needed
@@ -63,13 +66,10 @@ console.log(maxNonAdjacentSum([1,20,3]))
 // minChange([1, 4, 5], 8))         // => 2, because 4 + 4 = 8
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
-function minChange(coins, amount) {
-
-}
-
+function minChange(coins, amount) {}
 
 module.exports = {
-    stepper,
-    maxNonAdjacentSum,
-    minChange
+  stepper,
+  maxNonAdjacentSum,
+  minChange,
 };
