377b186f0c7d262ec311e7f54b564606c5056b66
Fares <fares.osman@outlook.com>
2020-08-07 02:05:46 -0400

    Finish minChange tabulation

--- data_structures_algorithms_curriculum/03_tabulation_project/lib/problems.js
+++ data_structures_algorithms_curriculum/03_tabulation_project/lib/problems.js
@@ -94,11 +94,23 @@ function maxNonAdjacentSum(nums) {
 //
 // Examples:
 //
-// minChange([1, 2, 5], 11)         // => 3, because 5 + 5 + 1 = 11
-// minChange([1, 4, 5], 8))         // => 2, because 4 + 4 = 8
-// minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
-// minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
-function minChange(coins, amount) {}
+// console.log(minChange([1, 2, 5], 11))         // => 3, because 5 + 5 + 1 = 11
+// console.log(minChange([1, 4, 5], 8))         // => 2, because 4 + 4 = 8
+// console.log(minChange([1, 5, 10, 25], 15))    // => 2, because 10 + 5 = 15
+// console.log(minChange([1, 5, 10, 25], 100))   // => 4, because 25 + 25 + 25 + 25 = 100
+function minChange(coins, amount) {
+  let dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+
+  for (let i = 0; i <= amount; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      if (coins[j] <= i) {
+        dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+      }
+    }
+  }
+  return dp[amount] > amount ? -1 : dp[amount];
+}
 
 module.exports = {
   stepper,
