d997cc39a98abd79e57e0474e2016919b289a5b3
Fares <fares.osman@outlook.com>
2020-08-09 23:34:24 -0400

    First attempt at minPathSum

--- data_structures_algorithms_curriculum/03_tabulation_project/lib/leet_code_64.js
+++ data_structures_algorithms_curriculum/03_tabulation_project/lib/leet_code_64.js
@@ -2,5 +2,30 @@
 // Feel free to use this file for scratch work.
 
 function minPathSum(grid) {
-  
+  let m = grid.length;
+  let n = grid[0].length;
+  let dp = new Array(m).fill().map(() => new Array(n));
+  dp[0][0] = grid[0][0];
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (i === 0) {
+        dp[i][j] += dp[i][j - 1];
+      } else if (j === 0) {
+        dp[i][j] += dp[i - 1][j];
+      } else {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[j][i - 1]);
+      }
+    }
+  }
+
+  return dp[m - 1][n - 1];
 }
+
+console.log(
+  minPathSum([
+    [1, 3, 1],
+    [1, 5, 1],
+    [4, 2, 1],
+  ])
+);
