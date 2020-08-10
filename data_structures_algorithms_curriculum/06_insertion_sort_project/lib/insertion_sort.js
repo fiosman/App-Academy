e73f0a2ae1d246d2e047193632bb7a05cd511d4d
Fares <fares.osman@outlook.com>
2020-08-10 14:44:52 -0400

    finish insertionSort

--- data_structures_algorithms_curriculum/06_insertion_sort_project/lib/insertion_sort.js
+++ data_structures_algorithms_curriculum/06_insertion_sort_project/lib/insertion_sort.js
@@ -1,7 +1,18 @@
 function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentEle = arr[i];
+    let j = i - 1;
 
+    while (j >= 0 && arr[j] > currentEle) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = currentEle;
+  }
+
+  return arr;
 }
 
 module.exports = {
-    insertionSort
+  insertionSort,
 };
