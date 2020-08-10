5230965e31999c4ccb749c1e36d82fcea6374a0b
Fares <fares.osman@outlook.com>
2020-08-10 11:57:29 -0400

    Finish exercise

--- data_structures_algorithms_curriculum/04_bubble_sort_project/lib/bubble_sort.js
+++ data_structures_algorithms_curriculum/04_bubble_sort_project/lib/bubble_sort.js
@@ -1,13 +1,28 @@
 function swap(array, idx1, idx2) {
+  let temp = array[idx1];
+  array[idx1] = array[idx2];
+  array[idx2] = temp;
 
+  return array;
 }
 
 function bubbleSort(array) {
+  let swapped = true;
 
+  while (swapped) {
+    swapped = false;
+
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        swap(array, i, i + 1);
+        swapped = true;
+      }
+    }
+  }
+  return array;
 }
 
-
 module.exports = {
-    bubbleSort,
-    swap
+  bubbleSort,
+  swap,
 };
