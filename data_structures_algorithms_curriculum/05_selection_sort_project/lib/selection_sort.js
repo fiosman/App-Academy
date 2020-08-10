9bef78bf0a150b74e9ba0ecf25d8ab9f258b2c63
Fares <fares.osman@outlook.com>
2020-08-10 12:25:55 -0400

    Finish selectionSort

--- data_structures_algorithms_curriculum/05_selection_sort_project/lib/selection_sort.js
+++ data_structures_algorithms_curriculum/05_selection_sort_project/lib/selection_sort.js
@@ -1,27 +1,27 @@
 function swap(arr, index1, index2) {
-    let temp = arr[index1]; 
-    arr[index1] = arr[index2]; 
-    arr[index2] = temp;
+  let temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
 
-    return arr;
+  return arr;
 }
 
 function selectionSort(arr) {
-    for (let i = 0; i < arr.length; i++) { 
-        let minIndex = i;
-        for (let j = i + 1; j < arr.length; j++) { 
-            if (arr[i] > arr[j]) { 
-                minIndex = j;
-            }
-        }
-
-        swap(arr, i, minIndex);
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        minIndex = j;
+      }
     }
 
-    return arr;
+    swap(arr, i, minIndex);
+  }
+
+  return arr;
 }
 
 module.exports = {
-    selectionSort,
-    swap
+  selectionSort,
+  swap,
 };
