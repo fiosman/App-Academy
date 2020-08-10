93209fcf9b8fa155ccc44c388e9d86b77179d029
Fares <fares.osman@outlook.com>
2020-08-10 12:27:05 -0400

    Fix typo

--- data_structures_algorithms_curriculum/05_selection_sort_project/lib/selection_sort.js
+++ data_structures_algorithms_curriculum/05_selection_sort_project/lib/selection_sort.js
@@ -10,7 +10,7 @@ function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     let minIndex = i;
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] > arr[j]) {
+      if (arr[minIndex] > arr[j]) {
         minIndex = j;
       }
     }
