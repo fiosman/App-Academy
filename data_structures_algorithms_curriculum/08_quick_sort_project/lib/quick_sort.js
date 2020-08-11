80a693e08d9cfe71c1af012b8e294d200e084207
Fares <fares.osman@outlook.com>
2020-08-11 10:24:39 -0400

    Finish quickSort

--- data_structures_algorithms_curriculum/08_quick_sort_project/lib/quick_sort.js
+++ data_structures_algorithms_curriculum/08_quick_sort_project/lib/quick_sort.js
@@ -1,5 +1,14 @@
 function quickSort(array) {
+    if (array.length <= 1) return array; 
 
+    let pivot = array.shift(); 
+    let lessThanPivot = array.filter((ele => ele < pivot)); 
+    let greaterThanPivot = array.filter((ele => ele >= pivot)); 
+
+    let sortedLess = quickSort(lessThanPivot); 
+    let sortedGreater = quickSort(greaterThanPivot); 
+
+    return [...sortedLess, pivot, ...sortedGreater];
 }
 
 
