7a63bb4375cb272ca0eeccf70de826a8b1093f4c
Fares <fares.osman@outlook.com>
2020-08-11 09:50:06 -0400

    Finish merge sort

--- data_structures_algorithms_curriculum/07_merge_sort_project/lib/merge_sort.js
+++ data_structures_algorithms_curriculum/07_merge_sort_project/lib/merge_sort.js
@@ -1,9 +1,31 @@
 function merge(array1, array2) {
+    let merged = []; 
 
+    while (array1.length || array2.length) { 
+        let ele1 = array1.length ? array1[0] : Infinity; 
+        let ele2 = array2.length ? array2[0] : Infinity;
+
+        if (ele1 < ele2) { 
+            merged.push(array1.shift());
+        } else { 
+            merged.push(array2.shift());
+        }
+    }
+
+    return merged;
 }
 
 function mergeSort(array) {
+    if (array.length <= 1) return array; 
+    let midIdx = Math.floor(array.length / 2); 
 
+    let leftHalf = array.slice(0, midIdx); 
+    let rightHalf = array.slice(midIdx);
+
+    let sortedLeft = mergeSort(leftHalf); 
+    let sortedRight = mergeSort(rightHalf);
+
+    return merge(sortedLeft, sortedRight);
 }
 
 module.exports = {
