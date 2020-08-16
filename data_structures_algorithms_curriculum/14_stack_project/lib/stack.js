4a5ac55b87f6cf4648f88aa8aad78003f593402c
Fares <fares.osman@outlook.com>
2020-08-16 15:07:12 -0400

    Finish stack implementation

--- data_structures_algorithms_curriculum/14_stack_project/lib/stack.js
+++ data_structures_algorithms_curriculum/14_stack_project/lib/stack.js
@@ -12,7 +12,7 @@
 // Constraints:
 // ------------
 //
-// Make sure the time and space complexity of each is equivalent to those 
+// Make sure the time and space complexity of each is equivalent to those
 // in the table provided in the Time and Space Complexity Analysis section
 // of your Stack reading!
 //
@@ -21,26 +21,47 @@
 // -----------
 
 class Node {
-	constructor(val) { 
-		this.next = null; 
-		this.val = val;
-	}
+  constructor(val) {
+    this.next = null;
+    this.value = val;
+  }
 }
 
 class Stack {
-	constructor() { 
-		this.top = null; 
-		this.bottom = null; 
-		this.length = 0;
-	}
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
 
-	pop() {}
+  pop() {
+    if (this.length === 0) return null;
+    if (this.top === this.bottom) this.bottom = null;
+    const nodeRemoved = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return nodeRemoved.value;
+  }
 
-	push() {}
+  push(val) {
+    const node = new Node(val);
+    if (this.length === 0) {
+      this.top = node;
+      this.bottom = node;
+      this.length++;
+    } else {
+      const currentTop = this.top;
+      this.top = node;
+      this.top.next = currentTop;
+      this.length++;
+    }
 
-	size() { 
-		return this.length;
-	}
+    return this.length;
+  }
+
+  size() {
+    return this.length;
+  }
 }
 
 exports.Node = Node;
