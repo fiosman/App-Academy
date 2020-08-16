84190d646ca0230f7dad0d16394deaf90cbe8fbf
Fares <fares.osman@outlook.com>
2020-08-16 15:44:19 -0400

    Finish implementing queue

--- data_structures_algorithms_curriculum/15_queue_project/lib/queue.js
+++ data_structures_algorithms_curriculum/15_queue_project/lib/queue.js
@@ -12,7 +12,7 @@
 // Constraints:
 // ------------
 //
-// Make sure the time and space complexity of each is equivalent to those 
+// Make sure the time and space complexity of each is equivalent to those
 // in the table provided in the Time and Space Complexity Analysis section
 // of your Stack reading!
 //
@@ -21,27 +21,56 @@
 // -----------
 
 class Node {
-	constructor(val) { 
-		this.next = null; 
-		this.value = val;
-	}
+  constructor(val) {
+    this.next = null;
+    this.value = val;
+  }
 }
 
 class Queue {
-	constructor() { 
-		this.front = null; 
-		this.back = null; 
-		this.length = 0;
-	}
+  constructor() {
+    this.front = null;
+    this.back = null;
+    this.length = 0;
+  }
 
-	enqueue() {}
+  enqueue(val) {
+    const node = new Node(val);
+    if (this.length === 0) {
+      this.front = node;
+      this.back = node;
+      this.length++;
+    } else {
+			this.back.next = node;
+			this.back = node;
+      this.length++;
+    }
 
-	dequeue() {}
-	
-	size() { 
-		return this.length;
-	}
+    return this.length;
+  }
 
+  dequeue() {
+    if (this.length === 0) {
+      return null;
+    }
+
+    const removedFront = this.front;
+
+    if (removedFront === this.back) {
+      this.back = null;
+      this.front = null;
+      this.length--;
+      return removedFront.value;
+    } else {
+      this.front = this.front.next;
+      this.length--;
+      return removedFront.value;
+    }
+  }
+
+  size() {
+    return this.length;
+  }
 }
 
 exports.Node = Node;
