0d9ceeb8443b070b53a566b2d0c05af745f66e35
Fares <fares.osman@outlook.com>
2020-08-17 01:07:35 -0400

    Finish iterateAcrossLinkedListBackwards

--- data_structures_algorithms_curriculum/16_stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
+++ data_structures_algorithms_curriculum/16_stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
@@ -6,11 +6,11 @@
 // Prompt:
 // -------
 //
-// Iterate over a Singly Linked List of primitives backwards. When finished, 
-// return a string representing the original linked list's values backwards 
+// Iterate over a Singly Linked List of primitives backwards. When finished,
+// return a string representing the original linked list's values backwards
 // in the following format:
 //
-//                             'A -> B -> C -> D' 
+//                             'A -> B -> C -> D'
 //
 // ------------
 // Constraints:
@@ -27,8 +27,76 @@
 // -----------
 
 function iterateAcrossLinkedListBackwards(linkedList) {
-    // TODO: Implement the iterateAcrossLinkedListBackwards function here
+  // TODO: Implement the iterateAcrossLinkedListBackwards function here
+  let stack = new Stack();
+  let currentNode = linkedList.head;
+  let finalStr = "";
 
+  while (currentNode) {
+    stack.push(currentNode.value);
+    currentNode = currentNode.next;
+  }
+
+  while (stack.size() > 0) {
+    if (stack.size() > 1) {
+      finalStr += `${stack.pop()} -> `;
+    } else {
+      finalStr += `${stack.pop()}`;
+    }
+  }
+
+  return finalStr;
+}
+
+class Node {
+  constructor(val) {
+    this.next = null;
+    this.value = val;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const node = new Node(val);
+    if (!this.top) {
+      this.top = node;
+      this.length++;
+    } else {
+      const currentTop = this.top;
+      this.top = node;
+      this.top.next = currentTop;
+      this.length++;
+    }
+
+    return this.size();
+  }
+
+  pop() {
+    if (!this.top) return null;
+
+    const removedEle = this.top;
+
+    if (this.size() === 1) {
+      this.top = null;
+      this.length--;
+    } else {
+      this.top = this.top.next;
+      this.length--;
+    }
+
+    return removedEle.value;
+  }
+
+  size() {
+    return this.length;
+  }
 }
 
 exports.iterateAcrossLinkedListBackwards = iterateAcrossLinkedListBackwards;
+exports.Node = Node;
+exports.Stack = Stack;
