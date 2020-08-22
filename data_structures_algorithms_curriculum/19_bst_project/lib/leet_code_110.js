8acf90e0ffc600b3e8c7cc665687eaa9f0d6c2ab
Fares <fares.osman@outlook.com>
2020-08-22 01:56:51 -0400

    Finish Leetcode 110

--- data_structures_algorithms_curriculum/19_bst_project/lib/leet_code_110.js
+++ data_structures_algorithms_curriculum/19_bst_project/lib/leet_code_110.js
@@ -1,7 +1,20 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/balanced-binary-tree/
 
+function getHeight(root) {
+  if (!root) return -1;
+
+  return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+}
 
 function isBalanced(root) {
+  if (!root) return true;
 
+  let leftHeight = getHeight(root.left);
+  let rightHeight = getHeight(root.right);
+  return (
+    Math.abs(leftHeight - rightHeight) <= 1 &&
+    isBalanced(root.left) &&
+    isBalanced(root.right)
+  );
 }
