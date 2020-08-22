08598c91bb70b67d2598bb30d1db47bbae4e37a0
Fares <fares.osman@outlook.com>
2020-08-22 02:40:59 -0400

    Finish Leetcode 108

--- data_structures_algorithms_curriculum/19_bst_project/lib/leet_code_108.js
+++ data_structures_algorithms_curriculum/19_bst_project/lib/leet_code_108.js
@@ -1,6 +1,15 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
+const { TreeNode } = require("./bst");
 
 function sortedArrayToBST(nums) {
+  if (!nums.length) return null;
+
+  let midIdx = Math.floor(nums.length / 2);
+  let root = new TreeNode(nums[midIdx]);
+  root.left = sortedArrayToBST(nums.slice(0, midIdx));
+  root.right = sortedArrayToBST(nums.slice(midIdx + 1));
+
+  return root;
 }
