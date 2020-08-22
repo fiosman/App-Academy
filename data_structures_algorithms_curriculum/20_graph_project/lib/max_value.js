b1e853123c2d1aa2e68fd50747867914ec13c5f8
Fares <fares.osman@outlook.com>
2020-08-22 19:20:06 -0400

    Finish maxValue

--- data_structures_algorithms_curriculum/20_graph_project/lib/max_value.js
+++ data_structures_algorithms_curriculum/20_graph_project/lib/max_value.js
@@ -1,7 +1,25 @@
-function maxValue(node, visited=new Set()) {
+function maxValue(node, visited = new Set()) {
+  let queue = [node];
+  let currMax = node.val;
 
+  while (queue.length) {
+    let potentialNode = queue.shift();
+
+    if (visited.has(potentialNode)) continue;
+    visited.add(potentialNode);
+
+    if (potentialNode.val > currMax) {
+      currMax = potentialNode.val;
+    }
+
+    if (potentialNode.neighbors.length > 0) {
+      queue.push(...potentialNode.neighbors);
+    }
+  }
+
+  return currMax;
 }
 
 module.exports = {
-    maxValue
+  maxValue,
 };
--- data_structures_algorithms_curriculum/20_graph_project/test/test.js
+++ data_structures_algorithms_curriculum/20_graph_project/test/test.js
@@ -8,7 +8,7 @@ const { maxValue } = require('../lib/max_value');
 const { numRegions } = require('../lib/num_regions');
 
 describe('GraphNode Algorithms', () => {
-    describe.only('breadthFirstSearch(startingNode, targetVal)', () => {
+    describe('breadthFirstSearch(startingNode, targetVal)', () => {
         // it should accept a starting GraphNode and a targetVal as args
 
         context('when the targetVal is not contained in the full graph', () => {
@@ -68,7 +68,7 @@ describe('GraphNode Algorithms', () => {
         });
     });
 
-    describe('maxValue(startingNode)', () => {
+    describe.only('maxValue(startingNode)', () => {
         // it should accept a starting GraphNode as an arg
 
         it('should return the largest value within the full graph', () => {
