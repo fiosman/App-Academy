d20ded5d3c4ee4a3c50686a3044366f2a21214cd
Fares <fares.osman@outlook.com>
2020-08-23 23:00:18 -0400

    Write insert

--- data_structures_algorithms_curriculum/21_heaps_project/lib/max_heap.js
+++ data_structures_algorithms_curriculum/21_heaps_project/lib/max_heap.js
@@ -1,7 +1,26 @@
 class MaxHeap {
-    
+  constructor() {
+    this.array = [null];
+  }
+
+  insert(val) {
+    this.array.push(val);
+    this.siftUp(this.array.length - 1);
+  }
+
+  siftUp(idx) {}
+
+  siftDown(idx) {}
+
+  deleteMax() {}
+
+  getParent(idx) {}
+
+  getLeftChild(idx) {}
+
+  getRightChild(idx) {}
 }
 
 module.exports = {
-    MaxHeap
+  MaxHeap,
 };
