8e3b887fcdcbf684735398fbb5f671bd29768902
Fares <fares.osman@outlook.com>
2020-08-23 23:07:06 -0400

    Write getParent, getLeftChild, getRightChild

--- data_structures_algorithms_curriculum/21_heaps_project/lib/max_heap.js
+++ data_structures_algorithms_curriculum/21_heaps_project/lib/max_heap.js
@@ -14,11 +14,17 @@ class MaxHeap {
 
   deleteMax() {}
 
-  getParent(idx) {}
+  getParent(idx) {
+      return Math.floor(idx / 2);
+  }
 
-  getLeftChild(idx) {}
+  getLeftChild(idx) {
+      return idx * 2; 
+  }
 
-  getRightChild(idx) {}
+  getRightChild(idx) {
+      return idx * 2 + 1; 
+  }
 }
 
 module.exports = {
