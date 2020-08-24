c91183aa7b31e968260040e1d1aa9b1d50bc80ef
Fares <fares.osman@outlook.com>
2020-08-23 21:43:53 -0400

    Question in progress - need to read up more on graph algorithms

--- data_structures_algorithms_curriculum/20_graph_project/lib/leet_code_207.js
+++ data_structures_algorithms_curriculum/20_graph_project/lib/leet_code_207.js
@@ -1,6 +1,25 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/course-schedule/
 
-function canFinish(numCourses, prerequisites) {
+function createGraph(prereqs) {
+  let adjacencyList = {};
 
+  prereqs.forEach((prereq) => {
+    let [courseToTake, req] = prereq;
+    if (courseToTake in adjacencyList) {
+      adjacencyList[courseToTake].push(String(req));
+    } else {
+      adjacencyList[courseToTake] = [String(req)];
+    }
+
+    if (!(req in adjacencyList)) {
+      adjacencyList[req] = [];
+    }
+  });
+
+  return adjacencyList;
 }
+
+function canFinish(numCourses, prerequisites) {}
+
+console.log(createGraph([[1, 0]]));
