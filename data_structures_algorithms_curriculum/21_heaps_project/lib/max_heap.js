ddfcff35d2408b5fd1a58c68fdb06ffcda545eda
Fares <fares.osman@outlook.com>
2020-08-24 23:26:22 -0400

    Write siftUp

--- data_structures_algorithms_curriculum/21_heaps_project/lib/max_heap.js
+++ data_structures_algorithms_curriculum/21_heaps_project/lib/max_heap.js
@@ -8,22 +8,31 @@ class MaxHeap {
     this.siftUp(this.array.length - 1);
   }
 
-  siftUp(idx) {}
+  siftUp(idx) {
+    if (idx === 1) return;
+    let parentIdx = this.getParent(idx);
+
+    if (this.array[idx] > this.array[parentIdx]) {
+      let temp = this.array[parentIdx];
+      this.array[parentIdx] = this.array[idx];
+      this.array[idx] = temp;
+      this.siftUp(parentIdx);
+    }
+  }
 
   siftDown(idx) {}
 
   deleteMax() {}
 
   getParent(idx) {
-      return Math.floor(idx / 2);
+    return Math.floor(idx / 2);
   }
-
   getLeftChild(idx) {
-      return idx * 2; 
+    return idx * 2;
   }
 
   getRightChild(idx) {
-      return idx * 2 + 1; 
+    return idx * 2 + 1;
   }
 }
 
