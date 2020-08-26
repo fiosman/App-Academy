c36a01f9dd0327576ad578b26e893477c034b8c4
Fares <fares.osman@outlook.com>
2020-08-25 23:21:56 -0400

    write findKthLargest using max heap

--- data_structures_algorithms_curriculum/21_heaps_project/lib/leet_code_215.js
+++ data_structures_algorithms_curriculum/21_heaps_project/lib/leet_code_215.js
@@ -1,3 +1,19 @@
 //https://leetcode.com/problems/kth-largest-element-in-an-array/
+import MaxHeap from "./max_heap";
 
-function findKthLargest (nums, k) {}
+function findKthLargest(nums, k) {
+  let maxHeap = new MaxHeap();
+
+  //heapify the given array;
+  for (let i = 0; i < nums.length; i++) {
+    maxHeap.insert(nums[i]);
+  }
+
+  let kthElement = 0;
+  while (kthElement < k - 1) {
+    maxHeap.deleteMax();
+    kthElement++;
+  }
+
+  return maxHeap.array[1];
+}
