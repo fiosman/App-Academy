720ada10567d3182c015e2194cffacb7b149c396
Fares <fares.osman@outlook.com>
2020-08-30 16:19:54 -0400

    Implement invertBinaryTree for fun

--- data_structures_algorithms_curriculum/17_tree_order_project/lib/tree_node.js
+++ data_structures_algorithms_curriculum/17_tree_order_project/lib/tree_node.js
@@ -9,3 +9,35 @@ class TreeNode {
 module.exports = {
   TreeNode,
 };
+
+function invertTree(root) {
+  if (!root) {
+    return null;
+  }
+
+  let temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+}
+
+let four = new TreeNode(4);
+let two = new TreeNode(2);
+let seven = new TreeNode(7);
+let one = new TreeNode(1);
+let three = new TreeNode(3);
+let six = new TreeNode(6);
+let nine = new TreeNode(9);
+
+four.left = two;
+four.right = seven;
+two.left = one;
+two.right = three;
+seven.left = six;
+seven.right = nine;
+
+console.log(invertTree(four));
